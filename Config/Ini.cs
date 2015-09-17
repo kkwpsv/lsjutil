a415031bb95fd436ee531bd24c54ef3e4ac03f0e
lsj <sdzxwxlsj@163.com>
2015-09-17 23:40:13 +0800

    Update Note

--- Config/Ini.cs
+++ Config/Ini.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -7,14 +7,14 @@ using System.Text;
 namespace Lsj.Util.Config
 {
     /// <summary>
-    /// Ini配置文件读取类
+    /// Ini Config Class
     /// </summary>
     public class Ini
     {
         string path;
         /// <summary>
-        /// 用Ini路径初始化一个新实例
-        /// <param name="path">Ini路径</param>  
+        /// Initiate A New Instance With A Path
+        /// <param name="path">Ini Path</param>  
         /// </summary>
         public Ini(string path)
         {
@@ -22,25 +22,25 @@ namespace Lsj.Util.Config
         }
 
         /// <summary>
-        /// 写INI文件
+        /// Write Ini File
         /// </summary>
         /// <param name="Section">Section</param>
         /// <param name="Key">Key</param>
-        /// <param name="Value">值</param>
+        /// <param name="Value">Value</param>
         public void IniWriteValue(string Section, string Key, string Value)
         {
             WritePrivateProfileString(Section, Key, Value, this.path);
         }
 
         /// <summary>
-        /// 读取INI文件
+        /// Read Ini File
         /// </summary>
         /// <param name="Section"></param>
         /// <param name="Key"></param>
         /// <returns></returns>
         public string IniReadValue(string Section, string Key)
         {
-            StringBuilder temp = new StringBuilder(512);
+            StringBuilder temp = new StringBuilder();
             long i = GetPrivateProfileString(Section, Key, "Null", temp, 512, this.path);
             return temp.ToString();
         }
