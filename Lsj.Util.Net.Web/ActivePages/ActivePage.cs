3f05b24fa72a267e7c0ed5b00967dda3666ebcc0
lsj <sdzxwxlsj>
2015-11-14 22:53:50 +0800

    11.14

---
+++ Lsj.Util.Net.Web/ActivePages/ActivePage.cs
@@ -0,0 +1,26 @@
+﻿using Lsj.Util.Net.Web.Request;
+using Lsj.Util.Net.Web.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lsj.Util.Net.Web.ActivePages
+{
+    public abstract class ActivePage
+    {
+        public HttpRequest request
+        {
+            private set;
+            get;
+        }
+        public HttpResponse response
+        {
+            private set;
+            get;
+        }
+        public void Process()
+        {
+        }
+    }
+}
---
+++ Lsj.Util.Net.Web/Modules/ActivePageModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lsj.Util.Net.Web.Request;
+using Lsj.Util.Net.Web.Response;
+using Lsj.Util.Net.Web.ActivePages;
+using Lsj.Util.Collections;
+
+namespace Lsj.Util.Net.Web.Modules
+{
+    public class ActivePageModule : IModule
+    {
+        public delegate ActivePage CreatePage();
+        public SafeDictionary<string, CreatePage> pages = new SafeDictionary<string, CreatePage>();
+        public bool CanProcess(HttpRequest request, ref int code)
+        {
+            return pages.ContainsKey(request.uri);
+        }
+
+        public HttpResponse Process(HttpRequest request)
+        {
+            if (pages[request.uri] == null)
+            {
+                return new ErrorResponse(404);
+            }
+            else
+            {
+                var page = pages[request.uri].Invoke();
+                page.Process();
+                return page.response;
+            }
+        }
+    }
+}
