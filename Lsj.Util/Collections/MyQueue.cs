63b9a90f098f5bff22287d7882f08445c5d4b89c
lsj <lsj@sdlsj.pw>
2017-03-28 16:18:52 +0800

    Add MyQueue

---
+++ Lsj.Util/Collections/MyQueue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lsj.Util.Collections
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MyQueue<T>
+    {
+        SeqList<T> m_list;
+        /// <summary>
+        /// 
+        /// </summary>
+        public MyQueue()
+        {
+            this.m_list = new SeqList<T>();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        public void Enqueue(T value)
+        {
+            m_list.Add(value);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public T Dequeue()
+        {
+            if (m_list.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                var value = m_list[0];
+                m_list.RemoveAt(0);
+                return value;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public T Peek()
+        {
+            if (m_list.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                var value = m_list[0];
+                return value;
+            }
+        }
+    }
+}
