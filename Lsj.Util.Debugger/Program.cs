c82db0ed4e2920b08d62bba1fd3f81bcb62443cc
lsj <lsj@sdlsj.pw>
2017-06-21 17:35:30 +0800

    Add Some Sort

--- Lsj.Util.Debugger/Program.cs
+++ Lsj.Util.Debugger/Program.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using Lsj.Util.Office.Word;
-using Microsoft.Office.Core;
-using Microsoft.Office.Interop.Word;
+using Lsj.Util.Collections;
 
 namespace Lsj.Util.Debugger
 {
@@ -12,154 +10,44 @@ namespace Lsj.Util.Debugger
     {
         public static void Main()
         {
-
-            try
+            var random = new Random(123456);
+            var list = new List<double>(10);
+            for (int i = 0; i < 10; i++)
             {
-
+                list.Add(random.NextDouble());
             }
-            finally
+            list.BubbleSort();
+            for (int i = 0; i < 10; i++)
             {
-                try
-                {
-
-                }
-                finally
-                {
-                    Console.WriteLine(1);
-                    throw new Exception();
-                    Console.WriteLine(2);
-                }
-
+                Console.WriteLine(list[i]);
             }
+            Console.WriteLine("===============================");
+            random = new Random(123456);
+            list = new List<double>(10);
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(random.NextDouble());
+            }
+            list.SingleSelectionSort();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine("===============================");
+            random = new Random(123456);
+            list = new List<double>(10);
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(random.NextDouble());
+            }
+            list.DirectInsertionSort();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine("===============================");
 
-            //	IntPtr a = Marshal.AllocHGlobal(1000000000);
-            //	Console.ReadLine();
-            //using (var doc = new WordDocument())
-            //{
-            //   doc.SetDocPaper(WdPaperSize.wdPaperA4);
-            //    doc.SetDocMargin(doc.MillimetersToPoints(38.1), doc.MillimetersToPoints(31.9), doc.MillimetersToPoints(27), doc.MillimetersToPoints(19.4));
-            //    doc.SetAppendStyle(size: 28, alignment: eParagraphAlignment.Center);
-            //    doc.AppendLine();
-            //    doc.AppendLine();
-
-            //    doc.SetAppendStyle(size: 22, fontname: "华文中宋", alignment: eParagraphAlignment.Center, fontcolor: Color.FromArgb(68, 84, 106));
-            //    doc.AppendLine("中小学生学业诊断分析系统");
-            //    doc.AppendLine("学业支持子系统个体测评报告");
-
-            //    doc.SetAppendStyle(size: 16, alignment: eParagraphAlignment.Center);
-            //    doc.AppendBlankLine(9);
-
-            //    doc.SetAppendStyle(size: 16, fontname: "宋体", alignment: eParagraphAlignment.Center, fontcolor: Color.Black, underline: eUnderline.Single);
-            //    doc.AppendLine("学校： 	远东仁民");
-
-            //    doc.SetAppendStyle(size: 16, alignment: eParagraphAlignment.Center);
-            //    doc.AppendLine();
-
-            //    doc.SetAppendStyle(size: 16, fontname: "宋体", alignment: eParagraphAlignment.Center, fontcolor: Color.Black, underline: eUnderline.Single);
-            //    doc.AppendLine("姓名： 	  李端沐");
-            //    doc.AppendPage();
-
-            //    doc.SetAppendStyle(size: 24, fontname: "宋体", alignment: eParagraphAlignment.Center, fontcolor: Color.FromArgb(46, 116, 181));
-            //    doc.AppendLine("目    录");
-
-            //    doc.SetAppendStyle(size: 14, fontname: "宋体", alignment: eParagraphAlignment.Left, fontcolor: Color.Black);
-            //    doc.AppendTableOfContents();
-
-            //    doc.AppendSection();
-            //    var section = doc.Sections[1];
-            //    section.AddPageNumberAtFooter();
-
-            //    doc.SetAppendStyle(style: eBuiltinStyle.Heading1);
-            //    doc.AppendLine("a");
-            //    doc.SetAppendStyle(style: eBuiltinStyle.Heading2);
-            //    doc.AppendLine("a");
-
-
-            //    doc.TablesOfContents[0].Update();
-            //    doc.TablesOfContents[0].Select();
-            //    doc.SetSelectionStyle(size: 14, fontname: "宋体", alignment: eParagraphAlignment.Left, fontcolor: Color.Black);
-
-            //    doc.SetAppendStyle(size: 24, fontname: "华文中宋", alignment: eParagraphAlignment.Center, fontcolor: Color.Black, backgroundcolor: Color.AliceBlue, bold: true, style: eBuiltinStyle.Heading1);
-            //    doc.AppendLine("心理测评知识普及");
-            //    doc.TablesOfContents[0].Update();
-
-            //    doc.SetAppendStyle(style: eBuiltinStyle.BodyText);
-            //    doc.AddTable(3, 3);
-            //    doc.Tables[0].AddTableBorder(Color.Red, Color.Red);
-            //    doc.Tables[0].SetTitle("table1");
-            //    doc.Tables[0].SetRowStyle(1, Color.AliceBlue);
-
-            //    doc.AddTable(3, 4);
-            //    Console.WriteLine(doc.Tables.Count());
-            //    doc.Tables[1].MergeCell(1, 1, 1, 2);
-            //    doc.Tables[1].AddTableBorder();
-            //    doc.Tables[1].CellText(1, 1, "test");
-            //    doc.Tables[1].SetCellStyle(1, 1, backgroundcolor: Color.AliceBlue, bold: true);
-
-            //    using (var chart = doc.AddChart(XlChartType.xlColumnClustered))
-            //    {
-            //        string[] names = { "公司A", "公司B", "公司C", "公司D", "公司E" }; // 数据名称
-            //        double[] values = { 10.0, 32.5, 22.4, 34.1, 15.9 }; // 对应数据
-            //        double[] values1 = { 5, 5, 5, 5, 5 }; // 对应数据
-            //        double[] values2 = { 5, 5, 3, 5, 5 }; // 对应数据
-            //        double[] values3 = { 5, 4, 5, 2, 5 }; // 对应数据
-            //        double[] values4 = { 0, 1, 1, 2, 2 }; // 对应数据
-            //        chart.SetData(names, "123", values);
-            //        chart.AddNewSeries(XlChartType.xlLine, "456111", values1);
-            //        chart.AddNewSeries(XlChartType.xlLine, "45611", values2);
-            //        chart.AddNewSeries(XlChartType.xlLine, "45611111", values3);
-            //        chart.AddNewSeries(XlChartType.xlLine, "4561111", values4);
-            //    }
-            //    doc.SaveAs(@"R:\temp.docx");
-            //    Console.ReadLine();
-            //    doc.Close();
-
-            //}
-
-            //using (var doc = new WordDocument())
-            //{
-
-            //    using (var chart = doc.AddChart(eChartType.xlColumnClustered))
-            //    {
-            //        var random = new Random();
-            //        List<string> fuck1 = new List<string>();
-            //        List<double> fuck2 = new List<double>();
-            //        List<double> fuck3 = new List<double>();
-            //        for (int i = 0; i < 5; i++)
-            //        {
-            //            fuck1.Add("Fuck" + i);
-            //            fuck2.Add(random.NextDouble());
-            //            fuck3.Add(random.NextDouble());
-            //        }
-
-
-
-
-            //        chart.SetData(fuck1.ToArray(), "123", fuck2.ToArray());
-            //        chart.AddNewSeries(eChartType.xlLine, "456111", fuck3.ToArray());
-            //    }
-
-            //    throw new Exception();
-
-
-            //    doc.SaveAs(@"R:\temp.docx");
-            //    Console.ReadLine();
-            //    doc.Close();
-
-            //}
-
-
-            //using (var doc1 = new WordDocument())
-            //{
-            //    doc1.AppendLine("test");
-
-            //    using (var doc2 = new WordDocument())
-            //    {
-            //        doc1.fuck.Content.Copy();
-            //        doc2.fuck.Application.Selection.Paste();
-            //    }
-            //}
-
+            Console.ReadLine();
         }
 
     }
--- Lsj.Util/Collections/ArrayHelper.cs
+++ Lsj.Util/Collections/ArrayHelper.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 
-#if NETCOREAPP1_1
+
 namespace Lsj.Util.Collections
 {
     public static class ArrayHelper
     {
+#if NETCOREAPP1_1
         public static TOutput[] ConvertAll<TInput, TOutput>(TInput[] array, Func<TInput, TOutput> converter)
         {
             var result = new List<TOutput>();
@@ -15,6 +16,56 @@ namespace Lsj.Util.Collections
             }
             return result.ToArray();
         }
-    }
-}
 #endif
+        public static (T value, int row, int col)[] ToValueTuples<T>(this T[][] array)
+        {
+            var result = new List<ValueTuple<T, int, int>>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (!array[i][j].Equals(default(T)))
+                    {
+                        result.Add((array[i][j], i, j));
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+        public static T[][] Transposition<T>(this T[][] array)
+        {
+            if (array[0] == null)
+            {
+                return null;
+            }
+            var result = new T[array[0].Length][];
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[0].Length && j < array[i].Length; j++)
+                {
+                    result[j][i] = array[i][j];
+                }
+            }
+            return result;
+        }
+        public static (T value, int row, int col)[] Transposition<T>(this (T value, int row, int col)[] tuples)
+        {
+            var result = new(T, int, int)[tuples.Length];
+
+            for (int i = 0, x = 0; i < result.Length; i++, x++)
+            {
+                for (int j = 0; j < tuples.Length; j++)
+                {
+                    if (tuples[j].row == i)
+                    {
+                        result[x] = (tuples[j].value, tuples[j].col, tuples[j].row);
+                        x++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+}
+
