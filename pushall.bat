2716cf531d73fce936feaa2b83ef9bc3add4d1c0
lsj <lsj@sdlsj.pw>
2017-09-16 15:39:04 +0800

    Change To Nuget.org

--- pushall.bat
+++ pushall.bat
@@ -1,19 +1,19 @@
-nuget push packages/Lsj.Util.1.0.3.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push packages/Lsj.Util.CsBuilder.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push packages/Lsj.Util.Data.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push packages/Lsj.Util.HtmlBuilder.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push packages/Lsj.Util.Net.1.0.1.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push packages/Lsj.Util.Net.Web.1.0.3.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push packages/Lsj.Util.Office.1.0.15.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push packages/Lsj.Util.Protobuf.1.0.1.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push packages/Lsj.Util.SQLBuilder.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
+nuget push packages/Lsj.Util.1.0.3.nupkg -Source nuget.org
+nuget push packages/Lsj.Util.CsBuilder.1.0.0.nupkg -Source nuget.org
+nuget push packages/Lsj.Util.Data.1.0.0.nupkg -Source nuget.org
+nuget push packages/Lsj.Util.HtmlBuilder.1.0.0.nupkg -Source nuget.org
+nuget push packages/Lsj.Util.Net.1.0.1.nupkg -Source nuget.org
+nuget push packages/Lsj.Util.Net.Web.1.0.3.nupkg -Source nuget.org
+nuget push packages/Lsj.Util.Office.1.0.15.nupkg -Source nuget.org
+nuget push packages/Lsj.Util.Protobuf.1.0.1.nupkg -Source nuget.org
+nuget push packages/Lsj.Util.SQLBuilder.1.0.0.nupkg -Source nuget.org
 
 
-nuget push Lsj.Util/bin/Release/Lsj.Util.Core.1.0.3.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push Lsj.Util.CsBuilder/bin/Release/Lsj.Util.CsBuilder.Core.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push Lsj.Util.HtmlBuilder/bin/Release/Lsj.Util.HtmlBuilder.Core.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push Lsj.Util.Net/bin/Release/Lsj.Util.Net.Core.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push Lsj.Util.Net.Web/bin/Release/Lsj.Util.Net.Web.Core.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push Lsj.Util.Protobuf/bin/Release/Lsj.Util.Protobuf.Core.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push Lsj.Util.SQLBuilder/bin/Release/Lsj.Util.SQLBuilder.Core.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
-nuget push Lsj.Util.AspNet/bin/Release/Lsj.Util.AspNet.Core.1.0.0.nupkg -Source https://nuget.o0o0o0o.cn
+nuget push Lsj.Util/bin/Release/Lsj.Util.Core.1.0.3.nupkg -Source nuget.org
+nuget push Lsj.Util.CsBuilder/bin/Release/Lsj.Util.CsBuilder.Core.1.0.0.nupkg -Source nuget.org
+nuget push Lsj.Util.HtmlBuilder/bin/Release/Lsj.Util.HtmlBuilder.Core.1.0.0.nupkg -Source nuget.org
+nuget push Lsj.Util.Net/bin/Release/Lsj.Util.Net.Core.1.0.0.nupkg -Source nuget.org
+nuget push Lsj.Util.Net.Web/bin/Release/Lsj.Util.Net.Web.Core.1.0.0.nupkg -Source nuget.org
+nuget push Lsj.Util.Protobuf/bin/Release/Lsj.Util.Protobuf.Core.1.0.0.nupkg -Source nuget.org
+nuget push Lsj.Util.SQLBuilder/bin/Release/Lsj.Util.SQLBuilder.Core.1.0.0.nupkg -Source nuget.org
+nuget push Lsj.Util.AspNet/bin/Release/Lsj.Util.AspNet.Core.1.0.0.nupkg -Source nuget.org
