0b30eb49ae469e07b7d82f25ac7a96ae0442008f
lsj <lsj@sdlsj.pw>
2017-09-16 15:39:29 +0800

    Some Comment

---
+++ Lsj.Util/Binary/DosExeFile.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Lsj.Util.Binary
+{
+    /// <summary>
+    /// Dos Executable File
+    /// </summary>
+    public class DosExeFile : BaseBinaryFile
+    {
+        /// <summary>
+        /// Dos Header
+        /// </summary>
+        public DosHeader DosHeader
+        {
+            get;
+            private set;
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Lsj.Util.Binary.DosExeFile"/> class.
+        /// </summary>
+        /// <param name="path">File Path</param>
+        public DosExeFile(string path) : base(path)
+        {
+        }
+
+        /// <summary>
+        /// DosHeader Byte Array
+        /// </summary>
+        protected byte[] dosheaderbytes;
+
+        /// <summary>
+        /// Read
+        /// </summary>
+        protected override void Read()
+        {
+            byte[] headers = new byte[28];
+            if (file.Read(headers, 0, 28) == 28 && headers[0] == 0x4d && headers[1] == 0x5a)
+            {
+                var headersize = (headers[0x08] | headers[0x09] << 8) << 4;
+                if (headersize >= 28)
+                {
+                    Array.Resize(ref headers, headersize);
+                    file.Read(headers, 28, headersize - 28);
+                    this.dosheaderbytes = headers;
+
+                    unsafe
+                    {
+                        var dosheader = new DosHeader();
+                        fixed (byte* headerptr = dosheaderbytes)
+                        {
+                            UnsafeHelper.Copy(headerptr, dosheader.buffer, 28);
+                        }
+                        this.DosHeader = dosheader;
+                    }
+
+
+
+                }
+                else
+                {
+                    throw new ArgumentException("Error Dos Exe File");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Error Dos Exe File");
+            }
+        }
+        /// <summary>
+        /// RelocationTable
+        /// </summary>
+        /// <returns></returns>
+        public RelocationItem[] GetRelocationTable()
+        {
+            var result = new RelocationItem[DosHeader.NumRelocs];
+            var size = DosHeader.NumRelocs * 4;
+
+            unsafe
+            {
+
+                fixed (byte* x = this.dosheaderbytes)
+                {
+                    var src = x + DosHeader.reloc_table_offset;
+                    fixed (RelocationItem* dst = result)
+                    {
+                        UnsafeHelper.Copy(src, (byte*)dst, size);
+                    }
+                }
+            }
+
+            return result;
+
+
+        }
+    }
+    /// <summary>
+    /// Dos Header
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct DosHeader
+    {
+        [FieldOffset(0x00)]
+        internal unsafe fixed byte buffer[28];
+
+        [FieldOffset(0x00)]
+        public readonly UInt16 Signature;
+
+        [FieldOffset(0x02)]
+        public readonly UInt16 BytesInLastBlock;
+
+        [FieldOffset(0x04)]
+        public readonly UInt16 BlocksInFile;
+
+        [FieldOffset(0x06)]
+        public readonly UInt16 NumRelocs;
+
+        [FieldOffset(0x08)]
+        public readonly UInt16 HeaderParagraphs;
+
+        [FieldOffset(0x0A)]
+        public readonly UInt16 MinExtraParagraphs;
+
+        [FieldOffset(0x0C)]
+        public readonly UInt16 MaxExtraParagraphs;
+
+        [FieldOffset(0x0E)]
+        public readonly UInt16 ss;
+
+        [FieldOffset(0x10)]
+        public readonly UInt16 sp;
+
+        [FieldOffset(0x12)]
+        public readonly UInt16 checksum;
+
+        [FieldOffset(0x14)]
+        public readonly UInt16 ip;
+
+        [FieldOffset(0x16)]
+        public readonly UInt16 cs;
+
+        [FieldOffset(0x18)]
+        public readonly UInt16 reloc_table_offset;
+
+        [FieldOffset(0x1A)]
+        public readonly UInt16 overlay_number;
+
+
+
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x04)]
+    public struct RelocationItem
+    {
+        [FieldOffset(0x00)]
+        public readonly UInt16 offset;
+        [FieldOffset(0x02)]
+        public readonly UInt16 segment;
+    };
+}
---
+++ Lsj.Util/Binary/PEFile.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Lsj.Util.Binary
+{
+    public class PEFile : DosExeFile
+    {
+        public PEFile(string path) : base(path)
+        {
+        }
+
+        public NTHeader NTHeader
+        {
+            get;
+            private set;
+        }
+        protected override void Read()
+        {
+            base.Read();
+            if (this.DosHeader.HeaderParagraphs == 4)
+            {
+                var peoffset = dosheaderbytes.ConvertToInt(0x3c);
+                file.Seek(peoffset, SeekOrigin.Begin);
+                byte[] x = new byte[4];
+                file.Read(x, 0, 4);
+                var t = x.ConvertToInt();
+                if (t != 0x4550)
+                {
+                    throw new ArgumentException("Error PE File");
+                }
+                var fileheader = new FileHeader();
+
+
+
+                var ntheader = new NTHeader(0x4550, fileheader);
+
+                this.NTHeader = ntheader;
+
+            }
+            else
+            {
+                throw new ArgumentException("Error PE File");
+            }
+        }
+    }
+    public struct NTHeader
+    {
+        public NTHeader(UInt16 signature, FileHeader fileheader)
+        {
+            this.signature = signature;
+            this.FileHeader = fileheader;
+        }
+        public readonly UInt16 signature;
+        public readonly FileHeader FileHeader;
+    }
+    [StructLayout(LayoutKind.Explicit, Size = 20)]
+    public struct FileHeader
+    {
+        [FieldOffset(0x00)]
+        unsafe fixed byte buffer[20];
+        [FieldOffset(0x00)]
+        Machine Machine;
+        [FieldOffset(0x02)]
+        UInt16 NumberOfSections;
+        [FieldOffset(0x04)]
+        UInt32 TimeDateStamp;
+        [FieldOffset(0x08)]
+        UInt32 PointerToSymbolTable;
+        [FieldOffset(0x0C)]
+        UInt32 NumberOfSymbols;
+        [FieldOffset(0x10)]
+        UInt16 SizeOfOptionalHeader;
+        [FieldOffset(0x12)]
+        Characteristics Characteristics;
+
+    }
+    public enum Machine : UInt16
+    {
+        UNKNOWN = 0,
+        I386 = 0x014c,
+        R3000 = 0x0162,
+        R4000 = 0x0166,
+        R10000 = 0x0168,
+        WCEMIPSV2 = 0x0169,
+        ALPHA = 0x0184,
+        SH3 = 0x01a2,
+        SH3DSP = 0x01a3,
+        SH3E = 0x01a4,
+        SH4 = 0x01a6,
+        SH5 = 0x01a8,
+        ARM = 0x01c0,
+        THUMB = 0x01c2,
+        AM33 = 0x01d3,
+        POWERPC = 0x01F0,
+        POWERPCFP = 0x01f1,
+        IA64 = 0x0200,
+        MIPS16 = 0x0266,
+        ALPHA64 = 0x0284,
+        MIPSFPU = 0x0366,
+        MIPSFPU16 = 0x0466,
+        AXP64 = 0x284,
+        TRICORE = 0x0520,
+        CEF = 0x0CEF,
+        EBC = 0x0EBC,
+        AMD64 = 0x8664,
+        M32R = 0x9041,
+        CEE = 0xC0EE
+    }
+    [Flags]
+    public enum Characteristics : UInt16
+    {
+        RELOCS_STRIPPED = 0x0001,
+        EXECUTABLE_IMAGE = 0x0002,
+        LINE_NUMS_STRIPPED = 0x0004,
+        LOCAL_SYMS_STRIPPED = 0x0008,
+        AGGRESIVE_WS_TRIM = 0x0010,
+        LARGE_ADDRESS_AWARE = 0x0020,
+        BYTES_REVERSED_LO = 0x0080,
+        BIT32_MACHINE = 0x0100,
+        DEBUG_STRIPPED = 0x0200,
+        REMOVABLE_RUN_FROM_SWAP = 0x0400,
+        NET_RUN_FROM_SWAP = 0x0800,
+        SYSTEM = 0x1000,
+        DLL = 0x2000,
+        UP_SYSTEM_ONLY = 0x4000,
+        BYTES_REVERSED_HI = 0x8000
+    }
+}
