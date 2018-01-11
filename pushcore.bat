1387fa77a0481bc265deda6495f31dc4aa5f48d8
Shaojun Li <lsj@sdlsj.pw>
2018-01-12 01:10:29 +0800

    Push .Net Core nupkg

---
+++ pushcore.bat
@@ -0,0 +1,8 @@
+nuget push Lsj.Util/bin/Release/Lsj.Util.Core.1.1.0.nupkg -Source nuget.org
+nuget push Lsj.Util.CsBuilder/bin/Release/Lsj.Util.CsBuilder.Core.1.1.0.nupkg -Source nuget.org
+nuget push Lsj.Util.HtmlBuilder/bin/Release/Lsj.Util.HtmlBuilder.Core.1.1.0.nupkg -Source nuget.org
+nuget push Lsj.Util.Net/bin/Release/Lsj.Util.Net.Core.1.1.0.nupkg -Source nuget.org
+nuget push Lsj.Util.Net.Web/bin/Release/Lsj.Util.Net.Web.Core.1.1.0.nupkg -Source nuget.org
+nuget push Lsj.Util.Protobuf/bin/Release/Lsj.Util.Protobuf.Core.1.1.0.nupkg -Source nuget.org
+nuget push Lsj.Util.SQLBuilder/bin/Release/Lsj.Util.SQLBuilder.Core.1.1.0.nupkg -Source nuget.org
+nuget push Lsj.Util.AspNet/bin/Release/Lsj.Util.AspNet.Core.1.1.0.nupkg -Source nuget.org
