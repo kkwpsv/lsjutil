ab61eff183f5bbd358ed917e9d30fea84a1c89df
Shaojun Li <lsj@sdlsj.pw>
2018-01-12 01:10:44 +0800

    Add Remark

--- Lsj.Util/ICloneable.cs
+++ Lsj.Util/ICloneable.cs
@@ -4,6 +4,9 @@ using System.Text;
 #if NETCOREAPP1_1
 namespace Lsj.Util
 {
+    /// <summary>
+    /// ICloneable Just For Compatibility
+    /// </summary>
     public interface ICloneable
     {
     }
