885a4b0d5bc111a5b990ea242edb725a48e13096
Shaojun Li <lsj@sdlsj.pw>
2018-01-12 01:06:01 +0800

    Update Version and Format assemblyinfo

--- Lsj.Util/Properties/AssemblyInfo.cs
+++ Lsj.Util/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lsj")]
 [assembly: AssemblyProduct("Lsj.Util")]
-[assembly: AssemblyCopyright("Copyright © Lsj 2015")]
+[assembly: AssemblyCopyright("Copyright © Lsj 2015-")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 #endif
