86e44462bc51e531348a38f2081e4ed8ecbd91ad
Shaojun Li <lsj@sdlsj.pw>
2018-01-12 01:14:05 +0800

    Add some convert method

--- Lsj.Util/MathHelper.cs
+++ Lsj.Util/MathHelper.cs
@@ -14,9 +14,14 @@ namespace Lsj.Util
         /// <summary>
         /// ConvertToInt
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="min"></param>
-        /// <param name="max"></param>
+        /// <param name="x">Value</param>
+        public static int ConvertToInt(this long x) => ConvertToInt(x, int.MinValue, int.MaxValue);
+        /// <summary>
+        /// ConvertToInt
+        /// </summary>
+        /// <param name="x">Value</param>
+        /// <param name="min">MinValue</param>
+        /// <param name="max">MaxValue</param>
         public static int ConvertToInt(this long x, int min, int max)
         {
             if (x > max)
@@ -29,7 +34,42 @@ namespace Lsj.Util
         /// <summary>
         /// ConvertToInt
         /// </summary>
-        /// <param name="x"></param>
-        public static int ConvertToInt(this long x) => ConvertToInt(x, int.MinValue, int.MaxValue);
+        /// <param name="x">Value</param>
+        public static int ConvertToInt(this decimal x) => ConvertToInt(x, int.MinValue, int.MaxValue);
+        /// <summary>
+        /// ConvertToInt
+        /// </summary>
+        /// <param name="x">Value</param>
+        /// <param name="min">MinValue</param>
+        /// <param name="max">MaxValue</param>
+        public static int ConvertToInt(this decimal x, int min, int max)
+        {
+            if (x > max)
+                return max;
+            else if (x < min)
+                return min;
+            else
+                return (int)x;
+        }
+        /// <summary>
+        /// ConvertToLong
+        /// </summary>
+        /// <param name="x">Value</param>
+        public static long ConvertToLong(this decimal x) => ConvertToLong(x, long.MinValue, long.MaxValue);
+        /// <summary>
+        /// ConvertToInt
+        /// </summary>
+        /// <param name="x">Value</param>
+        /// <param name="min">MinValue</param>
+        /// <param name="max">MaxValue</param>
+        public static long ConvertToLong(this decimal x, long min, long max)
+        {
+            if (x > max)
+                return max;
+            else if (x < min)
+                return min;
+            else
+                return (long)x;
+        }
     }
 }
