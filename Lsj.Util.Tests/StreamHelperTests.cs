8638e813cda606b84466372d65b20180004beed4
Shaojun Li <lsj@sdlsj.vip>
2018-04-28 11:38:29 +0800

    Add StramHelper Test

---
+++ Lsj.Util.Tests/StreamHelperTests.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lsj.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lsj.Util.Tests
+{
+    [TestClass()]
+    public class StreamHelperTests
+    {
+        public class StreamCannotSeek : Stream
+        {
+            public override bool CanRead => throw new NotImplementedException();
+
+            public override bool CanSeek => false;
+
+            public override bool CanWrite => throw new NotImplementedException();
+
+            public override long Length => throw new NotImplementedException();
+
+            public override long Position
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public override void Flush() => throw new NotImplementedException();
+            public override int Read(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+            public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
+            public override void SetLength(long value) => throw new NotImplementedException();
+            public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+        }
+        public class StreamCannotRead : Stream
+        {
+            public override bool CanRead => false;
+
+            public override bool CanSeek => true;
+
+            public override bool CanWrite => throw new NotImplementedException();
+
+            public override long Length => 0;
+
+            public override long Position
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public override void Flush() => throw new NotImplementedException();
+            public override int Read(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                return 0;
+            }
+            public override void SetLength(long value) => throw new NotImplementedException();
+            public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+        }
+        public class StreamEmpty : Stream
+        {
+            public override bool CanRead => true;
+
+            public override bool CanSeek => true;
+
+            public override bool CanWrite => throw new NotImplementedException();
+
+            public override long Length => 0;
+
+            public override long Position
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public override void Flush() => throw new NotImplementedException();
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                return 0;
+            }
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                return 0;
+            }
+            public override void SetLength(long value) => throw new NotImplementedException();
+            public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+        }
+        public class StreamTest : Stream
+        {
+            byte[] data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            public override bool CanRead => true;
+
+            public override bool CanSeek => true;
+
+            public override bool CanWrite => throw new NotImplementedException();
+
+            public override long Length => 10;
+
+            public override long Position
+            {
+                get;
+                set;
+            } = 0;
+
+            public override void Flush() => throw new NotImplementedException();
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                var result = 0;
+                for (int i = 0; i < count && Position < Length; i++, Position++, result++)
+                {
+                    buffer[i] = data[Position];
+                }
+                return result;
+            }
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                return 0;
+            }
+            public override void SetLength(long value) => throw new NotImplementedException();
+            public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReadAllTest_CannotSeek()
+        {
+            new StreamCannotSeek().ReadAll();
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReadAllTest_CannotRead()
+        {
+            new StreamCannotRead().ReadAll();
+        }
+        [TestMethod()]
+        public void ReadAllTest_Empty()
+        {
+            var x = new StreamEmpty().ReadAll();
+            Assert.AreEqual(x.Length, 0);
+        }
+        [TestMethod()]
+        public void ReadAllTest()
+        {
+            var x = new StreamTest().ReadAll();
+            Assert.AreEqual(x.Length, 10);
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(x[i], i);
+            }
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReadAllWithoutSeekTest_CannotRead()
+        {
+            new StreamCannotRead().ReadAllWithoutSeek();
+        }
+        [TestMethod()]
+        public void ReadAllWithoutSeekTest_Empty()
+        {
+            var x = new StreamEmpty().ReadAllWithoutSeek();
+            Assert.AreEqual(x.Length, 0);
+        }
+        [TestMethod()]
+        public void ReadAllWithoutSeekTest()
+        {
+            var x = new StreamTest().ReadAllWithoutSeek();
+            Assert.AreEqual(x.Length, 10);
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(x[i], i);
+            }
+        }
+    }
+}
--- Lsj.Util/StreamHelper.cs
+++ Lsj.Util/StreamHelper.cs
@@ -16,24 +16,35 @@ namespace Lsj.Util
         /// ReadAll (Seek Before Read)
         /// </summary>
         /// <param name="stream"></param>
+        /// <exception cref="ArgumentException">The stream cannot seek or read</exception>
         /// <returns></returns>
         public static byte[] ReadAll(this Stream stream)
         {
+            if (!stream.CanSeek)
+            {
+                throw new ArgumentException("Stream must be able to seek.");
+            }
             stream.Seek(0, SeekOrigin.Begin);
             byte[] result = new byte[stream.Length];
-            if (stream.CanRead)
+            if (!stream.CanRead)
             {
-                stream.Read(result, 0, result.Length);
+                throw new ArgumentException("Stream must be able to read.");
             }
+            stream.Read(result, 0, result.Length);
             return result;
         }
         /// <summary>
         /// ReadAll Without Seek
         /// </summary>
         /// <param name="stream"></param>
+        /// <exception cref="ArgumentException">The stream cannot read</exception>
         /// <returns></returns>
         public static byte[] ReadAllWithoutSeek(this Stream stream)
         {
+            if (!stream.CanRead)
+            {
+                throw new ArgumentException("Stream must be able to read.");
+            }
             var result = new List<byte>();
             var x = stream.ReadByte();
             while (x != -1)
@@ -48,20 +59,16 @@ namespace Lsj.Util
         /// </summary>
         /// <param name="stream"></param>
         /// <param name="buffer"></param>
-        public static void Write(this Stream stream, byte[] buffer)
-        {
-            stream.Write(buffer, 0, buffer.Length);
-        }
+        public static void Write(this Stream stream, byte[] buffer) => stream.Write(buffer, 0, buffer.Length);
+
         /// <summary>
         /// Write
         /// </summary>
         /// <param name="stream"></param>
         /// <param name="buffer"></param>
         /// <param name="offset"></param>
-        public static void Write(this Stream stream, byte[] buffer, int offset)
-        {
-            stream.Write(buffer, offset, buffer.Length - offset);
-        }
+        public static void Write(this Stream stream, byte[] buffer, int offset) => stream.Write(buffer, offset, buffer.Length - offset);
+
 
 
 #if !NETCOREAPP2_0
