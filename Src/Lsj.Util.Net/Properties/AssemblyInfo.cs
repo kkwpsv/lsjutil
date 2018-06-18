9c4dffa8e7a3dc048e8311161ca0ba93c5429aeb
Shaojun Li <lsj@sdlsj.vip>
2018-06-19 03:53:48 +0800

    Fix Sign

--- Src/Lsj.Util.Net/Properties/AssemblyInfo.cs
+++ Src/Lsj.Util.Net/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@
 [assembly: AssemblyCopyright("Copyright © Lsj 2016-")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
