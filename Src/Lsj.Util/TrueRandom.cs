d5b85b6928e968a0a2320785cc8348a3f6a2f994
Shaojun Li <lsj@sdlsj.vip>
2018-08-31 13:42:16 +0800

    Add TrueRandom

---
+++ Src/Lsj.Util/TrueRandom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Lsj.Util
+{
+    public class TrueRandom : DisposableClass, IDisposable
+    {
+        private readonly RNGCryptoServiceProvider csp;
+
+        public TrueRandom()
+        {
+            this.csp = new RNGCryptoServiceProvider();
+        }
+        protected override void CleanUpManagedResources()
+        {
+            this.csp.Dispose();
+            base.CleanUpManagedResources();
+        }
+        public int NextInt()
+        {
+            var result = new byte[4];
+            this.csp.GetBytes(result);
+            return QuickBitConverter.ConvertToInt(result);
+        }
+    }
+}
