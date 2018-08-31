391782ea007f5d3ca95fec29f25296b914f50f2c
Shaojun Li <lsj@sdlsj.vip>
2018-08-31 13:45:29 +0800

    Update Push Script

--- push.bat
+++ push.bat
@@ -1,9 +1,10 @@
-nuget push Src/Lsj.Util/bin/Release/Lsj.Util.2.0.3.nupkg -Source nuget.org
+nuget push Src/Lsj.Util/bin/Release/Lsj.Util.2.0.4.nupkg -Source nuget.org
+nuget push Src/Lsj.Util.APIs.Alipay/bin/Release/Lsj.Util.APIs.Alipay.1.0.0.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.AspNetCore/bin/Release/Lsj.Util.AspNetCore.2.0.5.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.CsBuilder/bin/Release/Lsj.Util.CsBuilder.2.0.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Data/bin/Release/Lsj.Util.Data.2.0.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.HtmlBuilder/bin/Release/Lsj.Util.HtmlBuilder.2.0.2.nupkg -Source nuget.org
-nuget push Src/Lsj.Util.JSON/bin/Release/Lsj.Util.JSON.2.0.9.nupkg -Source nuget.org
+nuget push Src/Lsj.Util.JSON/bin/Release/Lsj.Util.JSON.2.1.0.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Net/bin/Release/Lsj.Util.Net.2.0.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Net.Web/bin/Release/Lsj.Util.Net.Web.2.0.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Office/bin/Release/Lsj.Util.Office.2.0.1.nupkg -Source nuget.org
