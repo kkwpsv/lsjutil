34a758cd330d0e8d8431f725dcc4a563f5255433
Shaojun Li <lsj@sdlsj.vip>
2018-10-10 19:20:01 +0800

    Add Some Test

---
+++ Test/Lsj.Util.Tests/UnSafeHelperTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lsj.Util.Tests
+{
+    [TestClass]
+    public class UnSafeHelperTest
+    {
+        [TestMethod]
+        public void ContactTest()
+        {
+            var nullBytes = new byte[0];
+            var result = UnsafeHelper.Contact(nullBytes, nullBytes);
+            Assert.AreEqual(0, result.Length);
+
+            var bytes1 = new byte[] { 0, 1, 2, 3 };
+            var bytes2 = new byte[] { 4, 5, 6 };
+            result = UnsafeHelper.Contact(bytes1, bytes2);
+            Assert.AreEqual(7, result.Length);
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.AreEqual(i, result[i]);
+            }
+        }
+    }
+}
