d563d1b40695dbb7ba250417e6e08b8ca5399e1c
Shaojun Li <i@shaojun.li>
2019-02-26 14:33:03 +0800

    Merge Tests

--- Test/Lsj.Util.Tests/UnsafeHelperTests.cs
+++ Test/Lsj.Util.Tests/UnsafeHelperTests.cs
@@ -127,13 +127,17 @@ namespace Lsj.Util.Tests
         [TestMethod]
         public void ContactTest()
         {
-            var a = new byte[] { 0, 1, 2, 3 };
-            var b = new byte[] { 4, 5 };
-            var result = UnsafeHelper.Contact(a, b);
-            Assert.AreEqual(result.Length, 6);
-            for (int i = 0; i < 6; i++)
+            var nullBytes = new byte[0];
+            var result = UnsafeHelper.Contact(nullBytes, nullBytes);
+            Assert.AreEqual(0, result.Length);
+
+            var bytes1 = new byte[] { 0, 1, 2, 3 };
+            var bytes2 = new byte[] { 4, 5, 6 };
+            result = UnsafeHelper.Contact(bytes1, bytes2);
+            Assert.AreEqual(7, result.Length);
+            for (int i = 0; i < 7; i++)
             {
-                Assert.AreEqual(result[i], i);
+                Assert.AreEqual(i, result[i]);
             }
         }
     }
