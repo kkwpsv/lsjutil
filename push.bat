272bffc3b8bad6f1ea828760d203f7c70f20cb68
Shaojun Li <i@shaojun.li>
2019-05-13 10:37:58 +0800

    Update version to fix missing sign

--- push.bat
+++ push.bat
@@ -6,11 +6,11 @@ nuget push Src/Lsj.Util.AspNetCore/bin/Release/Lsj.Util.AspNetCore.2.3.1.nupkg -
 nuget push Src/Lsj.Util.CsBuilder/bin/Release/Lsj.Util.CsBuilder.2.0.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Data/bin/Release/Lsj.Util.Data.2.0.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.HtmlBuilder/bin/Release/Lsj.Util.HtmlBuilder.2.0.3.nupkg -Source nuget.org
-nuget push Src/Lsj.Util.JSON/bin/Release/Lsj.Util.JSON.3.0.1.nupkg -Source nuget.org
+nuget push Src/Lsj.Util.JSON/bin/Release/Lsj.Util.JSON.3.0.2.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Net/bin/Release/Lsj.Util.Net.2.0.2.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Net.Web/bin/Release/Lsj.Util.Net.Web.2.0.3.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Office/bin/Release/Lsj.Util.Office.2.0.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Protobuf/bin/Release/Lsj.Util.Protobuf.2.0.2.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.SQLBuilder/bin/Release/Lsj.Util.SQLBuilder.2.0.2.nupkg -Source nuget.org
-nuget push Src/Lsj.Util.WPF/bin/Release/Lsj.Util.WPF.1.1.1.nupkg -Source nuget.org
+nuget push Src/Lsj.Util.WPF/bin/Release/Lsj.Util.WPF.1.1.2.nupkg -Source nuget.org
 pause
