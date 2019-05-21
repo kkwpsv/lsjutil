ff6366fb3bf0d434439240bc1dea334b4a435417
Shaojun Li <i@shaojun.li>
2019-05-21 15:01:35 +0800

    Version

--- push.bat
+++ push.bat
@@ -7,12 +7,12 @@ nuget push Src/Lsj.Util.CsBuilder/bin/Release/Lsj.Util.CsBuilder.2.0.1.nupkg -So
 nuget push Src/Lsj.Util.Data/bin/Release/Lsj.Util.Data.2.0.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Dynamic/bin/Release/Lsj.Util.Dynamic.1.0.0.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.HtmlBuilder/bin/Release/Lsj.Util.HtmlBuilder.2.0.3.nupkg -Source nuget.org
-nuget push Src/Lsj.Util.JSON/bin/Release/Lsj.Util.JSON.3.1.0.nupkg -Source nuget.org
+nuget push Src/Lsj.Util.JSON/bin/Release/Lsj.Util.JSON.3.1.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Net/bin/Release/Lsj.Util.Net.2.1.0.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Net.Web/bin/Release/Lsj.Util.Net.Web.2.1.0.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Office/bin/Release/Lsj.Util.Office.2.0.1.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.Protobuf/bin/Release/Lsj.Util.Protobuf.2.0.2.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.SQLBuilder/bin/Release/Lsj.Util.SQLBuilder.2.0.2.nupkg -Source nuget.org
 nuget push Src/Lsj.Util.WinForm/bin/Release/Lsj.Util.WinForm.1.0.0.nupkg -Source nuget.org
-nuget push Src/Lsj.Util.WPF/bin/Release/Lsj.Util.WPF.1.2.0.nupkg -Source nuget.org
+nuget push Src/Lsj.Util.WPF/bin/Release/Lsj.Util.WPF.1.2.1.nupkg -Source nuget.org
 pause
