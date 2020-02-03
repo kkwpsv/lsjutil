9b62ba2aa1f6928ee7c80b0dd38c9eceea0dd4d3
Shaojun Li <i@shaojun.li>
2020-02-03 14:09:03 +0800

    Add IntPtrToHexConverter

---
+++ Src/Lsj.Util.WPF/Converters/IntPtrToHexConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Lsj.Util.WPF.Converters
+{
+    [ValueConversion(typeof(IntPtr), typeof(string), ParameterType = typeof(string))]
+    public class IntPtrToHexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is IntPtr ptr ? ptr.ToString(parameter as string ?? "X8") : DependencyProperty.UnsetValue;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return IntPtr.Size == 4 ? (IntPtr)int.Parse(str, NumberStyles.HexNumber, culture) : (IntPtr)long.Parse(str, NumberStyles.HexNumber, culture);
+            }
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
