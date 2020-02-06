fedc8c15738bf09ee1ff7aea01a1e2b886d340f9
Shaojun Li <i@shaojun.li>
2020-02-06 13:03:17 +0800

    Add FormatMessage,  LocalFree and GetSystemErrorMessageFromCode

---
+++ Src/Lsj.Util.Win32/Extensions/ErrorMessageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Lsj.Util.Win32.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Lsj.Util.Win32.Kernel32;
+
+namespace Lsj.Util.Win32.Extensions
+{
+    public static class ErrorMessageExtensions
+    {
+        public static string GetSystemErrorMessageFromCode(uint code)
+        {
+            if (FormatMessage(FormatMessageFlags.FORMAT_MESSAGE_ALLOCATE_BUFFER | FormatMessageFlags.FORMAT_MESSAGE_FROM_SYSTEM, IntPtr.Zero,
+                code, 0, out var str, 0, IntPtr.Zero) != 0)
+            {
+                return str;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
