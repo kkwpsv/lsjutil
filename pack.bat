68ac08a9ce7abcf9787aea7bb95e4953fa5f4869
Shaojun Li <i@shaojun.li>
2020-02-09 13:26:33 +0800

    Fix bat

--- pack.bat
+++ pack.bat
@@ -1,55 +1,55 @@
 b64 -d %SnkFile% Src\LSJ.snk
 
 cd Src\Lsj.Util\
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.APIs.Alipay
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.APIs.UmeTrip
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.APIs.WeChat
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.AspNetCore
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.CsBuilder
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.Data
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.Dynamic
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.HtmlBuilder
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.JSON
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.Net
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.Net.Web
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.Office
-dotnet pack -c Release --include-source
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" /t:pack /p:Configuration=Release /p:IncludeSymbols=true || exit -1
 
 cd ..\Lsj.Util.Protobuf
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.SQLBuilder
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.Win32
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.WinForm
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
 
 cd ..\Lsj.Util.WPF
-dotnet pack -c Release --include-source
+dotnet pack -c Release --include-source || exit -1
