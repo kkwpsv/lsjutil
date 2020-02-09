e2eb271d42925c48dadbe3fa3105a6cac251862c
Shaojun Li <i@shaojun.li>
2020-02-09 19:00:14 +0800

    Fix comment

--- Src/Lsj.Util.Win32/Marshals/StructPointerOrNullObject.cs
+++ Src/Lsj.Util.Win32/Marshals/StructPointerOrNullObject.cs
@@ -4,14 +4,30 @@ using System.Text;
 
 namespace Lsj.Util.Win32.Marshals
 {
+    /// <summary>
+    /// StructPointerOrNullObject
+    /// </summary>
+    /// <typeparam name="T">struct type</typeparam>
     public class StructPointerOrNullObject<T> where T : struct
     {
+        /// <summary>
+        /// Struct Value
+        /// </summary>
         public T? Value { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
         public StructPointerOrNullObject(T val)
         {
             Value = val;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
         public static implicit operator StructPointerOrNullObject<T>(T val) => new StructPointerOrNullObject<T>(val);
     }
 }
