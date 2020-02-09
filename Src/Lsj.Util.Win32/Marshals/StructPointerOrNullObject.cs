54a4d163df9b410c99b0ad84bb4ec16e1715465d
Shaojun Li <i@shaojun.li>
2020-02-09 18:30:17 +0800

    Add StructPointerOrNullObject

---
+++ Src/Lsj.Util.Win32/Marshals/StructPointerOrNullObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lsj.Util.Win32.Marshals
+{
+    public class StructPointerOrNullObject<T> where T : struct
+    {
+        public T? Value { get; private set; }
+        public StructPointerOrNullObject(T val)
+        {
+            Value = val;
+        }
+
+        public static implicit operator StructPointerOrNullObject<T>(T val) => new StructPointerOrNullObject<T>(val);
+    }
+}
