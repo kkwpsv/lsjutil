62e00742bafbdefe3f1e24866a1269c27875d413
Shaojun Li <i@shaojun.li>
2020-02-10 14:52:04 +0800

    Add AUDIODESCRIPTION

---
+++ Src/Lsj.Util.Win32/Structs/AUDIODESCRIPTION.cs
@@ -0,0 +1,33 @@
+﻿using Lsj.Util.Win32.Enums;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.Structs
+{
+    /// <summary>
+    /// <para>
+    /// Contains information associated with audio descriptions.
+    /// This structure is used with the <see cref="SystemParametersInfo"/> function
+    /// when the <see cref="SystemParametersInfoParameters.SPI_GETAUDIODESCRIPTION"/>
+    /// or <see cref="SystemParametersInfoParameters.SPI_SETAUDIODESCRIPTION"/> action value is specified.
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct AUDIODESCRIPTION
+    {
+        /// <summary>
+        /// The size of the structure, in bytes. The caller must set this member to <code>sizeof(AUDIODESCRIPTION)</code>.
+        /// </summary>
+        public uint cbSize;
+
+        /// <summary>
+        /// If this member is <see langword="true"/>, audio descriptions are enabled; Otherwise, this member is <see langword="false"/>.
+        /// </summary>
+        [MarshalAs(UnmanagedType.Bool)]
+        public bool Enabled;
+
+        /// <summary>
+        /// The locale identifier (LCID) of the language for the audio description. For more information, see Locales and Languages.
+        /// </summary>
+        public uint Locale;
+    }
+}
