1560687ecd6e943c5889f8d4df8923257b2187a4
Shaojun Li <i@shaojun.li>
2020-02-10 22:05:15 +0800

    Add GetModuleHandle

--- Src/Lsj.Util.Win32/Kernel32.cs
+++ Src/Lsj.Util.Win32/Kernel32.cs
@@ -542,6 +542,51 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Retrieves a module handle for the specified module. The module must have been loaded by the calling process.
+        /// To avoid the race conditions described in the Remarks section, use the <see cref="GetModuleHandleEx"/> function.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandlew
+        /// </para>
+        /// </summary>
+        /// <param name="lpModuleName">
+        /// The name of the loaded module (either a .dll or .exe file).
+        /// If the file name extension is omitted, the default library extension .dll is appended.
+        /// The file name string can include a trailing point character (.) to indicate that the module name has no extension.
+        /// The string does not have to specify a path. When specifying a path, be sure to use backslashes (), not forward slashes (/).
+        /// The name is compared (case independently) to the names of modules currently mapped into the address space of the calling process.
+        /// If this parameter is <see langword="null"/>, <see cref="GetModuleHandle"/> returns a handle to the file 
+        /// used to create the calling process (.exe file).
+        /// The <see cref="GetModuleHandle"/> function does not retrieve handles for modules
+        /// that were loaded using the <see cref="LOAD_LIBRARY_AS_DATAFILE"/> flag.
+        /// For more information, see <see cref="LoadLibraryEx"/>.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is a handle to the specified module.
+        /// If the function fails, the return value is <see cref="IntPtr.Zero"/>.
+        /// To get extended error information, call <see cref="Marshal.GetLastWin32Error"/>.
+        /// </returns>
+        /// <remarks>
+        /// The returned handle is not global or inheritable. It cannot be duplicated or used by another process.
+        /// If <paramref name="lpModuleName"/> does not include a path and there is more than one loaded module with the same base name and extension,
+        /// you cannot predict which module handle will be returned.
+        /// To work around this problem, you could specify a path, use side-by-side assemblies, or use <see cref="GetModuleHandleEx"/>
+        /// to specify a memory location rather than a DLL name.
+        /// The <see cref="GetModuleHandle"/> function returns a handle to a mapped module without incrementing its reference count.
+        /// However, if this handle is passed to the FreeLibrary function, the reference count of the mapped module will be decremented.
+        /// Therefore, do not pass a handle returned by <see cref="GetModuleHandle"/> to the <see cref="FreeLibrary"/> function.
+        /// Doing so can cause a DLL module to be unmapped prematurely.
+        /// This function must be used carefully in a multithreaded application.
+        /// There is no guarantee that the module handle remains valid between the time this function returns the handle and the time it is used.
+        /// For example, suppose that a thread retrieves a module handle, but before it uses the handle, a second thread frees the module.
+        /// If the system loads another module, it could reuse the module handle that was recently freed.
+        /// Therefore, the first thread would have a handle to a different module than the one intended.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetModuleHandleW", SetLastError = true)]
+        public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)][In]string lpModuleName);
+
+        /// <summary>
+        /// <para>
         /// Allocates the specified number of bytes from the heap.
         /// </para>
         /// <para>
