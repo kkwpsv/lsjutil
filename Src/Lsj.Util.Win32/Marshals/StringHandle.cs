4c33ce1b3f5533aa65edeb9995a48a76107567f3
Shaojun Li <i@shaojun.li>
2020-02-13 19:00:47 +0800

    Fix comment

--- Src/Lsj.Util.Win32/Marshals/StringHandle.cs
+++ Src/Lsj.Util.Win32/Marshals/StringHandle.cs
@@ -5,25 +5,46 @@ using System.Text;
 
 namespace Lsj.Util.Win32.Marshals
 {
+    /// <summary>
+    /// String Handle
+    /// </summary>
     public class StringHandle : CriticalHandle
     {
         private bool _needRelease = false;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
         public StringHandle(IntPtr val) : this(val, true)
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="needRelease"></param>
         public StringHandle(IntPtr val, bool needRelease) : base(val)
         {
             _needRelease = needRelease;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
         public StringHandle(string val) : this(val, true)
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="needRelease"></param>
         public StringHandle(string val, bool needRelease) : base(IntPtr.Zero)
         {
             if (val != null)
@@ -33,12 +54,22 @@ namespace Lsj.Util.Win32.Marshals
             }
         }
 
+        /// <summary>
+        /// IsRelease
+        /// </summary>
         public override bool IsInvalid => IsClosed;
 
+        /// <summary>
+        /// String Val
+        /// </summary>
         public string StringVal => handle != IntPtr.Zero ? Marshal.PtrToStringUni(handle) : null;
 
         internal IntPtr InternalGetHandle() => handle;
 
+        /// <summary>
+        /// Release Handle
+        /// </summary>
+        /// <returns></returns>
         protected override bool ReleaseHandle()
         {
             if (handle != IntPtr.Zero)
@@ -55,8 +86,16 @@ namespace Lsj.Util.Win32.Marshals
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
         public static implicit operator StringHandle(string val) => new StringHandle(val);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
         public static implicit operator StringHandle(IntPtr val) => new StringHandle(val);
     }
 }
