e181644134b352b7e4d482562a6c468980e0f385
Shaojun Li <i@shaojun.li>
2020-02-14 14:51:18 +0800

    Add DLGPROC, CreateDialog and CreateDialogParam

--- Src/Lsj.Util.Win32/User32.cs
+++ Src/Lsj.Util.Win32/User32.cs
@@ -79,6 +79,54 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Application-defined callback function used with the <see cref="CreateDialog"/> and <see cref="DialogBox"/> families of functions.
+        /// It processes messages sent to a modal or modeless dialog box.
+        /// The <see cref="DLGPROC"/> type defines a pointer to this callback function.
+        /// DialogProc is a placeholder for the application-defined function name.
+        /// Typically, the dialog box procedure should return <code>(IntPtr)1</code> if it processed the message,
+        /// and <see cref="IntPtr.Zero"/> if it did not.
+        /// If the dialog box procedure returns <see cref="IntPtr.Zero"/>,
+        /// the dialog manager performs the default dialog operation in response to the message.
+        /// If the dialog box procedure processes a message that requires a specific return value,
+        /// the dialog box procedure should set the desired return value by
+        /// calling <code>SetWindowLong(hwndDlg, DWL_MSGRESULT, lResult)</code> immediately before returning <code>(IntPtr)1</code>.
+        /// Note that you must call <see cref="SetWindowLong"/> immediately before returning <code>(IntPtr)1</code>;
+        /// doing so earlier may result in the <see cref="DWL_MSGRESULT"/> value being overwritten by a nested dialog box message.
+        /// The following messages are exceptions to the general rules stated above.
+        /// Consult the documentation for the specific message for details on the semantics of the return value.
+        /// <see cref="WindowsMessages.WM_CHARTOITEM"/>, <see cref="WindowsMessages.WM_COMPAREITEM"/>, <see cref="WindowsMessages.WM_CTLCOLORBTN"/>,
+        /// <see cref="WindowsMessages.WM_CTLCOLORDLG"/>, <see cref="WindowsMessages.WM_CTLCOLOREDIT"/>, <see cref="WindowsMessages.WM_CTLCOLORLISTBOX"/>,
+        /// <see cref="WindowsMessages.WM_CTLCOLORSCROLLBAR"/>, <see cref="WindowsMessages.WM_CTLCOLORSTATIC"/>,
+        /// <see cref="WindowsMessages.WM_INITDIALOG"/>, <see cref="WindowsMessages.WM_QUERYDRAGICON"/>, <see cref="WM_VKEYTOITEM"/>.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nc-winuser-dlgproc
+        /// </para>
+        /// </summary>
+        /// <param name="Arg1">
+        /// A handle to the dialog box.
+        /// </param>
+        /// <param name="Arg2">
+        /// The message.
+        /// </param>
+        /// <param name="Arg3">
+        /// Additional message-specific information.
+        /// </param>
+        /// <param name="Arg4">
+        /// Additional message-specific information.
+        /// </param>
+        /// <returns></returns>
+        /// <remarks>
+        /// You should use the dialog box procedure only if you use the dialog box class for the dialog box.
+        /// This is the default class and is used when no explicit class is specified in the dialog box template.
+        /// Although the dialog box procedure is similar to a window procedure,
+        /// it must not call the <see cref="DefWindowProc"/> function to process unwanted messages.
+        /// Unwanted messages are processed internally by the dialog box window procedure.
+        /// </remarks>
+        public delegate IntPtr DLGPROC([In]IntPtr Arg1, [In]WindowsMessages Arg2, [In]UIntPtr Arg3, [In]IntPtr Arg4);
+
+        /// <summary>
+        /// <para>
         /// An application-defined function that processes messages sent to a window.
         /// The <see cref="WNDPROC"/> type defines a pointer to this callback function.
         /// WindowProc is a placeholder for the application-defined function name.
@@ -274,6 +322,101 @@ namespace Lsj.Util.Win32
         [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "CharToOemW", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool CharToOem([MarshalAs(UnmanagedType.LPWStr)][In]string pSrc, [In]IntPtr pDst);
+
+        /// <summary>
+        /// <para>
+        /// Creates a modeless dialog box from a dialog box template resource.
+        /// The <see cref="CreateDialog"/> macro uses the <see cref="CreateDialogParam"/> function.
+        /// </para>
+        /// </summary>
+        /// <param name="hInstance">
+        /// A handle to the module which contains the dialog box template.
+        /// If this parameter is <see cref="IntPtr.Zero"/>, then the current executable is used.
+        /// </param>
+        /// <param name="lpName">
+        /// The dialog box template.
+        /// This parameter is either the pointer to a null-terminated character string that
+        /// specifies the name of the dialog box template or
+        /// an integer value that specifies the resource identifier of the dialog box template.
+        /// If the parameter specifies a resource identifier, its high-order word must be zero and its low-order word must contain the identifier.
+        /// You can use the <see cref="MAKEINTRESOURCE"/> macro to create this value.
+        /// </param>
+        /// <param name="hWndParent">
+        /// A handle to the window that owns the dialog box.
+        /// </param>
+        /// <param name="lpDialogFunc">
+        /// A pointer to the dialog box procedure. For more information about the dialog box procedure, see DialogProc.
+        /// </param>
+        /// <returns></returns>
+        /// <remarks>
+        /// The <see cref="CreateDialog"/> function uses the <see cref="CreateWindowEx"/> function to create the dialog box.
+        /// <see cref="CreateDialog"/> then sends a <see cref="WindowsMessages.WM_INITDIALOG"/> message
+        /// (and a <see cref="WindowsMessages.WM_SETFONT"/> message if the template specifies
+        /// the <see cref="DS_SETFONT"/> or <see cref="DS_SHELLFONT"/> style) to the dialog box procedure.
+        /// The function displays the dialog box if the template specifies the <see cref="WindowStyles.WS_VISIBLE"/> style.
+        /// Finally, <see cref="CreateDialog"/> returns the window handle to the dialog box.
+        /// After <see cref="CreateDialog"/> returns, the application displays the dialog box
+        /// (if it is not already displayed) by using the <see cref="ShowWindow"/> function.
+        /// The application destroys the dialog box by using the <see cref="DestroyWindow"/> function.
+        /// To support keyboard navigation and other dialog box functionality,
+        /// the message loop for the dialog box must call the <see cref="IsDialogMessage"/> function.
+        /// </remarks>
+        public static IntPtr CreateDialog(IntPtr hInstance, StringOrIntPtrObject lpName, IntPtr hWndParent, DLGPROC lpDialogFunc) =>
+            CreateDialogParam(hInstance, lpName, hWndParent, lpDialogFunc, IntPtr.Zero);
+
+        /// <summary>
+        /// <para>
+        /// Creates a modeless dialog box from a dialog box template resource.
+        /// Before displaying the dialog box, the function passes an application-defined value to the dialog box procedure
+        /// as the lParam parameter of the <see cref="WindowsMessages.WM_INITDIALOG"/> message.
+        /// An application can use this value to initialize dialog box controls.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-createdialogparamw
+        /// </para>
+        /// </summary>
+        /// <param name="hInstance">
+        /// A handle to the module which contains the dialog box template.
+        /// If this parameter is <see cref="IntPtr.Zero"/>, then the current executable is used.
+        /// </param>
+        /// <param name="lpTemplateName">
+        /// The dialog box template.
+        /// This parameter is either the pointer to a null-terminated character string that specifies the name of the dialog box template
+        /// or an integer value that specifies the resource identifier of the dialog box template.
+        /// If the parameter specifies a resource identifier, its high-order word must be zero and low-order word must contain the identifier.
+        /// You can use the <see cref="MAKEINTRESOURCE"/> macro to create this value.
+        /// </param>
+        /// <param name="hWndParent">
+        /// A handle to the window that owns the dialog box.
+        /// </param>
+        /// <param name="lpDialogFunc">
+        /// A pointer to the dialog box procedure.
+        /// For more information about the dialog box procedure, see <see cref="DLGPROC"/>.
+        /// </param>
+        /// <param name="dwInitParam">
+        /// The value to be passed to the dialog box procedure in the lParam parameter in the <see cref="WindowsMessages.WM_INITDIALOG"/> message.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the window handle to the dialog box.
+        /// If the function fails, the return value is <see cref="IntPtr.Zero"/>.
+        /// To get extended error information, call <see cref="Marshal.GetLastWin32Error"/>.
+        /// </returns>
+        /// <remarks>
+        /// The <see cref="CreateDialogParam"/> function uses the <see cref="CreateWindowEx"/> function to create the dialog box.
+        /// <see cref="CreateDialogParam"/> then sends a <see cref="WindowsMessages.WM_INITDIALOG"/> message
+        /// (and a <see cref="WindowsMessages.WM_SETFONT"/> message if the template specifies
+        /// the <see cref="DS_SETFONT"/> or <see cref="DS_SHELLFONT"/> style) to the dialog box procedure.
+        /// The function displays the dialog box if the template specifies the <see cref="WindowStyles.WS_VISIBLE"/> style.
+        /// Finally, <see cref="CreateDialogParam"/> returns the window handle of the dialog box.
+        /// After <see cref="CreateDialogParam"/> returns, the application displays the dialog box
+        /// (if it is not already displayed) using the <see cref="ShowWindow"/> function.
+        /// The application destroys the dialog box by using the <see cref="DestroyWindow"/> function.
+        /// To support keyboard navigation and other dialog box functionality,
+        /// the message loop for the dialog box must call the <see cref="IsDialogMessage"/> function.
+        /// </remarks>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "CreateDialogParamW", SetLastError = true)]
+        public static extern IntPtr CreateDialogParam([In]IntPtr hInstance, [In]StringOrIntPtrObject lpTemplateName,
+            [In]IntPtr hWndParent, [In]DLGPROC lpDialogFunc, [In]IntPtr dwInitParam);
 
         /// <summary>
         /// <para>
