b042667dc7a779679c492485253bbe8e76f98fe4
Shaojun Li <i@shaojun.li>
2020-02-15 20:33:37 +0800

    Add GetModuleHandleEx

--- Src/Lsj.Util.Win32/Kernel32.cs
+++ Src/Lsj.Util.Win32/Kernel32.cs
@@ -1500,6 +1500,68 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Retrieves a module handle for the specified module and increments the module's reference count
+        /// unless <see cref="GetModuleHandleExFlags.GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT"/> is specified.
+        /// The module must have been loaded by the calling process.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandleexw
+        /// </para>
+        /// </summary>
+        /// <param name="dwFlags">
+        /// This parameter can be zero or one or more of the following values.
+        /// If the module's reference count is incremented, the caller must use the <see cref="FreeLibrary"/> function
+        /// to decrement the reference count when the module handle is no longer needed.
+        /// </param>
+        /// <param name="lpModuleName">
+        /// The name of the loaded module (either a .dll or .exe file), or an address in the module
+        /// (if <paramref name="dwFlags"/> is <see cref="GetModuleHandleExFlags.GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS"/>).
+        /// For a module name, if the file name extension is omitted, the default library extension .dll is appended.
+        /// The file name string can include a trailing point character (.) to indicate that the module name has no extension.
+        /// The string does not have to specify a path.
+        /// When specifying a path, be sure to use backslashes (), not forward slashes (/).
+        /// The name is compared (case independently) to the names of modules currently mapped into the address space of the calling process.
+        /// If this parameter is <see langword="null"/>, the function returns a handle to the file used to create the calling process (.exe file).
+        /// </param>
+        /// <param name="phModule">
+        /// A handle to the specified module.
+        /// If the function fails, this parameter is <see cref="IntPtr.Zero"/>.
+        /// The <see cref="GetModuleHandleEx"/> function does not retrieve handles for modules that were loaded
+        /// using the <see cref="LOAD_LIBRARY_AS_DATAFILE"/> flag.
+        /// For more information, see <see cref="LoadLibraryEx"/>.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see langword="true"/>.
+        /// If the function fails, the return value is <see langword="false"/>.
+        /// To get extended error information, see <see cref="Marshal.GetLastWin32Error"/>.
+        /// </returns>
+        /// <remarks>
+        /// The handle returned is not global or inheritable. It cannot be duplicated or used by another process.
+        /// If <paramref name="lpModuleName"/> does not include a path and there is more than one loaded module with the same base name and extension,
+        /// you cannot predict which module handle will be returned.
+        /// To work around this problem, you could specify a path, use side-by-side assemblies, or specify a memory location rather than a DLL name
+        /// in the <paramref name="lpModuleName"/> parameter.
+        /// If <paramref name="dwFlags"/> contains <see cref="GetModuleHandleExFlags.GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT"/>,
+        /// the <see cref="GetModuleHandleEx"/> function returns a handle to a mapped module without incrementing its reference count.
+        /// However, if this handle is passed to the FreeLibrary function, the reference count of the mapped module will be decremented.
+        /// Therefore, do not pass a handle returned by <see cref="GetModuleHandleEx"/>
+        /// with <see cref="GetModuleHandleExFlags.GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT"/> to the <see cref="FreeLibrary"/> function.
+        /// Doing so can cause a DLL module to be unmapped prematurely.
+        /// If <paramref name="dwFlags"/> contains <see cref="GetModuleHandleExFlags.GET_MODULE_HANDLE_EX_UNCHANGED_REFCOUNT"/>,
+        /// this function must be used carefully in a multithreaded application.
+        /// There is no guarantee that the module handle remains valid between the time this function returns the handle and the time it is used.
+        /// For example, a thread retrieves a module handle, but before it uses the handle, a second thread frees the module.
+        /// If the system loads another module, it could reuse the module handle that was recently freed.
+        /// Therefore, first thread would have a handle to a module different than the one intended.
+        /// To compile an application that uses this function, define _WIN32_WINNT as 0x0501 or later. For more information, see Using the Windows Headers.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetModuleHandleExW", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetModuleHandleEx([In]uint dwFlags, [MarshalAs(UnmanagedType.LPWStr)][In]string lpModuleName,
+            [Out]out IntPtr phModule);
+
+        /// <summary>
+        /// <para>
         /// Allocates the specified number of bytes from the heap.
         /// </para>
         /// <para>
