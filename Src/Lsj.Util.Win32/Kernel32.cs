fbc21c1348a69230eefb3d8cba379b5596a431c4
Shaojun Li <i@shaojun.li>
2020-02-18 16:19:27 +0800

    Add AssignProcessToJobObject

--- Src/Lsj.Util.Win32/Kernel32.cs
+++ Src/Lsj.Util.Win32/Kernel32.cs
@@ -85,6 +85,86 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Assigns a process to an existing job object.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/jobapi2/nf-jobapi2-assignprocesstojobobject
+        /// </para>
+        /// </summary>
+        /// <param name="hJob">
+        /// A handle to the job object to which the process will be associated.
+        /// The <see cref="CreateJobObject"/> or <see cref="OpenJobObject"/> function returns this handle.
+        /// The handle must have the <see cref="JOB_OBJECT_ASSIGN_PROCESS"/> access right.
+        /// For more information, see Job Object Security and Access Rights.
+        /// </param>
+        /// <param name="hProcess">
+        /// A handle to the process to associate with the job object.
+        /// The handle must have the <see cref="PROCESS_SET_QUOTA"/> and <see cref="PROCESS_TERMINATE"/> access rights.
+        /// For more information, see Process Security and Access Rights.
+        /// If the process is already associated with a job, the job specified by hJob must be empty or it must be
+        /// in the hierarchy of nested jobs to which the process already belongs,
+        /// and it cannot have UI limits set (<see cref="SetInformationJobObject"/> with <see cref="JobObjectBasicUIRestrictions"/>).
+        /// For more information, see Remarks.
+        /// Windows 7, Windows Server 2008 R2, Windows XP with SP3, Windows Server 2008, Windows Vista and Windows Server 2003:
+        /// The process must not already be assigned to a job; if it is, the function fails with <see cref="SystemErrorCodes.ERROR_ACCESS_DENIED"/>.
+        /// This behavior changed starting in Windows 8 and Windows Server 2012.
+        /// Terminal Services:  All processes within a job must run within the same session as the job.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see langword="true"/>.
+        /// If the function fails, the return value is <see langword="false"/>.
+        /// To get extended error information, call <see cref="Marshal.GetLastWin32Error"/>.
+        /// </returns>
+        /// <remarks>
+        /// After you associate a process with a job object using <see cref="AssignProcessToJobObject"/>,
+        /// the process is subject to the limits set for the job.
+        /// To set limits for a job, use the <see cref="SetInformationJobObject"/> function.
+        /// If the job has a user-mode time limit, and the time limit has been exhausted,
+        /// <see cref="AssignProcessToJobObject"/> fails and the specified process is terminated.
+        /// If the time limit would be exceeded by associating the process, <see cref="AssignProcessToJobObject"/> still succeeds.
+        /// However, the time limit violation will be reported.
+        /// If the job has an active process limit, and the limit would be exceeded by associating this process,
+        /// <see cref="AssignProcessToJobObject"/> fails, and the specified process is terminated.
+        /// Memory operations performed by a process associated with a job that has a memory limit are subject to the memory limit.
+        /// Memory operations performed by the process before it was associated with the job are not examined by <see cref="AssignProcessToJobObject"/>.
+        /// If the process is already running and the job has security limitations, <see cref="AssignProcessToJobObject"/> may fail.
+        /// For example, if the primary token of the process contains the local administrators group,
+        /// but the job object has the security limitation <see cref="JOB_OBJECT_SECURITY_NO_ADMIN"/>, the function fails.
+        /// If the job has the security limitation <see cref="JOB_OBJECT_SECURITY_ONLY_TOKEN"/>, the process must be created suspended.
+        /// To create a suspended process, call the <see cref="CreateProcess"/> function with the <see cref="ProcessCreationFlags.CREATE_SUSPENDED"/> flag.
+        /// A process can be associated with more than one job in a hierarchy of nested jobs.
+        /// For priority class, affinity, commit charge, per-process execution time limit, scheduling class limit, and working set minimum and maximum,
+        /// the process inherits an effective limit which is the most restrictive limit of all the jobs in its parent job chain.
+        /// For other resource limits, the process inherits limits from its immediate job in the hierarchy.
+        /// Accounting information is added to the immediate job and aggregated in each parent job in the job chain.
+        /// By default, all child processes are associated with the immediate job and every job in the parent job chain.
+        /// To create a child process that is not part of the same job chain, call the <see cref="CreateProcess"/> function 
+        /// with the <see cref="ProcessCreationFlags.CREATE_BREAKAWAY_FROM_JOB"/> flag.
+        /// The child process breaks away from every job in the job chain unless a job in the chain does not allow breakaway.
+        /// In this case, the child process does not break away from that job or any job above it in the job chain.
+        /// For more information, see Nested Jobs.
+        /// Windows 7, Windows Server 2008 R2, Windows XP with SP3, Windows Server 2008, Windows Vista and Windows Server 2003:
+        /// A process can be associated only with a single job.
+        /// A process inherits limits from the job it is associated with and adds its accounting information to the job.
+        /// If a process is associated with a job, all child processes it creates are associated with that job by default.
+        /// To create a child process that is not part of the same job, call the <see cref="CreateProcess"/> function
+        /// with the <see cref="ProcessCreationFlags.CREATE_BREAKAWAY_FROM_JOB"/> flag.
+        /// A process can be associated with more than one job starting in Windows 8 and Windows Server 2012.
+        /// Windows 7, Windows Server 2008 R2, Windows Server 2008 and Windows Vista:
+        /// If the process is being monitored by the Program Compatibility Assistant (PCA), it is placed into a compatibility job.
+        /// Therefore, the process must be created using <see cref="ProcessCreationFlags.CREATE_BREAKAWAY_FROM_JOB"/> before it can be placed in another job.
+        /// Alternatively, you can embed an application manifest that specifies a User Account Control (UAC) level in your application
+        /// and PCA will not add the process to the compatibility job.
+        /// For more information, see Application Development Requirements for User Account Control Compatibility.
+        /// If the job or any of its parent jobs in the job chain is terminating when <see cref="AssignProcessToJobObject"/> is called, the function fails.
+        /// To compile an application that uses this function, define _WIN32_WINNT as 0x0500 or later. For more information, see Using the Windows Headers.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "AssignProcessToJobObject", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AssignProcessToJobObject([In]IntPtr hJob, [In]IntPtr hProcess);
+
+        /// <summary>
+        /// <para>
         /// Attaches the calling process to the console of the specified process.
         /// </para>
         /// <para>
