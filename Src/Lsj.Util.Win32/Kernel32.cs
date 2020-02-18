e032ad72ee587dad55850ac84af8bbd96f8f99ab
Shaojun Li <i@shaojun.li>
2020-02-18 18:35:46 +0800

    Add CreateThread

--- Src/Lsj.Util.Win32/Kernel32.cs
+++ Src/Lsj.Util.Win32/Kernel32.cs
@@ -3438,7 +3438,7 @@ namespace Lsj.Util.Win32
         public static extern IntPtr CreateRemoteThread([In]IntPtr hProcess,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(StructPointerOrNullObjectMarshaler<SECURITY_ATTRIBUTES>))][In]StructPointerOrNullObject<SECURITY_ATTRIBUTES> lpThreadAttributes,
             [In]UIntPtr dwStackSize, [MarshalAs(UnmanagedType.FunctionPtr)][In]ThreadProc lpStartAddress, [In]IntPtr lpParameter,
-            [In]uint dwCreationFlags, [Out]out uint lpThreadId);
+            [In]ThreadCreationFlags dwCreationFlags, [Out]out uint lpThreadId);
 
         /// <summary>
         /// <para>
@@ -3537,7 +3537,7 @@ namespace Lsj.Util.Win32
         public static extern IntPtr CreateRemoteThreadEx([In]IntPtr hProcess,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(StructPointerOrNullObjectMarshaler<SECURITY_ATTRIBUTES>))][In]StructPointerOrNullObject<SECURITY_ATTRIBUTES> lpThreadAttributes,
             [In]UIntPtr dwStackSize, [MarshalAs(UnmanagedType.FunctionPtr)][In]ThreadProc lpStartAddress, [In]IntPtr lpParameter,
-            [In]uint dwCreationFlags, [In]IntPtr lpAttributeList, [Out]out uint lpThreadId);
+            [In]ThreadCreationFlags dwCreationFlags, [In]IntPtr lpAttributeList, [Out]out uint lpThreadId);
 
         /// <summary>
         /// <para>
@@ -3804,6 +3804,105 @@ namespace Lsj.Util.Win32
         public static extern IntPtr CreateSemaphoreEx(
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(StructPointerOrNullObjectMarshaler<SECURITY_ATTRIBUTES>))][In]StructPointerOrNullObject<SECURITY_ATTRIBUTES> lpSemaphoreAttributes,
             [In]int lInitialCount, [In]int lMaximumCount, [MarshalAs(UnmanagedType.LPWStr)][In]string lpName, [In]uint dwFlags, [In]uint dwDesiredAccess);
+
+        /// <summary>
+        /// <para>
+        /// Creates a thread to execute within the virtual address space of the calling process.
+        /// To create a thread that runs in the virtual address space of another process, use the <see cref="CreateRemoteThread"/> function.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/processthreadsapi/nf-processthreadsapi-createthread
+        /// </para>
+        /// </summary>
+        /// <param name="lpThreadAttributes">
+        /// A pointer to a <see cref="SECURITY_ATTRIBUTES"/> structure that determines whether the returned handle can be inherited by child processes.
+        /// If <paramref name="lpThreadAttributes"/> is <see langword="null"/>, the handle cannot be inherited.
+        /// The <see cref="SECURITY_ATTRIBUTES.lpSecurityDescriptor"/> member of the structure specifies a security descriptor for the new thread.
+        /// If <paramref name="lpThreadAttributes"/> is <see langword="null"/>, the thread gets a default security descriptor.
+        /// The ACLs in the default security descriptor for a thread come from the primary token of the creator.
+        /// </param>
+        /// <param name="dwStackSize">
+        /// The initial size of the stack, in bytes.
+        /// The system rounds this value to the nearest page.
+        /// If this parameter is zero, the new thread uses the default size for the executable.
+        /// For more information, see Thread Stack Size.
+        /// </param>
+        /// <param name="lpStartAddress">
+        /// A pointer to the application-defined function to be executed by the thread.
+        /// This pointer represents the starting address of the thread. For more information on the thread function, see <see cref="ThreadProc"/>.
+        /// </param>
+        /// <param name="lpParameter">
+        /// A pointer to a variable to be passed to the thread.
+        /// </param>
+        /// <param name="dwCreationFlags">
+        /// The flags that control the creation of the thread.
+        /// 0: The thread runs immediately after creation.
+        /// <see cref="ThreadCreationFlags.CREATE_SUSPENDED"/>, <see cref="ThreadCreationFlags.STACK_SIZE_PARAM_IS_A_RESERVATION"/>
+        /// </param>
+        /// <param name="lpThreadId">
+        /// A pointer to a variable that receives the thread identifier.
+        /// If this parameter is <see langword="null"/>, the thread identifier is not returned.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is a handle to the new thread.
+        /// If the function fails, the return value is <see cref="IntPtr.Zero"/>.
+        /// To get extended error information, call <see cref="Marshal.GetLastWin32Error"/>.
+        /// Note that <see cref="CreateThread"/> may succeed even if <paramref name="lpStartAddress"/> points to data, code, or is not accessible.
+        /// If the start address is invalid when the thread runs, an exception occurs, and the thread terminates.
+        /// Thread termination due to a invalid start address is handled as an error exit for the thread's process.
+        /// This behavior is similar to the asynchronous nature of <see cref="CreateProcess"/>, where the process is created
+        /// even if it refers to invalid or missing dynamic-link libraries (DLLs).
+        /// </returns>
+        /// <remarks>
+        /// The number of threads a process can create is limited by the available virtual memory.
+        /// By default, every thread has one megabyte of stack space.
+        /// Therefore, you can create at most 2,048 threads.
+        /// If you reduce the default stack size, you can create more threads.
+        /// However, your application will have better performance if you create one thread per processor
+        /// and build queues of requests for which the application maintains the context information.
+        /// A thread would process all requests in a queue before processing requests in the next queue.
+        /// The new thread handle is created with the <see cref="THREAD_ALL_ACCESS"/> access right.
+        /// If a security descriptor is not provided when the thread is created, a default security descriptor is constructed for the new thread
+        /// using the primary token of the process that is creating the thread.
+        /// When a caller attempts to access the thread with the <see cref="OpenThread"/> function,
+        /// the effective token of the caller is evaluated against this security descriptor to grant or deny access.
+        /// The newly created thread has full access rights to itself when calling the <see cref="GetCurrentThread"/> function.
+        /// Windows Server 2003:  The thread's access rights to itself are computed by evaluating the primary token of the process
+        /// in which the thread was created against the default security descriptor constructed for the thread.
+        /// If the thread is created in a remote process, the primary token of the remote process is used.
+        /// As a result, the newly created thread may have reduced access rights to itself when calling <see cref="GetCurrentThread"/>.
+        /// Some access rights including <see cref="THREAD_SET_THREAD_TOKEN"/> and <see cref="THREAD_GET_CONTEXT"/> may not be present,
+        /// leading to unexpected failures.
+        /// For this reason, creating a thread while impersonating another user is not recommended.
+        /// If the thread is created in a runnable state (that is, if the <see cref="ThreadCreationFlags.CREATE_SUSPENDED"/> flag is not used),
+        /// the thread can start running before CreateThread returns and, in particular,
+        /// before the caller receives the handle and identifier of the created thread.
+        /// The thread execution begins at the function specified by the <paramref name="lpStartAddress"/> parameter.
+        /// If this function returns, the DWORD return value is used to terminate the thread in an implicit call to the <see cref="ExitThread"/> function.
+        /// Use the <see cref="GetExitCodeThread"/> function to get the thread's return value.
+        /// The thread is created with a thread priority of <see cref="THREAD_PRIORITY_NORMAL"/>.
+        /// Use the <see cref="GetThreadPriority"/> and <see cref="SetThreadPriority"/> functions to get and set the priority value of a thread.
+        /// When a thread terminates, the thread object attains a signaled state, satisfying any threads that were waiting on the object.
+        /// The thread object remains in the system until the thread has terminated and all handles
+        /// to it have been closed through a call to <see cref="CloseHandle"/>.
+        /// The <see cref="ExitProcess"/>, <see cref="ExitThread"/>, <see cref="CreateThread"/>, <see cref="CreateRemoteThread"/> functions,
+        /// and a process that is starting(as the result of a call by <see cref="CreateProcess"/>) are serialized between each other within a process.
+        /// Only one of these events can happen in an address space at a time.
+        /// This means that the following restrictions hold:
+        /// During process startup and DLL initialization routines, new threads can be created,
+        /// but they do not begin execution until DLL initialization is done for the process.
+        /// Only one thread in a process can be in a DLL initialization or detach routine at a time.
+        /// <see cref="ExitProcess"/> does not complete until there are no threads in their DLL initialization or detach routines.
+        /// A thread in an executable that calls the C run - time library(CRT) should use the _beginthreadex and _endthreadex functions
+        /// for thread management rather than <see cref="CreateThread"/> and <see cref="ExitThread"/>;
+        /// this requires the use of the multithreaded version of the CRT.
+        /// If a thread created using <see cref="CreateThread"/> calls the CRT, the CRT may terminate the process in low - memory conditions.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "CreateThread", SetLastError = true)]
+        public static extern IntPtr CreateThread(
+            [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(StructPointerOrNullObjectMarshaler<SECURITY_ATTRIBUTES>))][In]StructPointerOrNullObject<SECURITY_ATTRIBUTES> lpThreadAttributes,
+            [In]UIntPtr dwStackSize, [MarshalAs(UnmanagedType.FunctionPtr)][In] ThreadProc lpStartAddress, [In]IntPtr lpParameter,
+            [In]ThreadCreationFlags dwCreationFlags, [Out]out uint lpThreadId);
 
         /// <summary>
         /// <para>
