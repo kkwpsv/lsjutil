3b8ced9d4938611f131ae49ca59f937ddba42ee8
Shaojun Li <i@shaojun.li>
2020-02-21 17:29:27 +0800

    Add FileTimeToSystemTime

--- Src/Lsj.Util.Win32/Kernel32.cs
+++ Src/Lsj.Util.Win32/Kernel32.cs
@@ -1,4 +1,7 @@
-﻿namespace Lsj.Util.Win32
+﻿using Lsj.Util.Win32.Structs;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32
 {
     /// <summary>
     /// Kernel32.dll
@@ -10,5 +13,28 @@
         /// </summary>
         public const uint NUMA_NO_PREFERRED_NODE = 0xffffffff;
 
+        /// <summary>
+        /// <para>
+        /// Converts a file time to system time format. System time is based on Coordinated Universal Time (UTC).
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/timezoneapi/nf-timezoneapi-filetimetosystemtime
+        /// </para>
+        /// </summary>
+        /// <param name="lpFileTime">
+        /// A pointer to a <see cref="FILETIME"/> structure containing the file time to be converted to system (UTC) date and time format.
+        /// This value must be less than 0x8000000000000000. Otherwise, the function fails.
+        /// </param>
+        /// <param name="lpSystemTime">
+        /// A pointer to a <see cref="SYSTEMTIME"/> structure to receive the converted file time.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see langword="true"/>.
+        /// If the function fails, the return value is <see langword="false"/>.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "FileTimeToSystemTime ", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool FileTimeToSystemTime([In]ref FILETIME lpFileTime, [In][Out]ref SYSTEMTIME lpSystemTime);
     }
 }
