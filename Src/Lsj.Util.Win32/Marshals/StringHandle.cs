041d1867e9c35e39885e7f6381e19a1e4f6ef38b
Shaojun Li <i@shaojun.li>
2020-02-22 14:03:00 +0800

    Better comments

--- Src/Lsj.Util.Win32/Marshals/StringHandle.cs
+++ Src/Lsj.Util.Win32/Marshals/StringHandle.cs
@@ -6,45 +6,53 @@ using System.Text;
 namespace Lsj.Util.Win32.Marshals
 {
     /// <summary>
-    /// String Handle
+    /// String Handle For P/Invoke
     /// </summary>
     public class StringHandle : CriticalHandle
     {
         private bool _needRelease = false;
 
         /// <summary>
-        /// 
+        /// Initialize a <see cref="StringHandle"/> with <see cref="IntPtr.Zero"/>
         /// </summary>
-        /// <param name="val"></param>
-        public StringHandle(IntPtr val) : this(val, true)
+        public StringHandle() : this(IntPtr.Zero, false)
         {
 
         }
 
         /// <summary>
-        /// 
+        /// Initialize a <see cref="StringHandle"/> with a <see cref="IntPtr"/> which is not need to release.
         /// </summary>
-        /// <param name="val"></param>
-        /// <param name="needRelease"></param>
+        /// <param name="val">The <see cref="IntPtr"/> value.</param>
+        public StringHandle(IntPtr val) : this(val, false)
+        {
+
+        }
+
+        /// <summary>
+        /// Initialize a <see cref="StringHandle"/> with a <see cref="IntPtr"/>.
+        /// </summary>
+        /// <param name="val">The <see cref="IntPtr"/> value.</param>
+        /// <param name="needRelease">If need to release this handle.</param>
         public StringHandle(IntPtr val, bool needRelease) : base(val)
         {
             _needRelease = needRelease;
         }
 
         /// <summary>
-        /// 
+        /// Initialize a <see cref="StringHandle"/> with a <see cref="string"/> which is need to release.
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="val">The <see cref="string"/> value.</param>
         public StringHandle(string val) : this(val, true)
         {
 
         }
 
         /// <summary>
-        /// 
+        /// Initialize a <see cref="StringHandle"/> with a <see cref="string"/>.
         /// </summary>
-        /// <param name="val"></param>
-        /// <param name="needRelease"></param>
+        /// <param name="val">The <see cref="string"/> value.</param>
+        /// <param name="needRelease">If need to release this handle.</param>
         public StringHandle(string val, bool needRelease) : base(IntPtr.Zero)
         {
             if (val != null)
@@ -54,30 +62,27 @@ namespace Lsj.Util.Win32.Marshals
             }
         }
 
-        /// <summary>
-        /// IsRelease
-        /// </summary>
+        /// <inheritdoc/>
         public override bool IsInvalid => IsClosed;
 
         /// <summary>
-        /// String Val
+        /// If need to release this handle.
+        /// </summary>
+        public bool NeedRelease => _needRelease;
+
+        /// <summary>
+        /// The <see cref="string"/> val
         /// </summary>
         public string StringVal => handle != IntPtr.Zero ? Marshal.PtrToStringUni(handle) : null;
 
         internal IntPtr InternalGetHandle() => handle;
 
-        /// <summary>
-        /// Release Handle
-        /// </summary>
-        /// <returns></returns>
+        /// <inheritdoc/>
         protected override bool ReleaseHandle()
         {
-            if (handle != IntPtr.Zero)
+            if (handle != IntPtr.Zero && _needRelease)
             {
-                if (_needRelease)
-                {
-                    Marshal.FreeHGlobal(handle);
-                }
+                Marshal.FreeHGlobal(handle);
                 return true;
             }
             else
@@ -86,14 +91,16 @@ namespace Lsj.Util.Win32.Marshals
             }
         }
 
+        public override string ToString() => StringVal ?? string.Empty;
+
         /// <summary>
-        /// 
+        /// Return a <see cref="StringHandle"/> with a <see cref="string"/> which is need to release.
         /// </summary>
         /// <param name="val"></param>
         public static implicit operator StringHandle(string val) => new StringHandle(val);
 
         /// <summary>
-        /// 
+        /// Return a <see cref="StringHandle"/> with a <see cref="IntPtr"/> which is not need to release.
         /// </summary>
         /// <param name="val"></param>
         public static implicit operator StringHandle(IntPtr val) => new StringHandle(val);
