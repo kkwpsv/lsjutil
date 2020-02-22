148f53ef486d79c22c0c9151b4de5249b151e18d
Shaojun Li <i@shaojun.li>
2020-02-22 22:39:34 +0800

    Add GetCurrentProcess

--- Src/Lsj.Util.Win32/Kernel32.Process.cs
+++ Src/Lsj.Util.Win32/Kernel32.Process.cs
@@ -1046,6 +1046,37 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Retrieves a pseudo handle for the current process.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentprocess
+        /// </para>
+        /// </summary>
+        /// <returns>
+        /// The return value is a pseudo handle to the current process.
+        /// </returns>
+        /// <remarks>
+        /// A pseudo handle is a special constant, currently -1, that is interpreted as the current process handle.
+        /// For compatibility with future operating systems, it is best to call <see cref="GetCurrentProcess"/> instead of hard-coding this constant value.
+        /// The calling process can use a pseudo handle to specify its own process whenever a process handle is required.
+        /// Pseudo handles are not inherited by child processes.
+        /// This handle has the <see cref="PROCESS_ALL_ACCESS"/> access right to the process object.
+        /// For more information, see Process Security and Access Rights.
+        /// Windows Server 2003 and Windows XP:
+        /// This handle has the maximum access allowed by the security descriptor of the process to the primary token of the process.
+        /// A process can create a "real" handle to itself that is valid in the context of other processes,
+        /// or that can be inherited by other processes,
+        /// by specifying the pseudo handle as the source handle in a call to the <see cref="DuplicateHandle"/> function.
+        /// A process can also use the <see cref="OpenProcess"/> function to open a real handle to itself.
+        /// The pseudo handle need not be closed when it is no longer needed.
+        /// Calling the <see cref="CloseHandle"/> function with a pseudo handle has no effect.
+        /// If the pseudo handle is duplicated by <see cref="DuplicateHandle"/>, the duplicate handle must be closed.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetCurrentProcess", SetLastError = true)]
+        public static extern IntPtr GetCurrentProcess();
+
+        /// <summary>
+        /// <para>
         /// Retrieves the contents of the <see cref="STARTUPINFO"/> structure that was specified when the calling process was created.
         /// </para>
         /// <para>
