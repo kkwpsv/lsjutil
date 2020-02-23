7b1c4fb70ed6a556d18c99fb10a8bf8e0bcfc4a9
Shaojun Li <i@shaojun.li>
2020-02-23 20:37:48 +0800

    Add GetProcessImageFileName

--- Src/Lsj.Util.Win32/Kernel32.Process.cs
+++ Src/Lsj.Util.Win32/Kernel32.Process.cs
@@ -9,6 +9,7 @@ using static Lsj.Util.Win32.Enums.ProcessCreationFlags;
 using static Lsj.Util.Win32.Enums.ProcessPriorityClasses;
 using static Lsj.Util.Win32.Userenv;
 using static Lsj.Util.Win32.Enums.DllMainReasons;
+using System.Text;
 
 namespace Lsj.Util.Win32
 {
@@ -1145,6 +1146,59 @@ namespace Lsj.Util.Win32
         /// </remarks>
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetPriorityClass", SetLastError = true)]
         public static extern ProcessPriorityClasses GetPriorityClass([In]IntPtr hProcess);
+
+        /// <summary>
+        /// <para>
+        /// Retrieves the name of the executable file for the specified process.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/psapi/nf-psapi-getprocessimagefilenamew
+        /// </para>
+        /// </summary>
+        /// <param name="hProcess">
+        /// A handle to the process.
+        /// The handle must have the <see cref="PROCESS_QUERY_INFORMATION"/> or <see cref="PROCESS_QUERY_LIMITED_INFORMATION"/> access right.
+        /// For more information, see Process Security and Access Rights.
+        /// Windows Server 2003 and Windows XP:
+        /// The handle must have the <see cref="PROCESS_QUERY_INFORMATION"/> access right.
+        /// </param>
+        /// <param name="lpImageFileName">
+        /// A pointer to a buffer that receives the full path to the executable file.
+        /// </param>
+        /// <param name="nSize">
+        /// The size of the <paramref name="lpImageFileName"/> buffer, in characters.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value specifies the length of the string copied to the buffer.
+        /// If the function fails, the return value is zero. To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// The file Psapi.dll is installed in the %windir%\System32 directory.
+        /// If there is another copy of this DLL on your computer, it can lead to the following error when running applications on your system:
+        /// "The procedure entry point GetProcessImageFileName could not be located in the dynamic link library PSAPI.DLL."
+        /// To work around this problem, locate any versions that are not in the %windir%\System32 directory and delete or rename them, then restart.
+        /// The GetProcessImageFileName function returns the path in device form, rather than drive letters.
+        /// For example, the file name C:\Windows\System32\Ctype.nls would look as follows in device form:
+        /// \Device\Harddisk0\Partition1\Windows\System32\Ctype.nls
+        /// To retrieve the module name of the current process,
+        /// use the <see cref="GetModuleFileName"/> function with a <see cref="IntPtr.Zero"/> module handle.
+        /// This is more efficient than calling the <see cref="GetProcessImageFileName"/> function with a handle to the current process.
+        /// To retrieve the name of the main executable module for a remote process in win32 path format,
+        /// use the <see cref="QueryFullProcessImageName"/> function.
+        /// Starting with Windows 7 and Windows Server 2008 R2, Psapi.h establishes version numbers for the PSAPI functions.
+        /// The PSAPI version number affects the name used to call the function and the library that a program must load.
+        /// If PSAPI_VERSION is 2 or greater, this function is defined as K32GetProcessImageFileName in Psapi.h and
+        /// exported in Kernel32.lib and Kernel32.dll.
+        /// If PSAPI_VERSION is 1, this function is defined as GetProcessImageFileName in Psapi.h and exported in Psapi.lib and Psapi.dll
+        /// as a wrapper that calls K32GetProcessImageFileName.
+        /// Programs that must run on earlier versions of Windows as well as Windows 7 and later versions should always 
+        /// call this function as <see cref="GetProcessImageFileName"/>.
+        /// To ensure correct resolution of symbols, add Psapi.lib to the TARGETLIBS macro and compile the program with -DPSAPI_VERSION=1.
+        /// To use run-time dynamic linking, load Psapi.dll.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetProcessImageFileNameW", SetLastError = true)]
+        public static extern uint GetProcessImageFileName([In]IntPtr hProcess, [MarshalAs(UnmanagedType.LPWStr)][Out]StringBuilder lpImageFileName,
+            [In]uint nSize);
 
         /// <summary>
         /// <para>
