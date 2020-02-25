49997800821f27cd47a76892db8b8ed0e5838fb3
Shaojun Li <i@shaojun.li>
2020-02-25 17:08:08 +0800

    Add SYSTEM_INFO

---
+++ Src/Lsj.Util.Win32/Enums/ProcessorArchitectures.cs
@@ -0,0 +1,43 @@
+﻿namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// <para>
+    /// Processor Architectures
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/sysinfoapi/ns-sysinfoapi-system_info
+    /// </para>
+    /// </summary>
+    public enum ProcessorArchitectures : ushort
+    {
+        /// <summary>
+        /// x64 (AMD or Intel)
+        /// </summary>
+        PROCESSOR_ARCHITECTURE_AMD64 = 9,
+
+        /// <summary>
+        /// ARM
+        /// </summary>
+        PROCESSOR_ARCHITECTURE_ARM = 5,
+
+        /// <summary>
+        /// ARM64
+        /// </summary>
+        PROCESSOR_ARCHITECTURE_ARM64 = 12,
+
+        /// <summary>
+        /// Intel Itanium-based
+        /// </summary>
+        PROCESSOR_ARCHITECTURE_IA64 = 6,
+
+        /// <summary>
+        /// x86
+        /// </summary>
+        PROCESSOR_ARCHITECTURE_INTEL = 0,
+
+        /// <summary>
+        /// Unknown architecture.
+        /// </summary>
+        PROCESSOR_ARCHITECTURE_UNKNOWN = 0xffff,
+    }
+}
---
+++ Src/Lsj.Util.Win32/Enums/ProcessorTypes.cs
@@ -0,0 +1,33 @@
+﻿namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// Processor Types
+    /// </summary>
+    public enum ProcessorTypes : uint
+    {
+        /// <summary>
+        /// PROCESSOR_INTEL_386
+        /// </summary>
+        PROCESSOR_INTEL_386 = 386,
+
+        /// <summary>
+        /// PROCESSOR_INTEL_386
+        /// </summary>
+        PROCESSOR_INTEL_486 = 486,
+
+        /// <summary>
+        /// PROCESSOR_INTEL_PENTIUM 
+        /// </summary>
+        PROCESSOR_INTEL_PENTIUM = 586,
+
+        /// <summary>
+        /// PROCESSOR_INTEL_IA64 
+        /// </summary>
+        PROCESSOR_INTEL_IA64 = 2200,
+
+        /// <summary>
+        /// PROCESSOR_AMD_X8664 
+        /// </summary>
+        PROCESSOR_AMD_X8664 = 8664,
+    }
+}
