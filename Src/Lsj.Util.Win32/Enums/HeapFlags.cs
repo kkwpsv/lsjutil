cd36a045d1a57430c9921d6413a8d57b27bf1425
Shaojun Li <i@shaojun.li>
2020-02-25 15:21:36 +0800

    Add HeapAlloc

---
+++ Src/Lsj.Util.Win32/Enums/HeapFlags.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// Heap Flags
+    /// </summary>
+    [Flags]
+    public enum HeapFlags : uint
+    {
+        /// <summary>
+        /// HEAP_GENERATE_EXCEPTIONS
+        /// </summary>
+        HEAP_GENERATE_EXCEPTIONS = 0x00000004,
+
+        /// <summary>
+        /// HEAP_NO_SERIALIZE
+        /// </summary>
+        HEAP_NO_SERIALIZE = 0x00000001,
+
+        /// <summary>
+        /// HEAP_ZERO_MEMORY
+        /// </summary>
+        HEAP_ZERO_MEMORY = 0x00000008,
+    }
+}
--- Src/Lsj.Util.Win32/Kernel32.Memory.cs
+++ Src/Lsj.Util.Win32/Kernel32.Memory.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Runtime.InteropServices;
 using static Lsj.Util.Win32.Enums.GlobalMemoryFlags;
+using static Lsj.Util.Win32.Enums.HeapFlags;
 using static Lsj.Util.Win32.Enums.LocalMemoryFlags;
 
 namespace Lsj.Util.Win32
@@ -107,6 +108,78 @@ namespace Lsj.Util.Win32
         /// </returns>
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GlobalReAlloc", SetLastError = true)]
         public static extern IntPtr GlobalReAlloc(IntPtr hMem, IntPtr dwBytes, GlobalMemoryFlags uFlags);
+
+        /// <summary>
+        /// <para>
+        /// Allocates a block of memory from a heap. The allocated memory is not movable.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/heapapi/nf-heapapi-heapalloc
+        /// </para>
+        /// </summary>
+        /// <param name="hHeap">
+        /// A handle to the heap from which the memory will be allocated.
+        /// This handle is returned by the <see cref="HeapCreate"/> or <see cref="GetProcessHeap"/> function.
+        /// </param>
+        /// <param name="dwFlags">
+        /// The heap allocation options.
+        /// Specifying any of these values will override the corresponding value specified when the heap was created with <see cref="HeapCreate"/>.
+        /// This parameter can be one or more of the following values.
+        /// <see cref="HEAP_GENERATE_EXCEPTIONS"/>: 
+        /// The system will raise an exception to indicate a function failure, such as an out-of-memory condition, instead of returning NULL.
+        /// To ensure that exceptions are generated for all calls to this function,
+        /// specify <see cref="HEAP_GENERATE_EXCEPTIONS"/> in the call to <see cref="HeapCreate"/>.
+        /// In this case, it is not necessary to additionally specify <see cref="HEAP_GENERATE_EXCEPTIONS"/> in this function call.
+        /// <see cref="HEAP_NO_SERIALIZE"/>:
+        /// Serialized access will not be used for this allocation. For more information, see Remarks.
+        /// To ensure that serialized access is disabled for all calls to this function,
+        /// specify <see cref="HEAP_NO_SERIALIZE"/> in the call to <see cref="HeapCreate"/>.
+        /// In this case, it is not necessary to additionally specify <see cref="HEAP_NO_SERIALIZE"/> in this function call.
+        /// This value should not be specified when accessing the process's default heap.
+        /// The system may create additional threads within the application's process, such as a CTRL+C handler,
+        /// that simultaneously access the process's default heap.
+        /// <see cref="HEAP_ZERO_MEMORY"/>:
+        /// The allocated memory will be initialized to zero. Otherwise, the memory is not initialized to zero.
+        /// </param>
+        /// <param name="dwBytes">
+        /// The number of bytes to be allocated.
+        /// If the heap specified by the hHeap parameter is a "non-growable" heap, <paramref name="dwBytes"/> must be less than 0x7FFF8.
+        /// You create a non-growable heap by calling the <see cref="HeapCreate"/> function with a nonzero value.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is a pointer to the allocated memory block.
+        /// If the function fails and you have not specified <see cref="HEAP_GENERATE_EXCEPTIONS"/>, the return value is <see cref="IntPtr.Zero"/>.
+        /// If the function fails and you have specified <see cref="HEAP_GENERATE_EXCEPTIONS"/>,
+        /// the function may generate either of the exceptions listed in the following table.
+        /// The particular exception depends upon the nature of the heap corruption.
+        /// For more information, see <see cref="GetExceptionCode"/>.
+        /// <see cref="STATUS_NO_MEMORY"/>: The allocation attempt failed because of a lack of available memory or heap corruption.
+        /// <see cref="STATUS_ACCESS_VIOLATION"/>: The allocation attempt failed because of heap corruption or improper function parameters.
+        /// If the function fails, it does not call <see cref="SetLastError"/>.
+        /// An application cannot call GetLastError for extended error information.
+        /// </returns>
+        /// <remarks>
+        /// If the <see cref="HeapAlloc"/> function succeeds, it allocates at least the amount of memory requested.
+        /// To allocate memory from the process's default heap,
+        /// use <see cref="HeapAlloc"/> with the handle returned by the <see cref="GetProcessHeap"/> function.
+        /// To free a block of memory allocated by <see cref="HeapAlloc"/>, use the <see cref="HeapFree"/> function.
+        /// Memory allocated by <see cref="HeapAlloc"/> is not movable.
+        /// The address returned by HeapAlloc is valid until the memory block is freed or reallocated;
+        /// the memory block does not need to be locked.
+        /// Because the system cannot compact a private heap, it can become fragmented.
+        /// Applications that allocate large amounts of memory in various allocation sizes can use the low-fragmentation heap to reduce heap fragmentation.
+        /// Serialization ensures mutual exclusion when two or more threads attempt to simultaneously allocate or free blocks from the same heap.
+        /// There is a small performance cost to serialization, but it must be used whenever multiple threads allocate and free memory from the same heap.
+        /// Setting the <see cref="HEAP_NO_SERIALIZE"/> value eliminates mutual exclusion on the heap.
+        /// Without serialization, two or more threads that use the same heap handle might attempt to allocate or free memory simultaneously,
+        /// likely causing corruption in the heap.
+        /// The <see cref="HEAP_NO_SERIALIZE"/> value can, therefore, be safely used only in the following situations:
+        /// The process has only one thread.
+        /// The process has multiple threads, but only one thread calls the heap functions for a specific heap.
+        /// The process has multiple threads, and the application provides its own mechanism for mutual exclusion to a specific heap.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "HeapAlloc", SetLastError = true)]
+        public static extern IntPtr HeapAlloc([In]IntPtr hHeap, [In]HeapFlags dwFlags, [In]IntPtr dwBytes);
 
         /// <summary>
         /// <para>
