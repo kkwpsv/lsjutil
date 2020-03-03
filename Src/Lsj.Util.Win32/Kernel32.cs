21f3a9d6ecebee2c0f8d7de1e6430da4b8e4821f
Shaojun Li <i@shaojun.li>
2020-03-03 17:47:44 +0800

    Add IsWow64Process

--- Src/Lsj.Util.Win32/Kernel32.cs
+++ Src/Lsj.Util.Win32/Kernel32.cs
@@ -1,4 +1,5 @@
 ﻿using Lsj.Util.Win32.Structs;
+using System;
 using System.Runtime.InteropServices;
 using System.Text;
 using static Lsj.Util.Win32.Enums.SystemErrorCodes;
@@ -146,5 +147,42 @@ namespace Lsj.Util.Win32
         /// </remarks>
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GlobalDeleteAtom", SetLastError = true)]
         public static extern ushort GlobalDeleteAtom([In]uint nAtom);
+
+        /// <summary>
+        /// <para>
+        /// Determines whether the specified process is running under WOW64 or an Intel64 of x64 processor.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wow64apiset/nf-wow64apiset-iswow64process
+        /// </para>
+        /// </summary>
+        /// <param name="hProcess">
+        /// A handle to the process.
+        /// The handle must have the <see cref="PROCESS_QUERY_INFORMATION"/> or <see cref="PROCESS_QUERY_LIMITED_INFORMATION"/> access right.
+        /// For more information, see Process Security and Access Rights.
+        /// Windows Server 2003 and Windows XP:  The handle must have the <see cref="PROCESS_QUERY_INFORMATION"/> access right.
+        /// </param>
+        /// <param name="Wow64Process">
+        /// A pointer to a value that is set to <see langword="true"/> if the process is running under WOW64 on an Intel64 or x64 processor.
+        /// If the process is running under 32-bit Windows, the value is set to <see langword="false"/>.
+        /// If the process is a 32-bit application running under 64-bit Windows 10 on ARM, the value is set to <see langword="false"/>.
+        /// If the process is a 64-bit application running under 64-bit Windows, the value is also set to <see langword="false"/>.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see langword="true"/>.
+        /// If the function fails, the return value is <see langword="false"/>.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// Applications should use <see cref="IsWow64Process2"/> instead of <see cref="IsWow64Process"/> to determine if a process is running under WOW.
+        /// <see cref="IsWow64Process2"/> removes the ambiguity inherent to multiple WOW environments
+        /// by explicitly returning both the architecture of the host and guest for a given process.
+        /// Applications can use this information to reliably identify situations such as running under emulation on ARM64.
+        /// To compile an application that uses this function, define _WIN32_WINNT as 0x0501 or later.
+        /// For more information, see Using the Windows Headers.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "IsWow64Process", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWow64Process([In]IntPtr hProcess, [Out]out bool Wow64Process);
     }
 }
