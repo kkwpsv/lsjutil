c4b52f98e69a583eb95c1ff5fe96e57c66907a4a
Shaojun Li <i@shaojun.li>
2020-03-05 22:07:33 +0800

    Add UnionStruct

---
+++ Src/Lsj.Util.Win32/Marshals/UnionStruct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Lsj.Util.Win32.Marshals
+{
+    /// <summary>
+    /// UnionStruct
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
+    public struct UnionStruct<T1, T2> where T1 : struct where T2 : struct
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [FieldOffset(0)]
+        public T1 Struct1;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [FieldOffset(0)]
+        public T2 Struct2;
+    }
+}
