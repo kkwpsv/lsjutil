c104f665dab06acb1f35b567fc04ff3ee96e4e35
Shaojun Li <i@shaojun.li>
2020-03-09 20:26:29 +0800

    Implement FileOpenDialog

--- Src/Lsj.Util.Win32.NativeUI/Dialogs/FileOpenDialog.cs
+++ Src/Lsj.Util.Win32.NativeUI/Dialogs/FileOpenDialog.cs
@@ -1,19 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Lsj.Util.Text;
+using Lsj.Util.Win32.ComInterfaces;
+using System;
+using System.Runtime.InteropServices;
+using static Lsj.Util.Win32.ComInterfaces.CLSIDs;
+using static Lsj.Util.Win32.ComInterfaces.IIDs;
+using static Lsj.Util.Win32.Enums.CLSCTX;
+using static Lsj.Util.Win32.Enums.FILEOPENDIALOGOPTIONS;
+using static Lsj.Util.Win32.Enums.SIGDN;
+using static Lsj.Util.Win32.Ole32;
+using static Lsj.Util.Win32.Structs.HRESULT;
 
 namespace Lsj.Util.Win32.NativeUI.Dialogs
 {
     public class FileOpenDialog : BaseDialog<string>
     {
-        public FileOpenDialog()
-        {
+        IFileDialog _dialog = null;
 
-        }
+        /// <summary>
+        /// IsPickFolders
+        /// </summary>
+        public bool IsPickFolders { get; set; }
 
         public override ShowDialogResult ShowDialog(IntPtr owner)
         {
+            CreateDialog();
+            try
+            {
+                SetOptions();
+                if (_dialog.Show(owner))
+                {
+                    GetResult();
+                    return ShowDialogResult.OK;
+                }
+                else
+                {
+                    return ShowDialogResult.Cancel;
+                }
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(_dialog);
+            }
+        }
 
+        private void CreateDialog()
+        {
+            var result = CoCreateInstance(CLSID_FileOpenDialog.ToGuid(), null, CLSCTX_INPROC_SERVER, IID_IUnknown.ToGuid(), out var obj);
+            if (result)
+            {
+                _dialog = obj as IFileDialog;
+            }
+            else
+            {
+                throw Marshal.GetExceptionForHR(result);
+            }
+        }
+
+        private void SetOptions()
+        {
+            var result = S_OK;
+            if (_dialog.GetOptions(out var options))
+            {
+                if (IsPickFolders)
+                {
+                    options |= FOS_PICKFOLDERS;
+                }
+                if (_dialog.SetOptions(options))
+                {
+                    return;
+                }
+            }
+            throw Marshal.GetExceptionForHR(result);
+        }
+
+        private void GetResult()
+        {
+            var result = S_OK;
+            if (result = _dialog.GetResult(out var shellItem))
+            {
+                try
+                {
+                    if (result = shellItem.GetDisplayName(SIGDN_FILESYSPATH, out var path))
+                    {
+                        Result = path;
+                        return;
+                    }
+                }
+                finally
+                {
+                    Marshal.ReleaseComObject(shellItem);
+                }
+            }
+            throw Marshal.GetExceptionForHR(result);
         }
     }
 }
---
+++ Src/Lsj.Util.Win32/ComInterfaces/CLSIDs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lsj.Util.Win32.ComInterfaces
+{
+    public static class CLSIDs
+    {
+        /// <summary>
+        /// CLSID_FileOpenDialog
+        /// </summary>
+        public const string CLSID_FileOpenDialog = "DC1C5A9C-E88A-4dde-A5A1-60F82A20AEF7";
+    }
+}
