5028c21ed6e11ee7c38cb65bbdf6ce515e4ffe14
Shaojun Li <i@shaojun.li>
2020-03-09 20:23:57 +0800

    Fix HRESULT value type and add Succeed property

--- Src/Lsj.Util.Win32/Structs/HRESULT.cs
+++ Src/Lsj.Util.Win32/Structs/HRESULT.cs
@@ -11,12 +11,12 @@ namespace Lsj.Util.Win32.Structs
         /// <summary>
         /// E_OUTOFMEMORY
         /// </summary>
-        public static readonly HRESULT E_OUTOFMEMORY = new HRESULT { value = 0x8007000E };
+        public static readonly HRESULT E_OUTOFMEMORY = new HRESULT { value = unchecked((int)0x8007000E) };
 
         /// <summary>
         /// E_INVALIDARG
         /// </summary>
-        public static readonly HRESULT E_INVALIDARG = new HRESULT { value = 0x80070057 };
+        public static readonly HRESULT E_INVALIDARG = new HRESULT { value = unchecked((int)0x80070057) };
 
         /// <summary>
         /// S_OK
@@ -24,30 +24,36 @@ namespace Lsj.Util.Win32.Structs
         public static readonly HRESULT S_OK = new HRESULT();
 
         [FieldOffset(0)]
-        private uint value;
+        private int value;
+
+        public override string ToString() => value.ToString("X");
+
+        public bool Succeed => value >= 0;
+
+        public static implicit operator bool(HRESULT val) => val.Succeed;
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="val"></param>
-        public static implicit operator uint(HRESULT val) => val.value;
+        public static implicit operator uint(HRESULT val) => unchecked((uint)val.value);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="val"></param>
-        public static implicit operator int(HRESULT val) => unchecked((int)val.value);
+        public static implicit operator int(HRESULT val) => val.value;
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="val"></param>
-        public static implicit operator HRESULT(int val) => new HRESULT { value = unchecked((uint)val) };
+        public static implicit operator HRESULT(int val) => new HRESULT { value = val };
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="val"></param>
-        public static implicit operator HRESULT(uint val) => new HRESULT { value = val };
+        public static implicit operator HRESULT(uint val) => new HRESULT { value = unchecked((int)val) };
     }
 }
