c88dac2e4ee7c13832d0927e4f2cfc814e90ac6e
Shaojun Li <i@shaojun.li>
2020-03-10 16:41:48 +0800

    Add GetTokenInformation

--- Src/Lsj.Util.Win32/Kernel32.Token.cs
+++ Src/Lsj.Util.Win32/Kernel32.Token.cs
@@ -216,5 +216,50 @@ namespace Lsj.Util.Win32
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(StructPointerOrNullObjectMarshaler<SECURITY_ATTRIBUTES>))]
             [In]StructPointerOrNullObject<SECURITY_ATTRIBUTES> lpTokenAttributes, [In]SECURITY_IMPERSONATION_LEVEL ImpersonationLevel,
             [In]TOKEN_TYPE TokenType, [Out]out IntPtr DuplicateTokenHandle);
+
+        /// <summary>
+        /// <para>
+        /// The <see cref="GetTokenInformation"/> function retrieves a specified type of information about an access token.
+        /// The calling process must have appropriate access rights to obtain the information.
+        /// To determine if a user is a member of a specific group, use the <see cref="CheckTokenMembership"/> function.
+        /// To determine group membership for app container tokens, use the <see cref="CheckTokenMembershipEx"/> function.
+        /// </para>
+        /// </summary>
+        /// <param name="TokenHandle">
+        /// A handle to an access token from which information is retrieved.
+        /// If <paramref name="TokenInformationClass"/> specifies <see cref="TokenSource"/>, the handle must have <see cref="TOKEN_QUERY_SOURCE"/> access.
+        /// For all other <see cref="TokenInformationClass"/> values, the handle must have <see cref="TOKEN_QUERY"/> access.
+        /// </param>
+        /// <param name="TokenInformationClass">
+        /// Specifies a value from the <see cref="TOKEN_INFORMATION_CLASS"/> enumerated type to identify the type of information the function retrieves.
+        /// Any callers who check the TokenIsAppContainer and have it return 0 should also verify
+        /// that the caller token is not an identify level impersonation token.
+        /// If the current token is not an app container but is an identity level token, you should return AccessDenied.
+        /// </param>
+        /// <param name="TokenInformation">
+        /// A pointer to a buffer the function fills with the requested information.
+        /// The structure put into this buffer depends upon the type of information specified by the <paramref name="TokenInformationClass"/> parameter.
+        /// </param>
+        /// <param name="TokenInformationLength">
+        /// Specifies the size, in bytes, of the buffer pointed to by the <see cref="TokenInformation"/> parameter.
+        /// If <see cref="TokenInformation"/> is <see cref="IntPtr.Zero"/>, this parameter must be zero.
+        /// </param>
+        /// <param name="ReturnLength">
+        /// A pointer to a variable that receives the number of bytes needed for the buffer pointed to by the <paramref name="TokenInformation"/> parameter.
+        /// If this value is larger than the value specified in the <paramref name="TokenInformationLength"/> parameter,
+        /// the function fails and stores no data in the buffer.
+        /// If the value of the <paramref name="TokenInformationClass"/> parameter is <see cref="TokenInformationLength"/> and the token has no default DACL,
+        /// the function sets the variable pointed to by <paramref name="ReturnLength"/> to sizeof(<see cref="TOKEN_DEFAULT_DACL"/>) and
+        /// sets the <see cref="DefaultDacl"/> member of the <see cref="TOKEN_DEFAULT_DACL"/> structure to NULL.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see langword="true"/>.
+        /// If the function fails, the return value is <see langword="false"/>.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetTokenInformation", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetTokenInformation([In]IntPtr TokenHandle, [In]TOKEN_INFORMATION_CLASS TokenInformationClass,
+            [In]IntPtr TokenInformation, [In]uint TokenInformationLength, [Out]out uint ReturnLength);
     }
 }
