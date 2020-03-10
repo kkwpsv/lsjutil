5559b8cbaa008b60073c082d0d76ab2ffff3f282
Shaojun Li <i@shaojun.li>
2020-03-10 23:17:44 +0800

    Add ResumeThread

--- Src/Lsj.Util.Win32/Kernel32.Thread.cs
+++ Src/Lsj.Util.Win32/Kernel32.Thread.cs
@@ -565,6 +565,43 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Decrements a thread's suspend count.
+        /// When the suspend count is decremented to zero, the execution of the thread is resumed.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/processthreadsapi/nf-processthreadsapi-resumethread
+        /// </para>
+        /// </summary>
+        /// <param name="hThread">
+        /// A handle to the thread to be restarted.
+        /// This handle must have the <see cref="THREAD_SUSPEND_RESUME"/> access right.
+        /// For more information, see Thread Security and Access Rights.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the thread's previous suspend count.
+        /// If the function fails, the return value is (DWORD) -1.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// The <see cref="ResumeThread"/> function checks the suspend count of the subject thread.
+        /// If the suspend count is zero, the thread is not currently suspended. Otherwise, the subject thread's suspend count is decremented.
+        /// If the resulting value is zero, then the execution of the subject thread is resumed.
+        /// If the return value is zero, the specified thread was not suspended.
+        /// If the return value is 1, the specified thread was suspended but was restarted.
+        /// If the return value is greater than 1, the specified thread is still suspended.
+        /// Note that while reporting debug events, all threads within the reporting process are frozen.
+        /// Debuggers are expected to use the <see cref="SuspendThread"/> and <see cref="ResumeThread"/> functions
+        /// to limit the set of threads that can execute within a process.
+        /// By suspending all threads in a process except for the one reporting a debug event, it is possible to "single step" a single thread.
+        /// The other threads are not released by a continue operation if they are suspended.
+        /// Windows Phone 8.1: This function is supported for Windows Phone Store apps on Windows Phone 8.1 and later.
+        /// Windows 8.1 and Windows Server 2012 R2: This function is supported for Windows Store apps on Windows 8.1, Windows Server 2012 R2, and later.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetThreadPriority", SetLastError = true)]
+        public static extern uint ResumeThread([In]IntPtr hThread);
+
+        /// <summary>
+        /// <para>
         /// Terminates a thread.
         /// </para>
         /// <para>
