f14e9e8a6e74fd4520ae48127075a9c5b4b72296
Shaojun Li <i@shaojun.li>
2020-03-11 19:29:18 +0800

    Add SwitchToThread

--- Src/Lsj.Util.Win32/Kernel32.Thread.cs
+++ Src/Lsj.Util.Win32/Kernel32.Thread.cs
@@ -778,6 +778,38 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Causes the calling thread to yield execution to another thread that is ready to run on the current processor.
+        /// The operating system selects the next thread to be executed.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/processthreadsapi/nf-processthreadsapi-switchtothread
+        /// </para>
+        /// </summary>
+        /// <returns>
+        /// If calling the <see cref="SwitchToThread"/> function causes the operating system to switch execution to another thread,
+        /// the return value is <see langword="true"/>.
+        /// If there are no other threads ready to execute, the operating system does not switch execution to another thread,
+        /// and the return value is <see langword="false"/>.
+        /// </returns>
+        /// <remarks>
+        /// The yield of execution is in effect for up to one thread-scheduling time slice on the processor of the calling thread.
+        /// The operating system will not switch execution to another processor, even if that processor is idle or is running a thread of lower priority.
+        /// After the yielding thread's time slice elapses, the operating system reschedules execution for the yielding thread.
+        /// The rescheduling is determined by the priority of the yielding thread and the status of other threads that are available to run.
+        /// Note that the operating system will not switch to a thread that is being prevented from running only by concurrency control.
+        /// For example, an I/O completion port or thread pool limits the number of associated threads that can run.
+        /// If the maximum number of threads is already running, no additional associated thread can run until a running thread finishes.
+        /// If a thread uses <see cref="SwitchToThread"/> to wait for one of the additional associated threads to accomplish some work,
+        /// the process might deadlock.
+        /// To compile an application that uses this function, define _WIN32_WINNT as 0x0400 or later.
+        /// For more information, see Using the Windows Headers.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "SwitchToThread", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SwitchToThread();
+
+        /// <summary>
+        /// <para>
         /// Retrieves the value in the calling thread's thread local storage (TLS) slot for the specified TLS index.
         /// Each thread of a process has its own slot for each TLS index.
         /// </para>
