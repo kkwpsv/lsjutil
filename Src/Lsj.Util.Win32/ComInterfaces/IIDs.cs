b43e33125be43e0257e018a25b05f1d83b0840bc
Shaojun Li <i@shaojun.li>
2020-03-15 22:26:41 +0800

    Fix a error.

--- Src/Lsj.Util.Win32/ComInterfaces/IIDs.cs
+++ Src/Lsj.Util.Win32/ComInterfaces/IIDs.cs
@@ -13,7 +13,7 @@
         /// <summary>
         /// IID_IDataAdviseHolder
         /// </summary>
-        public const string IID_IDataAdviseHolder = "00000110-0000-0000-C000-000000000046"
+        public const string IID_IDataAdviseHolder = "00000110-0000-0000-C000-000000000046";
 
         /// <summary>
         /// IID_IFileDialog
