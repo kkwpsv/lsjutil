62cdc5ae80e1a1543761d6ab106dafee0fb041ac
Shaojun Li <i@shaojun.li>
2020-03-15 22:25:59 +0800

    Fix commetnts.

--- Src/Lsj.Util.Win32/ComInterfaces/IDataAdviseHolder.cs
+++ Src/Lsj.Util.Win32/ComInterfaces/IDataAdviseHolder.cs
@@ -3,6 +3,7 @@ using Lsj.Util.Win32.Structs;
 using System;
 using System.Runtime.InteropServices;
 using static Lsj.Util.Win32.ComInterfaces.IIDs;
+using static Lsj.Util.Win32.Enums.ADVF;
 
 namespace Lsj.Util.Win32.ComInterfaces
 {
