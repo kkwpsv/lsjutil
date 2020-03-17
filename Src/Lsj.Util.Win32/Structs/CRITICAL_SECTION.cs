3bde0f7021d06cd8a1d6f9dab4414b60d2459b79
Shaojun Li <i@shaojun.li>
2020-03-17 14:03:39 +0800

    Add some sync APIs.

---
+++ Src/Lsj.Util.Win32/Structs/CRITICAL_SECTION.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.Structs
+{
+    /// <summary>
+    /// CRITICAL_SECTION
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct CRITICAL_SECTION
+    {
+        /// <summary>
+        /// DebugInfo
+        /// </summary>
+        public IntPtr DebugInfo;
+
+        /// <summary>
+        /// LockCount
+        /// </summary>
+        public int LockCount;
+
+        /// <summary>
+        /// RecursionCount
+        /// </summary>
+        public int RecursionCount;
+
+        /// <summary>
+        /// OwningThread
+        /// </summary>
+        public IntPtr OwningThread;
+
+        /// <summary>
+        /// LockSemaphore
+        /// </summary>
+        public IntPtr LockSemaphore;
+
+        /// <summary>
+        /// SpinCount
+        /// </summary>
+        public UIntPtr SpinCount;
+    }
+}
