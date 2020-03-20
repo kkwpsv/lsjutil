5d908e73ae1e75e38578bd0223fe60d48916ba77
Shaojun Li <i@shaojun.li>
2020-03-20 19:00:58 +0800

    Add MsgWaitForMultipleObjectsEx

---
+++ Src/Lsj.Util.Win32/Enums/MsgWaitForMultipleObjectsExFlags.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// <para>
+    /// <see cref="MsgWaitForMultipleObjectsExFlags"/> Flags
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-msgwaitformultipleobjectsex
+    /// </para>
+    /// </summary>
+    [Flags]
+    public enum MsgWaitForMultipleObjectsExFlags : uint
+    {
+        /// <summary>
+        /// The function also returns if an APC has been queued to the thread with <see cref="QueueUserAPC"/> while the thread is in the waiting state.
+        /// </summary>
+        MWMO_ALERTABLE = 0x0002,
+
+        /// <summary>
+        /// The function returns if input exists for the queue, even if the input has been seen (but not removed) using a call to another function,
+        /// such as <see cref="PeekMessage"/>.
+        /// </summary>
+        MWMO_INPUTAVAILABLE = 0x0004,
+
+        /// <summary>
+        /// The function returns when all objects in the pHandles array are signaled and an input event has been received, all at the same time.
+        /// </summary>
+        MWMO_WAITALL = 0x0001,
+    }
+}
--- Src/Lsj.Util.Win32/Kernel32.Sync.cs
+++ Src/Lsj.Util.Win32/Kernel32.Sync.cs
@@ -1,6 +1,8 @@
 ﻿using Lsj.Util.Win32.Structs;
 using System;
 using System.Runtime.InteropServices;
+using Lsj.Util.Win32.BaseTypes;
+using Lsj.Util.Win32.Enums;
 
 namespace Lsj.Util.Win32
 {
@@ -552,6 +554,117 @@ namespace Lsj.Util.Win32
         /// </remarks>
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "LeaveCriticalSection", SetLastError = true)]
         public static extern void LeaveCriticalSection([In][Out]ref CRITICAL_SECTION lpCriticalSection);
+
+        /// <summary>
+        /// <para>
+        /// Waits until one or all of the specified objects are in the signaled state,
+        /// an I/O completion routine or asynchronous procedure call (APC) is queued to the thread, or the time-out interval elapses.
+        /// The array of objects can include input event objects, which you specify using the dwWakeMask parameter.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-msgwaitformultipleobjectsex
+        /// </para>
+        /// </summary>
+        /// <param name="nCount">
+        /// The number of object handles in the array pointed to by <paramref name="pHandles"/>.
+        /// The maximum number of object handles is <see cref="MAXIMUM_WAIT_OBJECTS"/> minus one.
+        /// If this parameter has the value zero, then the function waits only for an input event.
+        /// </param>
+        /// <param name="pHandles">
+        /// An array of object handles.
+        /// For a list of the object types whose handles you can specify, see the Remarks section later in this topic.
+        /// The array can contain handles to multiple types of objects.
+        /// It may not contain multiple copies of the same handle.
+        /// If one of these handles is closed while the wait is still pending, the function's behavior is undefined.
+        /// The handles must have the <see cref="SYNCHRONIZE"/> access right. For more information, see Standard Access Rights.
+        /// </param>
+        /// <param name="dwMilliseconds">
+        /// The time-out interval, in milliseconds.
+        /// If a nonzero value is specified, the function waits until the specified objects are signaled,
+        /// an I/O completion routine or APC is queued, or the interval elapses.
+        /// If <paramref name="dwMilliseconds"/> is zero, the function does not enter a wait state if the criteria is not met;
+        /// it always returns immediately.
+        /// If <paramref name="dwMilliseconds"/> is <see cref="INFINITE"/>, the function will return only
+        /// when the specified objects are signaled or an I/O completion routine or APC is queued.
+        /// </param>
+        /// <param name="dwWakeMask">
+        /// The input types for which an input event object handle will be added to the array of object handles.
+        /// This parameter can be one or more of the following values.
+        /// <see cref="QS_ALLEVENTS"/>, <see cref="QS_ALLINPUT"/>, <see cref="QS_ALLPOSTMESSAGE"/>, <see cref="QS_HOTKEY"/>,
+        /// <see cref="QS_INPUT"/>, <see cref="QS_KEY"/>, <see cref="QS_MOUSE"/>, <see cref="QS_MOUSEBUTTON"/>, <see cref="QS_MOUSEMOVE"/>,
+        /// <see cref="QS_PAINT"/>, <see cref="QS_POSTMESSAGE"/>, <see cref="QS_RAWINPUT"/>, <see cref="QS_SENDMESSAGE"/>, <see cref="QS_TIMER"/>
+        /// </param>
+        /// <param name="dwFlags">
+        /// The wait type. This parameter can be one or more of the following values.
+        /// 0: 
+        /// The function returns when any one of the objects is signaled.
+        /// The return value indicates the object whose state caused the function to return.
+        /// <see cref="MWMO_ALERTABLE"/>, <see cref="MWMO_INPUTAVAILABLE"/>, <see cref="MWMO_WAITALL"/>
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value indicates the event that caused the function to return.
+        /// It can be one of the following values.
+        /// <see cref="WAIT_OBJECT_0"/> to (<see cref="WAIT_OBJECT_0"/> + <paramref name="nCount"/> - 1):
+        /// If the <see cref="MWMO_WAITALL"/> flag is used, the return value indicates that the state of all specified objects is signaled.
+        /// Otherwise, the return value minus <see cref="WAIT_OBJECT_0"/> indicates the <paramref name="pHandles"/> array index of the object
+        /// that caused the function to return.
+        /// <see cref="WAIT_OBJECT_0"/> + <paramref name="nCount"/>:
+        /// New input of the type specified in the <paramref name="dwWakeMask"/> parameter is available in the thread's input queue.
+        /// Functions such as <see cref="PeekMessage"/>, <see cref="GetMessage"/>, <see cref="GetQueueStatus"/>,
+        /// and <see cref="WaitMessage"/> mark messages in the queue as old messages.
+        /// Therefore, after you call one of these functions, a subsequent call to <see cref="MsgWaitForMultipleObjectsEx"/> will not return 
+        /// until new input of the specified type arrives.
+        /// This value is also returned upon the occurrence of a system event that requires the thread's action, such as foreground activation.
+        /// Therefore, <see cref="MsgWaitForMultipleObjectsEx"/> can return even though no appropriate input is available
+        /// and even if <paramref name="dwWakeMask"/> is set to 0.
+        /// If this occurs, call <see cref="GetMessage"/> or <see cref="PeekMessage"/> to process the system event
+        /// before trying the call to <see cref="MsgWaitForMultipleObjectsEx"/> again.
+        /// <see cref="WAIT_ABANDONED_0"/> to (<see cref="WAIT_ABANDONED_0"/> + <paramref name="nCount"/> - 1):
+        /// If the <see cref="MWMO_WAITALL"/> flag is used, the return value indicates that the state of all specified objects is signaled
+        /// and at least one of the objects is an abandoned mutex object.
+        /// Otherwise, the return value minus <see cref="WAIT_ABANDONED_0"/> indicates the <paramref name="pHandles"/> array index
+        /// of an abandoned mutex object that caused the function to return.
+        /// Ownership of the mutex object is granted to the calling thread, and the mutex is set to nonsignaled.
+        /// If the mutex was protecting persistent state information, you should check it for consistency.
+        /// <see cref="WAIT_IO_COMPLETION"/>:
+        /// The wait was ended by one or more user-mode asynchronous procedure calls (APC) queued to the thread.
+        /// <see cref="WAIT_TIMEOUT"/>:
+        /// The time-out interval elapsed, but the conditions specified by the <paramref name="dwFlags"/>
+        /// and <paramref name="dwWakeMask"/> parameters were not met.
+        /// <see cref="WAIT_FAILED"/>:
+        /// The function has failed. To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// The <see cref="MsgWaitForMultipleObjectsEx"/> function determines whether the conditions specified
+        /// by <paramref name="dwWakeMask"/> and <paramref name="dwFlags"/> have been met.
+        /// If the conditions have not been met, the calling thread enters the wait state until the conditions of the wait criteria have been met
+        /// or the time-out interval elapses.
+        /// When <paramref name="dwFlags"/> is zero, this function checks the handles in the array in order starting with index 0,
+        /// until one of the objects is signaled.
+        /// If multiple objects become signaled, the function returns the index of the first handle in the array whose object was signaled.
+        /// <see cref="MsgWaitForMultipleObjectsEx"/> does not return if there is unread input of the specified type in the message queue
+        /// after the thread has called a function to check the queue, unless you use the <see cref="MWMO_INPUTAVAILABLE"/> flag.
+        /// This is because functions such as <see cref="PeekMessage"/>, <see cref="GetMessage"/>, <see cref="GetQueueStatus"/>,
+        /// and <see cref="WaitMessage"/> check the queue and then change the state information for the queue so that the input is no longer considered new.
+        /// A subsequent call to <see cref="MsgWaitForMultipleObjectsEx"/> will not return until new input of the specified type arrives,
+        /// unless you use the <see cref="MWMO_INPUTAVAILABLE"/> flag.
+        /// If this flag is not used, the existing unread input (received prior to the last time the thread checked the queue) is ignored.
+        /// The function modifies the state of some types of synchronization objects.
+        /// Modification occurs only for the object or objects whose signaled state caused the function to return.
+        /// For example, the system decreases the count of a semaphore object by one.
+        /// For more information, see the documentation for the individual synchronization objects.
+        /// The <see cref="MsgWaitForMultipleObjectsEx"/> function can specify handles of any of the following object types in the pHandles array:
+        /// Change notification, Console input ,Event ,Memory resource notification, Mutex, Process, Semaphore, Thread, Waitable timer
+        /// The <see cref="QS_ALLPOSTMESSAGE"/> and <see cref="QS_POSTMESSAGE"/> flags differ in when they are cleared.
+        /// <see cref="QS_POSTMESSAGE"/> is cleared when you call <see cref="GetMessage"/> or <see cref="PeekMessage"/>,
+        /// whether or not you are filtering messages.
+        /// <see cref="QS_ALLPOSTMESSAGE"/> is cleared when you call <see cref="GetMessage"/> or <see cref="PeekMessage"/>
+        /// without filtering messages (wMsgFilterMin and wMsgFilterMax are 0).
+        /// This can be useful when you call <see cref="PeekMessage"/> multiple times to get messages in different ranges.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "MsgWaitForMultipleObjectsEx", SetLastError = true)]
+        public static extern DWORD MsgWaitForMultipleObjectsEx([In]DWORD nCount, [MarshalAs(UnmanagedType.LPArray)][In]HANDLE[] pHandles,
+            [In]DWORD dwMilliseconds, [In]QueueStatus dwWakeMask, [In]MsgWaitForMultipleObjectsExFlags dwFlags);
 
         /// <summary>
         /// <para>
