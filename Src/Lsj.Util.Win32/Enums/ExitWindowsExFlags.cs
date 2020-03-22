492274dbb266a60b92fec7731419ad21d321f2ef
Shaojun Li <i@shaojun.li>
2020-03-22 20:25:53 +0800

    Add ExitWindows and ExitWindowsEx

---
+++ Src/Lsj.Util.Win32/Enums/ExitWindowsExFlags.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// <para>
+    /// <see cref="ExitWindowsEx"/> Flags
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-exitwindowsex
+    /// </para>
+    /// </summary>
+    [Flags]
+    public enum ExitWindowsExFlags : uint
+    {
+        /// <summary>
+        /// Beginning with Windows 8:
+        /// You can prepare the system for a faster startup by combining the <see cref="EWX_HYBRID_SHUTDOWN"/> flag with the <see cref="EWX_SHUTDOWN"/> flag.
+        /// </summary>
+        EWX_HYBRID_SHUTDOWN = 0x00400000,
+
+        /// <summary>
+        /// Shuts down all processes running in the logon session of the process that called the <see cref="ExitWindowsEx"/> function.
+        /// Then it logs the user off.
+        /// This flag can be used only by processes running in an interactive user's logon session.
+        /// </summary>
+        EWX_LOGOFF = 0,
+
+        /// <summary>
+        /// Shuts down the system and turns off the power. The system must support the power-off feature.
+        /// The calling process must have the <see cref="SE_SHUTDOWN_NAME"/> privilege.
+        /// For more information, see the following Remarks section.
+        /// </summary>
+        EWX_POWEROFF = 0x00000008,
+
+        /// <summary>
+        /// Shuts down the system and then restarts the system.
+        /// The calling process must have the <see cref="SE_SHUTDOWN_NAME"/> privilege.
+        /// For more information, see the following Remarks section.
+        /// </summary>
+        EWX_REBOOT = 0x00000002,
+
+        /// <summary>
+        /// Shuts down the system and then restarts it, as well as any applications that have been registered for restart
+        /// using the <see cref="RegisterApplicationRestart"/> function.
+        /// These application receive the <see cref="WM_QUERYENDSESSION"/> message with lParam set to the <see cref="ENDSESSION_CLOSEAPP"/> value.
+        /// For more information, see Guidelines for Applications.
+        /// </summary>
+        EWX_RESTARTAPPS = 0x00000040,
+
+        /// <summary>
+        /// Shuts down the system to a point at which it is safe to turn off the power.
+        /// All file buffers have been flushed to disk, and all running processes have stopped.
+        /// The calling process must have the <see cref="SE_SHUTDOWN_NAME"/> privilege.
+        /// For more information, see the following Remarks section.
+        /// Specifying this flag will not turn off the power even if the system supports the power-off feature.
+        /// You must specify <see cref="EWX_POWEROFF"/> to do this.
+        /// Windows XP with SP1: If the system supports the power-off feature, specifying this flag turns off the power.
+        /// </summary>
+        EWX_SHUTDOWN = 0x00000001,
+
+        /// <summary>
+        /// This flag has no effect if terminal services is enabled.
+        /// Otherwise, the system does not send the <see cref="WM_QUERYENDSESSION"/> message.
+        /// This can cause applications to lose data.
+        /// Therefore, you should only use this flag in an emergency.
+        /// </summary>
+        EWX_FORCE = 0x00000004,
+
+        /// <summary>
+        /// Forces processes to terminate if they do not respond to the <see cref="WM_QUERYENDSESSION"/>
+        /// or <see cref="WM_ENDSESSION"/> message within the timeout interval.
+        /// For more information, see the Remarks.
+        /// </summary>
+        EWX_FORCEIFHUNG = 0x00000010,
+    }
+}
---
+++ Src/Lsj.Util.Win32/Enums/SystemShutdownReasonCodes.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// <para>
+    /// The shutdown reason codes are used by the <see cref="ExitWindowsEx"/> and <see cref="InitiateSystemShutdownEx"/> functions in the dwReason parameter.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/shutdown/system-shutdown-reason-codes
+    /// </para>
+    /// </summary>
+    [Flags]
+    public enum SystemShutdownReasonCodes : uint
+    {
+        /// <summary>
+        /// Application issue.
+        /// </summary>
+        SHTDN_REASON_MAJOR_APPLICATION = 0x00040000,
+
+        /// <summary>
+        /// Hardware issue.
+        /// </summary>
+        SHTDN_REASON_MAJOR_HARDWARE = 0x00010000,
+
+        /// <summary>
+        /// The <see cref="InitiateSystemShutdown"/> function was used instead of <see cref="InitiateSystemShutdownEx"/>.
+        /// </summary>
+        SHTDN_REASON_MAJOR_LEGACY_API = 0x00070000,
+
+        /// <summary>
+        /// Operating system issue.
+        /// </summary>
+        SHTDN_REASON_MAJOR_OPERATINGSYSTEM = 0x00020000,
+
+        /// <summary>
+        /// Other issue.
+        /// </summary>
+        SHTDN_REASON_MAJOR_OTHER = 0x00000000,
+
+        /// <summary>
+        /// Power failure.
+        /// </summary>
+        SHTDN_REASON_MAJOR_POWER = 0x00060000,
+
+        /// <summary>
+        /// Software issue.
+        /// </summary>
+        SHTDN_REASON_MAJOR_SOFTWARE = 0x00030000,
+
+        /// <summary>
+        /// System failure.
+        /// </summary>
+        SHTDN_REASON_MAJOR_SYSTEM = 0x00050000,
+
+        /// <summary>
+        /// Blue screen crash event.
+        /// </summary>
+        SHTDN_REASON_MINOR_BLUESCREEN = 0x0000000F,
+
+        /// <summary>
+        /// Unplugged.
+        /// </summary>
+        SHTDN_REASON_MINOR_CORDUNPLUGGED = 0x0000000B,
+
+        /// <summary>
+        /// Disk.
+        /// </summary>
+        SHTDN_REASON_MINOR_DISK = 0x00000007,
+
+        /// <summary>
+        /// Environment.
+        /// </summary>
+        SHTDN_REASON_MINOR_ENVIRONMENT = 0x0000000C,
+
+        /// <summary>
+        /// Driver.
+        /// </summary>
+        SHTDN_REASON_MINOR_HARDWARE_DRIVER = 0x0000000D,
+
+        /// <summary>
+        /// Hot fix.
+        /// </summary>
+        SHTDN_REASON_MINOR_HOTFIX = 0x00000011,
+
+        /// <summary>
+        /// Hot fix uninstallation.
+        /// </summary>
+        SHTDN_REASON_MINOR_HOTFIX_UNINSTALL = 0x00000017,
+
+        /// <summary>
+        /// Unresponsive.
+        /// </summary>
+        SHTDN_REASON_MINOR_HUNG = 0x00000005,
+
+        /// <summary>
+        /// Installation.
+        /// </summary>
+        SHTDN_REASON_MINOR_INSTALLATION = 0x00000002,
+
+        /// <summary>
+        /// Maintenance.
+        /// </summary>
+        SHTDN_REASON_MINOR_MAINTENANCE = 0x00000001,
+
+        /// <summary>
+        /// MMC issue.
+        /// </summary>
+        SHTDN_REASON_MINOR_MMC = 0x00000019,
+
+        /// <summary>
+        /// Network connectivity.
+        /// </summary>
+        SHTDN_REASON_MINOR_NETWORK_CONNECTIVITY = 0x00000014,
+
+        /// <summary>
+        /// Network card.
+        /// </summary>
+        SHTDN_REASON_MINOR_NETWORKCARD = 0x00000009,
+
+        /// <summary>
+        /// Other issue.
+        /// </summary>
+        SHTDN_REASON_MINOR_OTHER = 0x00000000,
+
+        /// <summary>
+        /// Other driver event.
+        /// </summary>
+        SHTDN_REASON_MINOR_OTHERDRIVER = 0x0000000e,
+
+        /// <summary>
+        /// Power supply.
+        /// </summary>
+        SHTDN_REASON_MINOR_POWER_SUPPLY = 0x0000000a,
+
+        /// <summary>
+        /// Processor.
+        /// </summary>
+        SHTDN_REASON_MINOR_PROCESSOR = 0x00000008,
+
+        /// <summary>
+        /// Reconfigure.
+        /// </summary>
+        SHTDN_REASON_MINOR_RECONFIG = 0x00000004,
+
+        /// <summary>
+        /// Security issue.
+        /// </summary>
+        SHTDN_REASON_MINOR_SECURITY = 0x00000013,
+
+        /// <summary>
+        /// Security patch.
+        /// </summary>
+        SHTDN_REASON_MINOR_SECURITYFIX = 0x00000012,
+
+        /// <summary>
+        /// Security patch uninstallation.
+        /// </summary>
+        SHTDN_REASON_MINOR_SECURITYFIX_UNINSTALL = 0x00000018,
+
+        /// <summary>
+        /// Service pack.
+        /// </summary>
+        SHTDN_REASON_MINOR_SERVICEPACK = 0x00000010,
+
+        /// <summary>
+        /// Service pack uninstallation.
+        /// </summary>
+        SHTDN_REASON_MINOR_SERVICEPACK_UNINSTALL = 0x00000016,
+
+        /// <summary>
+        /// Terminal Services.
+        /// </summary>
+        SHTDN_REASON_MINOR_TERMSRV = 0x00000020,
+
+        /// <summary>
+        /// Unstable.
+        /// </summary>
+        SHTDN_REASON_MINOR_UNSTABLE = 0x00000006,
+
+        /// <summary>
+        /// Upgrade.
+        /// </summary>
+        SHTDN_REASON_MINOR_UPGRADE = 0x00000003,
+
+        /// <summary>
+        /// WMI issue.
+        /// </summary>
+        SHTDN_REASON_MINOR_WMI = 0x00000015,
+
+        /// <summary>
+        /// The reason code is defined by the user.
+        /// For more information, see Defining a Custom Reason Code.
+        /// If this flag is not present, the reason code is defined by the system.
+        /// </summary>
+        SHTDN_REASON_FLAG_USER_DEFINED = 0x40000000,
+
+        /// <summary>
+        /// The shutdown was planned. The system generates a System State Data (SSD) file.
+        /// This file contains system state information such as the processes, threads, memory usage, and configuration.
+        /// If this flag is not present, the shutdown was unplanned.
+        /// Notification and reporting options are controlled by a set of policies.
+        /// For example, after logging in, the system displays a dialog box reporting the unplanned shutdown if the policy has been enabled.
+        /// An SSD file is created only if the SSD policy is enabled on the system.
+        /// The administrator can use Windows Error Reporting to send the SSD data to a central location, or to Microsoft.
+        /// </summary>
+        SHTDN_REASON_FLAG_PLANNED = 0x80000000,
+    }
+}
