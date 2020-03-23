0737b4f8cde7f46860a3fe920e0a0ac1fa205e78
Shaojun Li <i@shaojun.li>
2020-03-23 20:28:05 +0800

    Add IsBadStringPtr

--- Src/Lsj.Util.Win32/Kernel32.cs
+++ Src/Lsj.Util.Win32/Kernel32.cs
@@ -207,6 +207,56 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Verifies that the calling process has read access to the specified range of memory.
+        /// Important
+        /// This function is obsolete and should not be used. Despite its name,
+        /// it does not guarantee that the pointer is valid or that the memory pointed to is safe to use.
+        /// For more information, see Remarks on this page.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winbase/nf-winbase-isbadstringptrw
+        /// </para>
+        /// </summary>
+        /// <param name="lpsz">
+        /// A pointer to a null-terminated string, either Unicode or ASCII.
+        /// </param>
+        /// <param name="ucchMax">
+        /// The maximum size of the string, in TCHARs.
+        /// The function checks for read access in all characters up to the string's terminating null character or
+        /// up to the number of characters specified by this parameter, whichever is smaller.
+        /// If this parameter is zero, the return value is <see cref="BOOL.FALSE"/>.
+        /// </param>
+        /// <returns>
+        /// If the calling process has read access to all characters up to the string's terminating null character
+        /// or up to the number of characters specified by <paramref name="ucchMax"/>, the return value is <see cref="BOOL.FALSE"/>.
+        /// If the calling process does not have read access to all characters up to the string's terminating null character
+        /// or up to the number of characters specified by <paramref name="ucchMax"/>, the return value is <see cref="BOOL.TRUE"/>.
+        /// If the application is compiled as a debugging version, and the process does not have read access to the entire memory range specified,
+        /// the function causes an assertion and breaks into the debugger.
+        /// Leaving the debugger, the function continues as usual, and returns a <see cref="BOOL.FALSE"/> value This behavior is by design,
+        /// as a debugging aid.
+        /// </returns>
+        /// <remarks>
+        /// This function is typically used when working with pointers returned from third-party libraries,
+        /// where you cannot determine the memory management behavior in the third-party DLL.
+        /// Threads in a process are expected to cooperate in such a way that one will not free memory that the other needs.
+        /// Use of this function does not negate the need to do this.
+        /// If this is not done, the application may fail in an unpredictable manner.
+        /// Dereferencing potentially invalid pointers can disable stack expansion in other threads.
+        /// A thread exhausting its stack, when stack expansion has been disabled, results in the immediate termination of the parent process,
+        /// with no pop-up error window or diagnostic information.
+        /// If the calling process has read access to some, but not all, of the specified memory range, the return value is <see cref="BOOL.FALSE"/>.
+        /// In a preemptive multitasking environment, it is possible for some other thread to change the process's access to the memory being tested.
+        /// Even when the function indicates that the process has read access to the specified memory,
+        /// you should use structured exception handling when attempting to access the memory.
+        /// Use of structured exception handling enables the system to notify the process if an access violation exception occurs,
+        /// giving the process an opportunity to handle the exception.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "IsBadStringPtrW", SetLastError = true)]
+        public static extern BOOL IsBadStringPtr([In]IntPtr lpsz, UINT_PTR ucchMax);
+
+        /// <summary>
+        /// <para>
         /// Verifies that the calling process has write access to the specified range of memory.
         /// Important This function is obsolete and should not be used. Despite its name,
         /// it does not guarantee that the pointer is valid or that the memory pointed to is safe to use.
