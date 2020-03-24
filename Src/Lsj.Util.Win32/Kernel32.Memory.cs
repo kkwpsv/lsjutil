764a32a6c14893c6b67525ded8db23c3e92645a1
Shaojun Li <i@shaojun.li>
2020-03-24 14:26:22 +0800

    Add GlobalUnlock

--- Src/Lsj.Util.Win32/Kernel32.Memory.cs
+++ Src/Lsj.Util.Win32/Kernel32.Memory.cs
@@ -239,6 +239,46 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Decrements the lock count associated with a memory object that was allocated with <see cref="GMEM_MOVEABLE"/>.
+        /// This function has no effect on memory objects allocated with <see cref="GMEM_FIXED"/>.
+        /// Note The global functions have greater overhead and provide fewer features than other memory management functions.
+        /// New applications should use the heap functions unless documentation states that a global function should be used.
+        /// For more information, see Global and Local Functions.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winbase/nf-winbase-globalunlock
+        /// </para>
+        /// </summary>
+        /// <param name="hMem">
+        /// A handle to the global memory object.
+        /// This handle is returned by either the <see cref="GlobalAlloc"/> or <see cref="GlobalReAlloc"/> function.
+        /// </param>
+        /// <returns>
+        /// If the memory object is still locked after decrementing the lock count, the return value is a <see cref="BOOL.TRUE"/> value.
+        /// If the memory object is unlocked after decrementing the lock count,
+        /// the function returns <see cref="BOOL.FALSE"/> and <see cref="GetLastError"/> returns <see cref="NO_ERROR"/>.
+        /// If the function fails, the return value is <see cref="BOOL.FALSE"/> and
+        /// <see cref="GetLastError"/> returns a value other than <see cref="NO_ERROR"/>.
+        /// </returns>
+        /// <remarks>
+        /// The internal data structures for each memory object include a lock count that is initially zero.
+        /// For movable memory objects, the <see cref="GlobalLock"/> function increments the count by one,
+        /// and <see cref="GlobalUnlock"/> decrements the count by one.
+        /// For each call that a process makes to <see cref="GlobalLock"/> for an object, it must eventually call <see cref="GlobalUnlock"/>.
+        /// Locked memory will not be moved or discarded, unless the memory object is reallocated by using the <see cref="GlobalReAlloc"/> function.
+        /// The memory block of a locked memory object remains locked until its lock count is decremented to zero, at which time it can be moved or discarded.
+        /// Memory objects allocated with <see cref="GMEM_FIXED"/> always have a lock count of zero.
+        /// If the specified memory block is fixed memory, this function returns <see cref="BOOL.TRUE"/>.
+        /// If the memory object is already unlocked, <see cref="GlobalUnlock"/> returns <see cref="BOOL.FALSE"/>
+        /// and <see cref="GetLastError"/> reports <see cref="ERROR_NOT_LOCKED"/>.
+        /// A process should not rely on the return value to determine the number of times
+        /// it must subsequently call <see cref="GlobalUnlock"/> for a memory object.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GlobalUnlock", SetLastError = true)]
+        public static extern BOOL GlobalUnlock([In]HGLOBAL hMem);
+
+        /// <summary>
+        /// <para>
         /// Allocates a block of memory from a heap. The allocated memory is not movable.
         /// </para>
         /// <para>
