87c69d331b8742aecf4a64bfc098cb7c87332056
Shaojun Li <i@shaojun.li>
2020-03-24 14:18:11 +0800

    Add GlobalFree

--- Src/Lsj.Util.Win32/Kernel32.Memory.cs
+++ Src/Lsj.Util.Win32/Kernel32.Memory.cs
@@ -156,6 +156,43 @@ namespace Lsj.Util.Win32
         public static HGLOBAL GlobalDiscard(HGLOBAL h) => GlobalReAlloc(h, 0, GMEM_MOVEABLE);
 
         /// <summary>
+        /// <para>
+        /// Frees the specified global memory object and invalidates its handle.
+        /// Note The global functions have greater overhead and provide fewer features than other memory management functions.
+        /// New applications should use the heap functions unless documentation states that a global function should be used.
+        /// For more information, see Global and Local Functions.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winbase/nf-winbase-globalfree
+        /// </para>
+        /// </summary>
+        /// <param name="hMem">
+        /// A handle to the global memory object.
+        /// This handle is returned by either the <see cref="GlobalAlloc"/> or <see cref="GlobalReAlloc"/> function.
+        /// It is not safe to free memory allocated with <see cref="LocalAlloc"/>.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see cref="IntPtr.Zero"/>.
+        /// If the function fails, the return value is equal to a handle to the global memory object.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// If the process examines or modifies the memory after it has been freed,
+        /// heap corruption may occur or an access violation exception (EXCEPTION_ACCESS_VIOLATION) may be generated.
+        /// The <see cref="GlobalFree"/> function will free a locked memory object.
+        /// A locked memory object has a lock count greater than zero.
+        /// The <see cref="GlobalLock"/> function locks a global memory object and increments the lock count by one.
+        /// The <see cref="GlobalUnlock"/> function unlocks it and decrements the lock count by one.
+        /// To get the lock count of a global memory object, use the <see cref="GlobalFlags"/> function.
+        /// If an application is running under a debug version of the system,
+        /// <see cref="GlobalFree"/> will issue a message that tells you that a locked object is being freed.
+        /// If you are debugging the application, <see cref="GlobalFree"/> will enter a breakpoint just before freeing a locked object.
+        /// This allows you to verify the intended behavior, then continue execution.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GlobalFree", SetLastError = true)]
+        public static extern HGLOBAL GlobalFree(HGLOBAL hMem);
+
+        /// <summary>
         /// Locks a global memory object and returns a pointer to the first byte of the object's memory block.
         /// </summary>
         /// <param name="hMem">
