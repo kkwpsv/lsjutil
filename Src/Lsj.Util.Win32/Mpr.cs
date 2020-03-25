734464fa9b5bb38ba41e163b25a83fa53c424491
Shaojun Li <i@shaojun.li>
2020-03-25 15:32:29 +0800

    Add WNetCancelConnection

--- Src/Lsj.Util.Win32/Mpr.cs
+++ Src/Lsj.Util.Win32/Mpr.cs
@@ -75,5 +75,57 @@ namespace Lsj.Util.Win32
         [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "WNetAddConnectionW", SetLastError = true)]
         public static extern SystemErrorCodes WNetAddConnection([MarshalAs(UnmanagedType.LPWStr)][In]string lpRemoteName,
             [MarshalAs(UnmanagedType.LPWStr)][In]string lpPassword, [MarshalAs(UnmanagedType.LPWStr)][In]string lpLocalName);
+
+        /// <summary>
+        /// <para>
+        /// The <see cref="WNetCancelConnection"/> function cancels an existing network connection.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winnetwk/nf-winnetwk-wnetcancelconnectionw
+        /// </para>
+        /// </summary>
+        /// <param name="lpName">
+        /// Pointer to a constant null-terminated string that specifies the name of either the redirected local device
+        /// or the remote network resource to disconnect from.
+        /// When this parameter specifies a redirected local device, the function cancels only the specified device redirection.
+        /// If the parameter specifies a remote network resource, only the connections to remote networks without devices are canceled.
+        /// </param>
+        /// <param name="fForce">
+        /// Specifies whether or not the disconnection should occur if there are open files or jobs on the connection.
+        /// If this parameter is <see cref="BOOL.FALSE"/>, the function fails if there are open files or jobs.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see cref="NO_ERROR"/>.
+        /// If the function fails, the return value is a system error code, such as one of the following values.
+        /// <see cref="ERROR_BAD_PROFILE"/>: The user profile is in an incorrect format.
+        /// <see cref="ERROR_CANNOT_OPEN_PROFILE"/>: The system is unable to open the user profile to process persistent connections.
+        /// <see cref="ERROR_DEVICE_IN_USE"/>: The device is in use by an active process and cannot be disconnected.
+        /// <see cref="ERROR_EXTENDED_ERROR"/>:
+        /// A network-specific error occurred. To obtain a description of the error, call the <see cref="WNetGetLastError"/> function.
+        /// <see cref="ERROR_NOT_CONNECTED"/>:
+        /// The name specified by the <paramref name="lpName"/> parameter is not a redirected device, or the system is not currently connected
+        /// to the device specified by the parameter.
+        /// <see cref="ERROR_OPEN_FILES"/>:
+        /// There are open files, and the <paramref name="fForce"/> parameter is <see cref="BOOL.FALSE"/>.
+        /// </returns>
+        /// <remarks>
+        /// Windows Server 2003 and Windows XP:
+        /// The WNet functions create and delete network drive letters in the MS-DOS device namespace associated with a logon session
+        /// because MS-DOS devices are identified by AuthenticationID.
+        /// (An AuthenticationID is the locally unique identifier, or LUID, associated with a logon session.)
+        /// This can affect applications that call one of the WNet functions to create a network drive letter under one user logon,
+        /// but query for existing network drive letters under a different user logon.
+        /// An example of this situation could be when a user's second logon is created within a logon session, for example,
+        /// by calling the <see cref="CreateProcessAsUser"/> function, and the second logon runs an application
+        /// that calls the <see cref="GetLogicalDrives"/> function.
+        /// <see cref="GetLogicalDrives"/> does not return network drive letters created by a WNet function under the first logon.
+        /// Note that in the preceding example the first logon session still exists, and the example could apply to any logon session,
+        /// including a Terminal Services session.
+        /// For more information, see Defining an MS-DOS Device Name.
+        /// </remarks>
+        [Obsolete("The WNetCancelConnection function is provided for compatibility with 16-bit versions of Windows." +
+            " Other Windows-based applications should call the WNetCancelConnection2 function.")]
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "WNetCancelConnectionW", SetLastError = true)]
+        public static extern SystemErrorCodes WNetCancelConnection([MarshalAs(UnmanagedType.LPWStr)][In]string lpName, [In]BOOL fForce);
     }
 }
