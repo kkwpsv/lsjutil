b7ad2216e3ef78fcd018d65d61ed6a4108b0b40c
Shaojun Li <i@shaojun.li>
2020-03-25 15:55:41 +0800

    Add FindResource

---
+++ Src/Lsj.Util.Win32/BaseTypes/HRSRC.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.BaseTypes
+{
+    /// <summary>
+    /// <para>
+    /// A handle to a resource.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/winprog/windows-data-types
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct HRSRC
+    {
+        private HANDLE _value;
+
+        /// <inheritdoc/>
+        public override string ToString() => _value.ToString();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator HANDLE(HRSRC val) => val._value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator HRSRC(HANDLE val) => new HRSRC { _value = val };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator IntPtr(HRSRC val) => val._value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator HRSRC(IntPtr val) => new HRSRC { _value = val };
+    }
+}
