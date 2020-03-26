3b72eeaaebe786cad3e78123a01eeb835f8ae713
Shaojun Li <i@shaojun.li>
2020-03-26 14:10:50 +0800

    Add GetPrivateProfileInt

--- Src/Lsj.Util.Win32/Kernel32.Profile.cs
+++ Src/Lsj.Util.Win32/Kernel32.Profile.cs
@@ -10,6 +10,81 @@ namespace Lsj.Util.Win32
     {
         /// <summary>
         /// <para>
+        /// Retrieves an integer associated with a key in the specified section of an initialization file.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winbase/nf-winbase-getprivateprofileintw
+        /// </para>
+        /// </summary>
+        /// <param name="lpAppName">
+        /// The name of the section in the initialization file.
+        /// </param>
+        /// <param name="lpKeyName">
+        /// The name of the key whose value is to be retrieved.
+        /// This value is in the form of a string; the <see cref="GetPrivateProfileInt"/> function converts the string into an integer and returns the integer.
+        /// </param>
+        /// <param name="nDefault">
+        /// The default value to return if the key name cannot be found in the initialization file.
+        /// </param>
+        /// <param name="lpFileName">
+        /// The name of the initialization file.
+        /// If this parameter does not contain a full path to the file, the system searches for the file in the Windows directory.
+        /// </param>
+        /// <returns>
+        /// The return value is the integer equivalent of the string following the specified key name in the specified initialization file.
+        /// If the key is not found, the return value is the specified default value.
+        /// </returns>
+        /// <remarks>
+        /// The function searches the file for a key that matches the name specified by the <paramref name="lpKeyName"/> parameter
+        /// under the section name specified by the <paramref name="lpAppName"/> parameter.
+        /// A section in the initialization file must have the following form:
+        /// <code>
+        /// [section]
+        /// key=value
+        /// .
+        /// .
+        /// .
+        /// </code>
+        /// The <see cref="GetPrivateProfileInt"/> function is not case-sensitive;
+        /// the strings in <paramref name="lpAppName"/> and <paramref name="lpKeyName"/> can be a combination of uppercase and lowercase letters.
+        /// An application can use the <see cref="GetProfileInt"/> function to retrieve an integer value from the Win.ini file.
+        /// The system maps most .ini file references to the registry, using the mapping defined under the following registry key:
+        /// HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\IniFileMapping
+        /// This mapping is likely if an application modifies system-component initialization files, such as Control.ini, System.ini, and Winfile.ini.
+        /// In these cases, the function retrieves information from the registry, not from the initialization file;
+        /// the change in the storage location has no effect on the function's behavior.
+        /// The profile functions use the following steps to locate initialization information:
+        /// Look in the registry for the name of the initialization file under the IniFileMapping key.
+        /// Look for the section name specified by <paramref name="lpAppName"/>.
+        /// This will be a named value under the key that has the name of the initialization file, or a subkey with this name,
+        /// or the name will not exist as either a value or subkey.
+        /// If the section name specified by <paramref name="lpAppName"/> is a named value,
+        /// then that value specifies where in the registry you will find the keys for the section
+        /// If the section name specified by <paramref name="lpAppName"/> is a subkey,
+        /// then named values under that subkey specify where in the registry you will find the keys for the section.
+        /// If the key you are looking for does not exist as a named value, then there will be an unnamed value (shown as &lt;No Name&gt;) that
+        /// specifies the default location in the registry where you will find the key.
+        /// If the section name specified by <paramref name="lpAppName"/> does not exist as a named value or as a subkey,
+        /// then there will be an unnamed value (shown as &lt;No Name&gt;) that specifies the default location in the registry
+        /// where you will find the keys for the section.
+        /// If there is no subkey or entry for the section name, then look for the actual initialization file on the disk and read its contents.
+        /// When looking at values in the registry that specify other registry locations,
+        /// there are several prefixes that change the behavior of the .ini file mapping:
+        /// ! - this character forces all writes to go both to the registry and to the .ini file on disk.
+        /// # - this character causes the registry value to be set to the value in the Windows 3.1 .ini file
+        /// when a new user logs in for the first time after setup.
+        /// @ - this character prevents any reads from going to the .ini file on disk if the requested data is not found in the registry.
+        /// USR: - this prefix stands for HKEY_CURRENT_USER, and the text after the prefix is relative to that key.
+        /// SYS: - this prefix stands for HKEY_LOCAL_MACHINE\SOFTWARE, and the text after the prefix is relative to that key.
+        /// </remarks>
+        [Obsolete("This function is provided only for compatibility with 16-bit Windows-based applications." +
+            "Applications should store initialization information in the registry.")]
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetPrivateProfileIntW", SetLastError = true)]
+        public static extern UINT GetPrivateProfileInt([MarshalAs(UnmanagedType.LPWStr)][In]string lpAppName,
+            [MarshalAs(UnmanagedType.LPWStr)][In]string lpKeyName, [In]INT nDefault, [MarshalAs(UnmanagedType.LPWStr)][In]string lpFileName);
+
+        /// <summary>
+        /// <para>
         /// Retrieves an integer from a key in the specified section of the Win.ini file.
         /// </para>
         /// <para>
