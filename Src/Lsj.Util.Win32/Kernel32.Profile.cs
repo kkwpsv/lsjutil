6ce726c37c053aa928c4ce5ede5d82fc102c6069
Shaojun Li <i@shaojun.li>
2020-03-26 14:01:55 +0800

    Add WriteProfileString

--- Src/Lsj.Util.Win32/Kernel32.Profile.cs
+++ Src/Lsj.Util.Win32/Kernel32.Profile.cs
@@ -128,7 +128,7 @@ namespace Lsj.Util.Win32
         /// Calls to profile functions may be mapped to the registry instead of to the initialization files.
         /// This mapping occurs when the initialization file and section are specified in the registry under the following key:
         /// HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\IniFileMapping
-        /// When the operation has been mapped, the <see cref="GetProfileString "/> function retrieves information from the registry,
+        /// When the operation has been mapped, the <see cref="GetProfileString"/> function retrieves information from the registry,
         /// not from the initialization file; the change in the storage location has no effect on the function's behavior.
         /// The profile functions use the following steps to locate initialization information:
         /// Look in the registry for the name of the initialization file under the IniFileMapping key.
@@ -160,5 +160,73 @@ namespace Lsj.Util.Win32
         public static extern DWORD GetProfileString([MarshalAs(UnmanagedType.LPWStr)][In]string lpAppName,
             [MarshalAs(UnmanagedType.LPWStr)][In]string lpKeyName, [MarshalAs(UnmanagedType.LPWStr)][In]string lpDefault,
             [MarshalAs(UnmanagedType.LPWStr)][Out]StringBuilder lpReturnedString, [In]DWORD nSize);
+
+        /// <summary>
+        /// <para>
+        /// Copies a string into the specified section of the Win.ini file.
+        /// If Win.ini uses Unicode characters, the function writes Unicode characters to the file.
+        /// Otherwise, the function writes ANSI characters.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winbase/nf-winbase-writeprofilestringw
+        /// </para>
+        /// </summary>
+        /// <param name="lpAppName">
+        /// The section to which the string is to be copied. If the section does not exist, it is created.
+        /// The name of the section is not case-sensitive; the string can be any combination of uppercase and lowercase letters.
+        /// </param>
+        /// <param name="lpKeyName">
+        /// The key to be associated with the string.
+        /// If the key does not exist in the specified section, it is created.
+        /// If this parameter is <see langword="null"/>, the entire section, including all entries in the section, is deleted.
+        /// </param>
+        /// <param name="lpString">
+        /// A null-terminated string to be written to the file.
+        /// If this parameter is <see langword="null"/>, the key pointed to by the <paramref name="lpKeyName"/> parameter is deleted.
+        /// </param>
+        /// <returns>
+        /// If the function successfully copies the string to the Win.ini file, the return value is <see langword="true"/>.
+        /// If the function fails, or if it flushes the cached version of Win.ini, the return value is <see langword="false"/>.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// A section in the Win.ini file must have the following form: key=string.
+        /// The system keeps a cached version of the most recent registry file mapping to improve performance.
+        /// If all parameters are <see langword="null"/>, the function flushes the cache.
+        /// While the system is editing the cached version of the file, processes that edit the file itself will use the original file
+        /// until the cache has been cleared.
+        /// The system maps most .ini file references to the registry, using the mapping defined under the following registry key:
+        /// HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\IniFileMapping
+        /// When the operation has been mapped, the <see cref="WriteProfileString"/> function retrieves information from the registry,
+        /// not from the initialization file; the change in the storage location has no effect on the function's behavior.
+        /// The profile functions use the following steps to locate initialization information:
+        /// Look in the registry for the name of the initialization file under the IniFileMapping key.
+        /// Look for the section name specified by <paramref name="lpAppName"/>.
+        /// This will be a named value under the key that has the name of the initialization file, or a subkey with this name,
+        /// or the name will not exist as either a value or subkey.
+        /// If the section name specified by <paramref name="lpAppName"/> is a named value,
+        /// then that value specifies where in the registry you will find the keys for the section
+        /// If the section name specified by <paramref name="lpAppName"/> is a subkey,
+        /// then named values under that subkey specify where in the registry you will find the keys for the section.
+        /// If the key you are looking for does not exist as a named value, then there will be an unnamed value (shown as &lt;No Name&gt;) that
+        /// specifies the default location in the registry where you will find the key.
+        /// If the section name specified by <paramref name="lpAppName"/> does not exist as a named value or as a subkey,
+        /// then there will be an unnamed value (shown as &lt;No Name&gt;) that specifies the default location in the registry
+        /// where you will find the keys for the section.
+        /// If there is no subkey or entry for the section name, then look for the actual initialization file on the disk and read its contents.
+        /// When looking at values in the registry that specify other registry locations,
+        /// there are several prefixes that change the behavior of the .ini file mapping:
+        /// ! - this character forces all writes to go both to the registry and to the .ini file on disk.
+        /// # - this character causes the registry value to be set to the value in the Windows 3.1 .ini file
+        /// when a new user logs in for the first time after setup.
+        /// @ - this character prevents any reads from going to the .ini file on disk if the requested data is not found in the registry.
+        /// USR: - this prefix stands for HKEY_CURRENT_USER, and the text after the prefix is relative to that key.
+        /// SYS: - this prefix stands for HKEY_LOCAL_MACHINE\SOFTWARE, and the text after the prefix is relative to that key.
+        /// </remarks>
+        [Obsolete("This function is provided only for compatibility with 16-bit Windows-based applications," +
+            "therefore this function should not be called from server code. Applications should store initialization information in the registry.")]
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "WriteProfileStringW", SetLastError = true)]
+        public static extern BOOL WriteProfileString([MarshalAs(UnmanagedType.LPWStr)][In]string lpAppName,
+            [MarshalAs(UnmanagedType.LPWStr)][In]string lpKeyName, [MarshalAs(UnmanagedType.LPWStr)][In]string lpString);
     }
 }
