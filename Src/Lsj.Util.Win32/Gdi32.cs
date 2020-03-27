1279918b5a180f83d1b0971fdb3c5ec09fb27fa1
Shaojun Li <i@shaojun.li>
2020-03-27 13:02:18 +0800

    Add SetMapMode

--- Src/Lsj.Util.Win32/Gdi32.cs
+++ Src/Lsj.Util.Win32/Gdi32.cs
@@ -876,5 +876,55 @@ namespace Lsj.Util.Win32
         public static extern BoundsAccumulationFlags SetBoundsRect([In]HDC hdc,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(StructPointerOrNullObjectMarshaler<RECT>))][In]StructPointerOrNullObject<RECT> lprect,
             [In]BoundsAccumulationFlags flags);
+
+        /// <summary>
+        /// <para>
+        /// The SetMapMode function sets the mapping mode of the specified device context.
+        /// The mapping mode defines the unit of measure used to transform page-space units into device-space units,
+        /// and also defines the orientation of the device's x and y axes.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-setmapmode
+        /// </para>
+        /// </summary>
+        /// <param name="hdc">
+        /// A handle to the device context.
+        /// </param>
+        /// <param name="iMode">
+        /// The new mapping mode. This parameter can be one of the following values.
+        /// <see cref="MM_ANISOTROPIC"/>:
+        /// Logical units are mapped to arbitrary units with arbitrarily scaled axes.
+        /// Use the <see cref="SetWindowExtEx"/> and <see cref="SetViewportExtEx"/> functions to specify the units, orientation, and scaling.
+        /// <see cref="MM_HIENGLISH"/>:
+        /// Each logical unit is mapped to 0.001 inch. Positive x is to the right; positive y is up.
+        /// <see cref="MM_HIMETRIC"/>:
+        /// Each logical unit is mapped to 0.01 millimeter. Positive x is to the right; positive y is up.
+        /// <see cref="MM_ISOTROPIC"/>:
+        /// Logical units are mapped to arbitrary units with equally scaled axes; that is, one unit along the x-axis is equal to one unit along the y-axis.
+        /// Use the <see cref="SetWindowExtEx"/> and <see cref="SetViewportExtEx"/> functions to specify the units and the orientation of the axes.
+        /// Graphics device interface (GDI) makes adjustments as necessary to ensure the x and y units remain the same size
+        /// (When the window extent is set, the viewport will be adjusted to keep the units isotropic).
+        /// <see cref="MM_LOENGLISH"/>:
+        /// Each logical unit is mapped to 0.01 inch. Positive x is to the right; positive y is up.
+        /// <see cref="MM_LOMETRIC"/>:
+        /// Each logical unit is mapped to 0.1 millimeter. Positive x is to the right; positive y is up.
+        /// <see cref="MM_TEXT"/>:
+        /// Each logical unit is mapped to one device pixel. Positive x is to the right; positive y is down.
+        /// <see cref="MM_TWIPS"/>:
+        /// Each logical unit is mapped to one twentieth of a printer's point (1/1440 inch, also called a twip). Positive x is to the right; positive y is up.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value identifies the previous mapping mode.
+        /// If the function fails, the return value is zero.
+        /// </returns>
+        /// <remarks>
+        /// The <see cref="MM_TEXT"/> mode allows applications to work in device pixels, whose size varies from device to device.
+        /// The <see cref="MM_HIENGLISH"/>, <see cref="MM_HIMETRIC"/>, <see cref="MM_LOENGLISH"/>, <see cref="MM_LOMETRIC"/>,
+        /// and <see cref="MM_TWIPS"/> modes are useful for applications drawing in physically meaningful units (such as inches or millimeters).
+        /// The <see cref="MM_ISOTROPIC"/> mode ensures a 1:1 aspect ratio.
+        /// The <see cref="MM_ANISOTROPIC"/> mode allows the x-coordinates and y-coordinates to be adjusted independently.
+        /// </remarks>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "SetMapMode", SetLastError = true)]
+        public static extern int SetMapMode([In]HDC hdc, [In]int iMode);
     }
 }
