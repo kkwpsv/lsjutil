060669e3453d685dbec3a5ef0eef25f036adda70
Shaojun Li <i@shaojun.li>
2020-03-28 17:13:19 +0800

    Fix def

--- Src/Lsj.Util.Win32/Gdi32.cs
+++ Src/Lsj.Util.Win32/Gdi32.cs
@@ -224,35 +224,6 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
-        /// The <see cref="CreateSolidBrush"/> function creates a logical brush that has the specified solid color.
-        /// </para>
-        /// <para>
-        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-createsolidbrush
-        /// </para>
-        /// </summary>
-        /// <param name="color">
-        /// The color of the brush. To create a COLORREF color value, use the <see cref="RGB"/> macro.
-        /// </param>
-        /// <returns>
-        /// If the function succeeds, the return value identifies a logical brush.
-        /// If the function fails, the return value is <see cref="IntPtr.Zero"/>.
-        /// </returns>
-        /// <remarks>
-        /// When you no longer need the HBRUSH object, call the <see cref="DeleteObject"/> function to delete it.
-        /// A solid brush is a bitmap that the system uses to paint the interiors of filled shapes.
-        /// After an application creates a brush by calling <see cref="CreateSolidBrush"/>,
-        /// it can select that brush into any device context by calling the <see cref="SelectObject"/> function.
-        /// To paint with a system color brush, an application should use <see cref="GetSysColorBrush"/> (nIndex)
-        /// instead of <see cref="CreateSolidBrush"/>(<see cref="GetSysColor"/>(nIndex)),
-        /// because <see cref="GetSysColorBrush"/> returns a cached brush instead of allocating a new one.
-        /// ICM: No color management is done at brush creation.
-        /// However, color management is performed when the brush is selected into an ICM-enabled device context.
-        /// </remarks>
-        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "CreateSolidBrush", SetLastError = true)]
-        public static extern IntPtr CreateSolidBrush([In]uint color);
-
-        /// <summary>
-        /// <para>
         /// The <see cref="DeleteDC"/> function deletes the specified device context (DC).
         /// </para>
         /// <para>
