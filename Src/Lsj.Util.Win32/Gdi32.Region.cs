37f209f0e1250fea85f5d2fcabf8c3a40447d38d
Shaojun Li <i@shaojun.li>
2020-03-28 18:38:41 +0800

    Add CreateEllipticRgn

--- Src/Lsj.Util.Win32/Gdi32.Region.cs
+++ Src/Lsj.Util.Win32/Gdi32.Region.cs
@@ -11,6 +11,39 @@ namespace Lsj.Util.Win32
     {
         /// <summary>
         /// <para>
+        /// The <see cref="CreateEllipticRgn"/> function creates an elliptical region.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-createellipticrgn
+        /// </para>
+        /// </summary>
+        /// <param name="x1">
+        /// Specifies the x-coordinate in logical units, of the upper-left corner of the bounding rectangle of the ellipse.
+        /// </param>
+        /// <param name="y1">
+        /// Specifies the y-coordinate in logical units, of the upper-left corner of the bounding rectangle of the ellipse.
+        /// </param>
+        /// <param name="x2">
+        /// Specifies the x-coordinate in logical units, of the lower-right corner of the bounding rectangle of the ellipse.
+        /// </param>
+        /// <param name="y2">
+        /// Specifies the y-coordinate in logical units, of the lower-right corner of the bounding rectangle of the ellipse.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the handle to the region.
+        /// If the function fails, the return value is <see cref="NULL"/>.
+        /// </returns>
+        /// <remarks>
+        /// When you no longer need the <see cref="HRGN"/> object, call the <see cref="DeleteObject"/> function to delete it.
+        /// A bounding rectangle defines the size, shape, and orientation of the region:
+        /// The long sides of the rectangle define the length of the ellipse's major axis; the short sides define the length of the ellipse's minor axis;
+        /// and the center of the rectangle defines the intersection of the major and minor axes.
+        /// </remarks>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "CreateEllipticRgn", SetLastError = true)]
+        public static extern HRGN CreateEllipticRgn([In]int x1, [In]int y1, [In]int x2, [In]int y2);
+
+        /// <summary>
+        /// <para>
         /// The <see cref="CreateEllipticRgnIndirect"/> function creates an elliptical region.
         /// </para>
         /// <para>
