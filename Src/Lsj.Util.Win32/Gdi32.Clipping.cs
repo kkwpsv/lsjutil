929451484e341fdc410faeee1aa88fb69ee8e563
Shaojun Li <i@shaojun.li>
2020-03-29 17:44:19 +0800

    Add GetClipBox

--- Src/Lsj.Util.Win32/Gdi32.Clipping.cs
+++ Src/Lsj.Util.Win32/Gdi32.Clipping.cs
@@ -1,4 +1,5 @@
 ﻿using Lsj.Util.Win32.BaseTypes;
+using Lsj.Util.Win32.Structs;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -8,6 +9,33 @@ namespace Lsj.Util.Win32
 {
     public partial class Gdi32
     {
+        /// <summary>
+        /// <para>
+        /// The <see cref="GetClipBox"/> function retrieves the dimensions of the tightest bounding rectangle that can be drawn
+        /// around the current visible area on the device.
+        /// The visible area is defined by the current clipping region or clip path, as well as any overlapping windows.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-getclipbox
+        /// </para>
+        /// </summary>
+        /// <param name="hdc">
+        /// A handle to the device context.
+        /// </param>
+        /// <param name="lprect">
+        /// A pointer to a <see cref="RECT"/> structure that is to receive the rectangle dimensions, in logical units.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value specifies the clipping box's complexity and can be one of the following values.
+        /// <see cref="NULLREGION"/>: Region is empty.
+        /// <see cref="SIMPLEREGION"/>: Region is a single rectangle.
+        /// <see cref="COMPLEXREGION"/>: Region is more than one rectangle.
+        /// <see cref="ERROR"/>: An error occurred.
+        /// <see cref="GetClipBox"/> returns logical coordinates based on the given device context.
+        /// </returns>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetClipBox", SetLastError = true)]
+        public static extern int GetClipBox([In]HDC hdc, [Out]out RECT lprect);
+
         /// <summary>
         /// <para>
         /// The <see cref="SelectClipRgn"/> function selects a region as the current clipping region for the specified device context.
