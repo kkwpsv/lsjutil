c142d1f4ad14da5710a379d0aac9545a70fb77c7
Shaojun Li <i@shaojun.li>
2020-03-29 20:48:02 +0800

    Add GetTabbedTextExtent

--- Src/Lsj.Util.Win32/Gdi32.Text.cs
+++ Src/Lsj.Util.Win32/Gdi32.Text.cs
@@ -91,6 +91,55 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// The <see cref="GetTabbedTextExtent"/> function computes the width and height of a character string.
+        /// If the string contains one or more tab characters, the width of the string is based upon the specified tab stops.
+        /// The <see cref="GetTabbedTextExtent"/> function uses the currently selected font to compute the dimensions of the string.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-gettabbedtextextentw
+        /// </para>
+        /// </summary>
+        /// <param name="hdc">
+        /// A handle to the device context.
+        /// </param>
+        /// <param name="lpString">
+        /// A pointer to a character string.
+        /// </param>
+        /// <param name="chCount">
+        /// The length of the text string.
+        /// For the ANSI function it is a <see cref="BYTE"/> count and for the Unicode function it is a <see cref="WORD"/> count.
+        /// Note that for the ANSI function, characters in SBCS code pages take one byte each, while most characters in DBCS code pages take two bytes;
+        /// for the Unicode function, most currently defined Unicode characters (those in the Basic Multilingual Plane (BMP)) are one <see cref="WORD"/>
+        /// while Unicode surrogates are two <see cref="WORD"/>s.
+        /// </param>
+        /// <param name="nTabPositions">
+        /// The number of tab-stop positions in the array pointed to by the <paramref name="lpnTabStopPositions"/> parameter.
+        /// </param>
+        /// <param name="lpnTabStopPositions">
+        /// A pointer to an array containing the tab-stop positions, in device units.
+        /// The tab stops must be sorted in increasing order; the smallest x-value should be the first item in the array.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the dimensions of the string in logical units.
+        /// The height is in the high-order word and the width is in the low-order word.
+        /// If the function fails, the return value is 0.
+        /// <see cref="GetTabbedTextExtent"/> will fail if <paramref name="hdc"/> is invalid and if <paramref name="nTabPositions"/> is less than 0.
+        /// </returns>
+        /// <remarks>
+        /// The current clipping region does not affect the width and height returned by the <see cref="GetTabbedTextExtent"/> function.
+        /// Because some devices do not place characters in regular cell arrays (that is, they kern the characters),
+        /// the sum of the extents of the characters in a string may not be equal to the extent of the string.
+        /// If the <paramref name="nTabPositions"/> parameter is zero and the <paramref name="lpnTabStopPositions"/> parameter is <see langword="null"/>,
+        /// tabs are expanded to eight times the average character width.
+        /// If <paramref name="nTabPositions"/> is 1, the tab stops are separated by the distance specified
+        /// by the first value in the array to which <paramref name="lpnTabStopPositions"/> points.
+        /// </remarks>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetTabbedTextExtentW", SetLastError = true)]
+        public static extern DWORD GetTabbedTextExtent([In]HDC hdc, [MarshalAs(UnmanagedType.LPWStr)]string lpString, [In]int chCount,
+            [In]int nTabPositions, [MarshalAs(UnmanagedType.LPArray)][In]INT[] lpnTabStopPositions);
+
+        /// <summary>
+        /// <para>
         /// The <see cref="TabbedTextOut"/> function writes a character string at a specified location,
         /// expanding tabs to the values specified in an array of tab-stop positions.
         /// Text is written in the currently selected font, background color, and text color.
