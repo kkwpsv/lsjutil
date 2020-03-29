ffa7ddb4ca44b9e963894774e83868bafd9251e3
Shaojun Li <i@shaojun.li>
2020-03-29 19:40:44 +0800

    Add SetPolyFillMode

--- Src/Lsj.Util.Win32/Gdi32.Drawing.cs
+++ Src/Lsj.Util.Win32/Gdi32.Drawing.cs
@@ -515,5 +515,40 @@ namespace Lsj.Util.Win32
         /// </remarks>
         [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "RoundRect", SetLastError = true)]
         public static extern BOOL RoundRect([In]HDC hdc, [In]int left, [In]int top, [In]int right, [In]int bottom, [In]int width, [In]int height);
+
+        /// <summary>
+        /// <para>
+        /// The <see cref="SetPolyFillMode"/> function sets the polygon fill mode for functions that fill polygons.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-setpolyfillmode
+        /// </para>
+        /// </summary>
+        /// <param name="hdc">
+        /// A handle to the device context.
+        /// </param>
+        /// <param name="mode">
+        /// The new fill mode.
+        /// This parameter can be one of the following values.
+        /// <see cref="ALTERNATE"/>: Selects alternate mode (fills the area between odd-numbered and even-numbered polygon sides on each scan line).
+        /// <see cref="WINDING"/>: Selects winding mode (fills any region with a nonzero winding value).
+        /// </param>
+        /// <returns>
+        /// The return value specifies the previous filling mode.
+        /// If an error occurs, the return value is zero.
+        /// </returns>
+        /// <remarks>
+        /// In general, the modes differ only in cases where a complex, overlapping polygon must be filled
+        /// (for example, a five-sided polygon that forms a five-pointed star with a pentagon in the center).
+        /// In such cases, <see cref="ALTERNATE"/> mode fills every other enclosed region within the polygon (that is, the points of the star),
+        /// but <see cref="WINDING"/> mode fills all regions (that is, the points and the pentagon).
+        /// When the fill mode is <see cref="ALTERNATE"/>, GDI fills the area between odd-numbered and even-numbered polygon sides on each scan line.
+        /// That is, GDI fills the area between the first and second side, between the third and fourth side, and so on.
+        /// When the fill mode is <see cref="WINDING"/>, GDI fills any region that has a nonzero winding value.
+        /// This value is defined as the number of times a pen used to draw the polygon would go around the region.
+        /// The direction of each edge of the polygon is important.
+        /// </remarks>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "SetPolyFillMode", SetLastError = true)]
+        public static extern int SetPolyFillMode([In]HDC hdc, [In]int mode);
     }
 }
