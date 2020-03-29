e4cadddfc004ea7d77e0613e53070a6df6acb022
Shaojun Li <i@shaojun.li>
2020-03-29 17:48:05 +0800

    Add IntersectClipRect

--- Src/Lsj.Util.Win32/Gdi32.Clipping.cs
+++ Src/Lsj.Util.Win32/Gdi32.Clipping.cs
@@ -38,6 +38,45 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// The <see cref="IntersectClipRect"/> function creates a new clipping region from the intersection of the current clipping region
+        /// and the specified rectangle.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-intersectcliprect
+        /// </para>
+        /// </summary>
+        /// <param name="hdc">
+        /// A handle to the device context.
+        /// </param>
+        /// <param name="left">
+        /// The x-coordinate, in logical units, of the upper-left corner of the rectangle.
+        /// </param>
+        /// <param name="top">
+        /// The y-coordinate, in logical units, of the upper-left corner of the rectangle.
+        /// </param>
+        /// <param name="right">
+        /// The x-coordinate, in logical units, of the lower-right corner of the rectangle.
+        /// </param>
+        /// <param name="bottom">
+        /// The y-coordinate, in logical units, of the lower-right corner of the rectangle.
+        /// </param>
+        /// <returns>
+        /// The return value specifies the new clipping region's type and can be one of the following values.
+        /// <see cref="NULLREGION"/>: Region is empty.
+        /// <see cref="SIMPLEREGION"/>: Region is a single rectangle.
+        /// <see cref="COMPLEXREGION"/>: Region is more than one rectangle.
+        /// <see cref="ERROR"/>: An error occurred. (The current clipping region is unaffected.)
+        /// </returns>
+        /// <remarks>
+        /// The lower and right-most edges of the given rectangle are excluded from the clipping region.
+        /// If a clipping region does not already exist then the system may apply a default clipping region to the specified <see cref="HDC"/>.
+        /// A clipping region is then created from the intersection of that default clipping region and the rectangle specified in the function parameters.
+        /// </remarks>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetClipBox", SetLastError = true)]
+        public static extern int IntersectClipRect([In]HDC hdc, [In]int left, [In]int top, [In]int right, [In]int bottom);
+
+        /// <summary>
+        /// <para>
         /// The <see cref="SelectClipRgn"/> function selects a region as the current clipping region for the specified device context.
         /// </para>
         /// <para>
