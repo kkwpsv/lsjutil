d0f498ae396bfc3e0411be0427664a5aee1ab758
Shaojun Li <i@shaojun.li>
2020-04-01 14:17:59 +0800

    Add EndPage

--- Src/Lsj.Util.Win32/Gdi32.Print.cs
+++ Src/Lsj.Util.Win32/Gdi32.Print.cs
@@ -74,5 +74,41 @@ namespace Lsj.Util.Win32
         /// </remarks>
         [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "StartPage", SetLastError = true)]
         public static extern int StartPage([In]HDC hdc);
+
+        /// <summary>
+        /// <para>
+        /// The <see cref="EndPage"/> function notifies the device that the application has finished writing to a page.
+        /// This function is typically used to direct the device driver to advance to a new page.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-endpage
+        /// </para>
+        /// </summary>
+        /// <param name="hdc">
+        /// A handle to the device context for the print job.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is greater than zero.
+        /// If the function fails, the return value is less than or equal to zero.
+        /// </returns>
+        /// <remarks>
+        /// Note
+        /// This is a blocking or synchronous function and might not return immediately.
+        /// How quickly this function returns depends on run-time factors such as network status, print server configuration,
+        /// and printer driver implementation—factors that are difficult to predict when writing an application.
+        /// Calling this function from a thread that manages interaction with the user interface could make the application appear to be unresponsive.
+        /// Use the <see cref="ResetDC"/> function to change the device mode, if necessary, after calling the <see cref="EndPage"/> function.
+        /// Note that a call to <see cref="ResetDC"/> resets all device context attributes back to default values.
+        /// Neither <see cref="EndPage"/> nor <see cref="StartPage"/> resets the device context attributes.
+        /// Device context attributes remain constant across subsequent pages.
+        /// You do not need to re-select objects and set up the mapping mode again before printing the next page;
+        /// however, doing so will produce the same results and reduce code differences between versions of Windows.
+        /// When a page in a spooled file exceeds approximately 350 MB, it may fail to print and not send an error message.
+        /// For example, this can occur when printing large EMF files.
+        /// The page size limit depends on many factors including the amount of virtual memory available,
+        /// the amount of memory allocated by calling processes, and the amount of fragmentation in the process heap.
+        /// </remarks>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "EndPage", SetLastError = true)]
+        public static extern int EndPage([In]HDC hdc);
     }
 }
