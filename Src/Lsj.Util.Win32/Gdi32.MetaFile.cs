aed6bac64007a577189d164191d2d80d709705c3
Shaojun Li <i@shaojun.li>
2020-04-01 13:18:30 +0800

    Add CreateMetaFile

---
+++ Src/Lsj.Util.Win32/Gdi32.MetaFile.cs
@@ -0,0 +1,52 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Lsj.Util.Win32
+{
+    public partial class Gdi32
+    {
+        /// <summary>
+        /// <para>
+        /// The <see cref="CreateMetaFile"/> function creates a device context for a Windows-format metafile.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-createmetafilew
+        /// </para>
+        /// </summary>
+        /// <param name="pszFile">
+        /// A pointer to the file name for the Windows-format metafile to be created.
+        /// If this parameter is <see langword="null"/>, the Windows-format metafile is memory based and its contents are lost
+        /// when it is deleted by using the <see cref="DeleteMetaFile"/> function.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is a handle to the device context for the Windows-format metafile.
+        /// If the function fails, the return value is <see cref="NULL"/>.
+        /// </returns>
+        /// <remarks>
+        /// Where text arguments must use Unicode characters, use the <see cref="CreateMetaFile"/> function as a wide-character function.
+        /// Where text arguments must use characters from the Windows character set, use this function as an ANSI function.
+        /// <see cref="CreateMetaFile"/> is a Windows-format metafile function.
+        /// This function supports only 16-bit Windows-based applications, which are listed in Windows-Format Metafiles.
+        /// It does not record or play back GDI functions such as <see cref="PolyBezier"/>, which were not part of 16-bit Windows.
+        /// The device context created by this function can be used to record GDI output functions in a Windows-format metafile.
+        /// It cannot be used with GDI query functions such as <see cref="GetTextColor"/>.
+        /// When the device context is used with a GDI output function, the return value of that function
+        /// becomes <see cref="TRUE"/> if the function is recorded and <see cref="FALSE"/> otherwise.
+        /// When an object is selected by using the <see cref="SelectObject"/> function, only a copy of the object is recorded.
+        /// The object still belongs to the application.
+        /// To create a scalable Windows-format metafile, record the graphics output in the <see cref="MM_ANISOTROPIC"/> mapping mode.
+        /// The file cannot contain functions that modify the viewport origin and extents,
+        /// nor can it contain device-dependent functions such as the <see cref="SelectClipRgn"/> function.
+        /// Once created, the Windows metafile can be scaled and rendered to any output device-format
+        /// by defining the viewport origin and extents of the picture before playing it.
+        /// </remarks>
+        [Obsolete("This function is provided only for compatibility with Windows-format metafiles." +
+            "Enhanced-format metafiles provide superior functionality and are recommended for new applications." +
+            "The corresponding function for an enhanced-format metafile is CreateEnhMetaFile.")]
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "CreateMetaFileW", SetLastError = true)]
+        public static extern HDC CreateMetaFile([In]string pszFile);
+    }
+}
