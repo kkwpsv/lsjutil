43ddd2a323d7fad9582ed2efa07ce179ed7c0f82
Shaojun Li <i@shaojun.li>
2020-04-01 15:04:24 +0800

    Fix def

--- Src/Lsj.Util.Win32/User32.Window.cs
+++ Src/Lsj.Util.Win32/User32.Window.cs
@@ -1290,68 +1290,6 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
-        /// Retrieves or sets the value of one of the system-wide parameters.
-        /// This function can also update the user profile while setting a parameter.
-        /// </para>
-        /// <para>
-        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-systemparametersinfow
-        /// </para>
-        /// </summary>
-        /// <param name="uiAction">
-        /// The system-wide parameter to be retrieved or set.
-        /// </param>
-        /// <param name="uiParam">
-        /// A parameter whose usage and format depends on the system parameter being queried or set.
-        /// For more information about system-wide parameters, see the <paramref name="uiAction"/> parameter.
-        /// If not otherwise indicated, you must specify zero for this parameter.
-        /// </param>
-        /// <param name="pvParam">
-        /// A parameter whose usage and format depends on the system parameter being queried or set.
-        /// For more information about system-wide parameters, see the <paramref name="uiAction"/> parameter.
-        /// If not otherwise indicated, you must specify <see cref="NULL"/> for this parameter.
-        /// For information on the PVOID datatype, see Windows Data Types.
-        /// </param>
-        /// <param name="fWinIni">
-        /// If a system parameter is being set, specifies whether the user profile is to be updated, and if so,
-        /// whether the <see cref="WM_SETTINGCHANGE"/> message is to be broadcast to all top-level windows to notify them of the change.
-        /// This parameter can be zero if you do not want to update the user profile or broadcast the <see cref="WM_SETTINGCHANGE"/> message,
-        /// or it can be one or more of the following values.
-        /// <see cref="SPIF_UPDATEINIFILE"/>: Writes the new system-wide parameter setting to the user profile.
-        /// <see cref="SPIF_SENDCHANGE"/>: Broadcasts the <see cref="WM_SETTINGCHANGE"/> message after updating the user profile.
-        /// <see cref="SPIF_SENDWININICHANGE"/>: Same as <see cref="SPIF_SENDCHANGE"/>.
-        /// </param>
-        /// <returns>
-        /// If the function succeeds, the return value is a <see cref="TRUE"/> value.
-        /// If the function fails, the return value is <see cref="FALSE"/>.
-        /// To get extended error information, call <see cref="GetLastError"/>.
-        /// </returns>
-        /// <remarks>
-        /// This function is intended for use with applications that allow the user to customize the environment.
-        /// A keyboard layout name should be derived from the hexadecimal value of the language identifier corresponding to the layout.
-        /// For example, U.S. English has a language identifier of 0x0409, so the primary U.S. English layout is named "00000409".
-        /// Variants of U.S. English layout, such as the Dvorak layout, are named "00010409", "00020409" and so on.
-        /// For a list of the primary language identifiers and sublanguage identifiers that make up a language identifier, see the <see cref="MAKELANGID"/> macro.
-        /// There is a difference between the High Contrast color scheme and the High Contrast Mode.
-        /// The High Contrast color scheme changes the system colors to colors that have obvious contrast;
-        /// you switch to this color scheme by using the Display Options in the control panel.
-        /// The High Contrast Mode, which uses <see cref="SPI_GETHIGHCONTRAST"/> and <see cref="SPI_SETHIGHCONTRAST"/>,
-        /// advises applications to modify their appearance for visually-impaired users.
-        /// It involves such things as audible warning to users and customized color scheme (using the Accessibility Options in the control panel).
-        /// For more information, see HIGHCONTRAST. For more information on general accessibility features, see Accessibility.
-        /// During the time that the primary button is held down to activate the Mouse ClickLock feature, the user can move the mouse.
-        /// After the primary button is locked down, releasing the primary button does not result in a <see cref="WM_LBUTTONUP"/> message.
-        /// Thus, it will appear to an application that the primary button is still down.
-        /// Any subsequent button message releases the primary button, sending a <see cref="WM_LBUTTONUP"/> message to the application,
-        /// thus the button can be unlocked programmatically or through the user clicking any button.
-        /// This API is not DPI aware, and should not be used if the calling thread is per-monitor DPI aware.
-        /// For the DPI-aware version of this API, see <see cref="SystemParametersInfoForDPI"/>.
-        /// For more information on DPI awareness, see the Windows High DPI documentation.
-        /// </remarks>
-        [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "SystemParametersInfoW", SetLastError = true)]
-        public static extern BOOL SystemParametersInfo([In]SystemParametersInfoParameters uiAction, [In]UINT uiParam, [In]PVOID pvParam, [In]UINT fWinIni);
-
-        /// <summary>
-        /// <para>
         /// Updates the position, size, shape, content, and translucency of a layered window.
         /// </para>
         /// <para>
