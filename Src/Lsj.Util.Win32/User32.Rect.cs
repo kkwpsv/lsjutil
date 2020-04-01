6d5970253cb19cf548f1f7b690ed0f97653dc3cc
Shaojun Li <i@shaojun.li>
2020-04-01 17:03:05 +0800

    Add InflateRect

--- Src/Lsj.Util.Win32/User32.Rect.cs
+++ Src/Lsj.Util.Win32/User32.Rect.cs
@@ -99,6 +99,41 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// The <see cref="InflateRect"/> function increases or decreases the width and height of the specified rectangle.
+        /// The <see cref="InflateRect"/> function adds -<paramref name="dx"/> units to the left end and <paramref name="dx"/> to the right
+        /// end of the rectangle and -<paramref name="dy"/> units to the top and <paramref name="dy"/> to the bottom.
+        /// The <paramref name="dx"/> and <paramref name="dy"/> parameters are signed values; positive values increase the width and height,
+        /// and negative values decrease them.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-inflaterect
+        /// </para>
+        /// </summary>
+        /// <param name="lprc">
+        /// A pointer to the <see cref="RECT"/> structure that increases or decreases in size.
+        /// </param>
+        /// <param name="dx">
+        /// The amount to increase or decrease the rectangle width.
+        /// This parameter must be negative to decrease the width.
+        /// </param>
+        /// <param name="dy">
+        /// The amount to increase or decrease the rectangle height.
+        /// This parameter must be negative to decrease the height.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see cref="TRUE"/>.
+        /// If the function fails, the return value is <see cref="FALSE"/>.
+        /// </returns>
+        /// <remarks>
+        /// Because applications can use rectangles for different purposes, the rectangle functions do not use an explicit unit of measure.
+        /// Instead, all rectangle coordinates and dimensions are given in signed, logical values.
+        /// The mapping mode and the function in which the rectangle is used determine the units of measure.
+        /// </remarks>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "InflateRect", SetLastError = true)]
+        public static extern BOOL InflateRect([In][Out]RECT lprc, [In]int dx, [In]int dy);
+
+        /// <summary>
+        /// <para>
         /// The <see cref="IsRectEmpty"/> function determines whether the specified rectangle is empty.
         /// An empty rectangle is one that has no area; that is, the coordinate of the right side is less than or equal to the coordinate of the left side,
         /// or the coordinate of the bottom side is less than or equal to the coordinate of the top side.
