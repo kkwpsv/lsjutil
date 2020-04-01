a237b4f8da4d57c1df10acf0b67672cf4219ee23
Shaojun Li <i@shaojun.li>
2020-04-01 14:12:17 +0800

    Add StartDoc

---
+++ Src/Lsj.Util.Win32/Gdi32.Print.cs
@@ -0,0 +1,45 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using Lsj.Util.Win32.Structs;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Lsj.Util.Win32
+{
+    public partial class Gdi32
+    {
+        /// <summary>
+        /// <para>
+        /// The <see cref="StartDoc"/> function starts a print job.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-startdocw
+        /// </para>
+        /// </summary>
+        /// <param name="hdc">
+        /// A handle to the device context for the print job.
+        /// </param>
+        /// <param name="lpdi">
+        /// A pointer to a <see cref="DOCINFO"/> structure containing the name of the document file and the name of the output file.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is greater than zero.
+        /// This value is the print job identifier for the document.
+        /// If the function fails, the return value is less than or equal to zero.
+        /// </returns>
+        /// <remarks>
+        /// Note
+        /// This is a blocking or synchronous function and might not return immediately.
+        /// How quickly this function returns depends on run-time factors such as network status, print server configuration,
+        /// and printer driver implementation—factors that are difficult to predict when writing an application.
+        /// Calling this function from a thread that manages interaction with the user interface could make the application appear to be unresponsive.
+        /// Applications should call the <see cref="StartDoc"/> function immediately before beginning a print job.
+        /// Using this function ensures that multipage documents are not interspersed with other print jobs.
+        /// Applications can use the value returned by <see cref="StartDoc"/> to retrieve or set the priority of a print job.
+        /// Call the <see cref="GetJob"/> or <see cref="SetJob"/> function and supply this value as one of the required arguments.
+        /// </remarks>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "StartDocW", SetLastError = true)]
+        public static extern int StartDoc([In]HDC hdc, [MarshalAs(UnmanagedType.LPStruct)][In]DOCINFO lpdi);
+    }
+}
---
+++ Src/Lsj.Util.Win32/Structs/DOCINFO.cs
@@ -0,0 +1,56 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Lsj.Util.Win32.Structs
+{
+    /// <summary>
+    /// <para>
+    /// The <see cref="DOCINFO"/> structure contains the input and output file names and other information used by the <see cref="StartDoc"/> function.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/ns-wingdi-docinfow
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct DOCINFO
+    {
+        /// <summary>
+        /// The size, in bytes, of the structure.
+        /// </summary>
+        public int cbSize;
+
+        /// <summary>
+        /// Pointer to a null-terminated string that specifies the name of the document.
+        /// </summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string lpszDocName;
+
+        /// <summary>
+        /// Pointer to a null-terminated string that specifies the name of an output file.
+        /// If this pointer is <see langword="null"/>, the output will be sent to the device identified by the device context handle
+        /// that was passed to the <see cref="StartDoc"/> function.
+        /// </summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string lpszOutput;
+
+        /// <summary>
+        /// Pointer to a null-terminated string that specifies the type of data used to record the print job.
+        /// The legal values for this member can be found by calling <see cref="EnumPrintProcessorDatatypes"/> and
+        /// can include such values as raw, emf, or XPS_PASS.
+        /// This member can be <see langword="null"/>. Note that the requested data type might be ignored.
+        /// </summary>
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string lpszDatatype;
+
+        /// <summary>
+        /// Specifies additional information about the print job.
+        /// This member must be zero or one of the following values.
+        /// <see cref="DI_APPBANDING"/>: Applications that use banding should set this flag for optimal performance during printing.
+        /// <see cref="DI_ROPS_READ_DESTINATION"/>: The application will use raster operations that involve reading from the destination surface.
+        /// </summary>
+        public DWORD fwType;
+    }
+}
