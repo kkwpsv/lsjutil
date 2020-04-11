344477cb5cd5d1bdb8e3ad656e25a21afd9c12b3
Shaojun Li <i@shaojun.li>
2020-04-11 12:13:27 +0800

    Add AddFontResourceEx

--- Src/Lsj.Util.Win32/Gdi32.Font.cs
+++ Src/Lsj.Util.Win32/Gdi32.Font.cs
@@ -20,6 +20,7 @@ using static Lsj.Util.Win32.Enums.SystemErrorCodes;
 using static Lsj.Util.Win32.Enums.SystemParametersInfoParameters;
 using static Lsj.Util.Win32.Enums.TEXTMETRICPitchAndFamilyFlags;
 using static Lsj.Util.Win32.Enums.WindowsMessages;
+using static Lsj.Util.Win32.Enums.FontResourceFlags;
 using static Lsj.Util.Win32.Kernel32;
 using static Lsj.Util.Win32.User32;
 
@@ -123,6 +124,63 @@ namespace Lsj.Util.Win32
         /// </remarks>
         [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "AddFontResourceW", ExactSpelling = true, SetLastError = true)]
         public static extern int AddFontResource([MarshalAs(UnmanagedType.LPWStr)][In]string Arg1);
+
+        /// <summary>
+        /// <para>
+        /// The <see cref="AddFontResourceEx"/> function adds the font resource from the specified file to the system.
+        /// Fonts added with the <see cref="AddFontResourceEx"/> function can be marked as private and not enumerable.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-addfontresourceexw
+        /// </para>
+        /// </summary>
+        /// <param name="name">
+        /// A pointer to a null-terminated character string that contains a valid font file name.
+        /// This parameter can specify any of the following files.
+        /// .fon: Font resource file.
+        /// .fnt: Raw bitmap font file.
+        /// .ttf: Raw TrueType file.
+        /// .ttc: East Asian Windows: TrueType font collection.
+        /// .fot: TrueType resource file.
+        /// .otf: PostScript OpenType font.
+        /// .mmm: Multiple master Type1 font resource file. It must be used with .pfm and .pfb files.
+        /// .pfb: Type 1 font bits file. It is used with a .pfm file.
+        /// .pfm: Type 1 font metrics file. It is used with a .pfb file.
+        /// To add a font whose information comes from several resource files, have <paramref name="name"/> point to a string with the file names
+        /// separated by a "|" --for example, abcxxxxx.pfm | abcxxxxx.pfb.
+        /// </param>
+        /// <param name="fl">
+        /// The characteristics of the font to be added to the system.
+        /// This parameter can be one of the following values.
+        /// <see cref="FR_PRIVATE"/>:
+        /// Specifies that only the process that called the <see cref="AddFontResourceEx"/> function can use this font.
+        /// When the font name matches a public font, the private font will be chosen.
+        /// When the process terminates, the system will remove all fonts installed by the process with the <see cref="AddFontResourceEx"/> function.
+        /// <see cref="FR_NOT_ENUM"/>:
+        /// Specifies that no process, including the process that called the <see cref="AddFontResourceEx"/> function, can enumerate this font.
+        /// </param>
+        /// <param name="res">
+        /// Reserved. Must be zero.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value specifies the number of fonts added.
+        /// If the function fails, the return value is zero.
+        /// No extended error information is available.
+        /// </returns>
+        /// <remarks>
+        /// This function allows a process to use fonts without allowing other processes access to the fonts.
+        /// When an application no longer needs a font resource it loaded by calling the <see cref="AddFontResourceEx"/> function,
+        /// it must remove the resource by calling the <see cref="RemoveFontResourceEx"/> function.
+        /// This function installs the font only for the current session. When the system restarts, the font will not be present.
+        /// To have the font installed even after restarting the system, the font must be listed in the registry.
+        /// A font listed in the registry and installed to a location other than the %windir%\fonts\ folder cannot be modified,
+        /// deleted, or replaced as long as it is loaded in any session.
+        /// In order to change one of these fonts, it must first be removed by calling <see cref="RemoveFontResource"/>,
+        /// removed from the font registry (HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts), and the system restarted.
+        /// After restarting the system, the font will no longer be loaded and can be changed.
+        /// </remarks>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "AddFontResourceExW", ExactSpelling = true, SetLastError = true)]
+        public static extern int AddFontResourceEx([MarshalAs(UnmanagedType.LPWStr)][In]string name, [In]FontResourceFlags fl, [In]PVOID res);
 
         /// <summary>
         /// <para>
