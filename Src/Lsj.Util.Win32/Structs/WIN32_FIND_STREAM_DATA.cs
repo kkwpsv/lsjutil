492025eeff1f2462b95aefbecc82c94a642f0a1e
Shaojun Li <i@shaojun.li>
2020-04-16 15:26:29 +0800

    Add WIN32_FIND_STREAM_DATA

---
+++ Src/Lsj.Util.Win32/Structs/WIN32_FIND_STREAM_DATA.cs
@@ -0,0 +1,31 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using System.Runtime.InteropServices;
+using static Lsj.Util.Win32.Constants;
+using static Lsj.Util.Win32.Kernel32;
+
+namespace Lsj.Util.Win32.Structs
+{
+    /// <summary>
+    /// <para>
+    /// Contains information about the stream found by the <see cref="FindFirstStreamW"/> or <see cref="FindNextStreamW"/> function.
+    /// </para>
+    /// <para>
+    /// From: 
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct WIN32_FIND_STREAM_DATA
+    {
+        /// <summary>
+        /// A <see cref="LARGE_INTEGER"/> value that specifies the size of the stream, in bytes.
+        /// </summary>
+        public LARGE_INTEGER StreamSize;
+
+        /// <summary>
+        /// The name of the stream.
+        /// The string name format is ":streamname:$streamtype".
+        /// </summary>
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_PATH + 36)]
+        public string cStreamName;
+    }
+}
