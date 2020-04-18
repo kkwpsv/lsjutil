7a667b5c04808ddb376670fe0162e91210f377fc
Shaojun Li <i@shaojun.li>
2020-04-18 22:49:06 +0800

    Add JOBOBJECT_BASIC_AND_IO_ACCOUNTING_INFORMATION

---
+++ Src/Lsj.Util.Win32/Structs/IO_COUNTERS.cs
@@ -0,0 +1,49 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.Structs
+{
+    /// <summary>
+    /// <para>
+    /// Contains I/O accounting information for a process or a job object.
+    /// For a job object, the counters include all operations performed by all processes that have ever been associated with the job,
+    /// in addition to all processes currently associated with the job.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winnt/ns-winnt-io_counters
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct IO_COUNTERS
+    {
+        /// <summary>
+        /// The number of read operations performed.
+        /// </summary>
+        public ULONGLONG ReadOperationCount;
+
+        /// <summary>
+        /// The number of write operations performed.
+        /// </summary>
+        public ULONGLONG WriteOperationCount;
+
+        /// <summary>
+        /// The number of I/O operations performed, other than read and write operations.
+        /// </summary>
+        public ULONGLONG OtherOperationCount;
+
+        /// <summary>
+        /// The number of bytes read.
+        /// </summary>
+        public ULONGLONG ReadTransferCount;
+
+        /// <summary>
+        /// The number of bytes written.
+        /// </summary>
+        public ULONGLONG WriteTransferCount;
+
+        /// <summary>
+        /// The number of bytes transferred during operations other than read and write operations.
+        /// </summary>
+        public ULONGLONG OtherTransferCount;
+    }
+}
---
+++ Src/Lsj.Util.Win32/Structs/JOBOBJECT_BASIC_AND_IO_ACCOUNTING_INFORMATION.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.Structs
+{
+    /// <summary>
+    /// <para>
+    /// Contains basic accounting and I/O accounting information for a job object.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winnt/ns-winnt-jobobject_basic_and_io_accounting_information
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct JOBOBJECT_BASIC_AND_IO_ACCOUNTING_INFORMATION
+    {
+        /// <summary>
+        /// A <see cref="JOBOBJECT_BASIC_ACCOUNTING_INFORMATION"/> structure that specifies the basic accounting information for the job.
+        /// </summary>
+        public JOBOBJECT_BASIC_ACCOUNTING_INFORMATION BasicInfo;
+
+        /// <summary>
+        /// An <see cref="IO_COUNTERS"/> structure that specifies the I/O accounting information for the job.
+        /// The structure includes information for all processes that have ever been associated with the job,
+        /// in addition to the information for all processes currently associated with the job.
+        /// </summary>
+        public IO_COUNTERS IoInfo;
+    }
+}
