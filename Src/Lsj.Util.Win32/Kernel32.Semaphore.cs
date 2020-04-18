ecd1023a35e86a3b97642b35b4edf81e0109ac4e
Shaojun Li <i@shaojun.li>
2020-04-18 22:15:45 +0800

    Add ReleaseSemaphore

--- Src/Lsj.Util.Win32/Kernel32.Semaphore.cs
+++ Src/Lsj.Util.Win32/Kernel32.Semaphore.cs
@@ -226,5 +226,58 @@ namespace Lsj.Util.Win32
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "OpenSemaphoreW", ExactSpelling = true, SetLastError = true)]
         public static extern HANDLE OpenSemaphore([In]ACCESS_MASK dwDesiredAccess, [In]BOOL bInheritHandle,
             [MarshalAs(UnmanagedType.LPWStr)][In]string lpName);
+
+        /// <summary>
+        /// <para>
+        /// Increases the count of the specified semaphore object by a specified amount.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/synchapi/nf-synchapi-releasesemaphore
+        /// </para>
+        /// </summary>
+        /// <param name="hSemaphore">
+        /// A handle to the semaphore object.
+        /// The <see cref="CreateSemaphore"/> or <see cref="OpenSemaphore"/> function returns this handle.
+        /// This handle must have the <see cref="SEMAPHORE_MODIFY_STATE"/> access right.
+        /// For more information, see Synchronization Object Security and Access Rights.
+        /// </param>
+        /// <param name="lReleaseCount">
+        /// The amount by which the semaphore object's current count is to be increased.
+        /// The value must be greater than zero.
+        /// If the specified amount would cause the semaphore's count to exceed the maximum count that was specified when the semaphore was created,
+        /// the count is not changed and the function returns <see cref="FALSE"/>.
+        /// </param>
+        /// <param name="lpPreviousCount">
+        /// A pointer to a variable to receive the previous count for the semaphore.
+        /// This parameter can be <see cref="NULL"/> if the previous count is not required.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see cref="TRUE"/>.
+        /// If the function fails, the return value is <see cref="FALSE"/>.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// The state of a semaphore object is signaled when its count is greater than zero and nonsignaled when its count is equal to zero.
+        /// The process that calls the CreateSemaphore function specifies the semaphore's initial count.
+        /// Each time a waiting thread is released because of the semaphore's signaled state, the count of the semaphore is decreased by one.
+        /// Typically, an application uses a semaphore to limit the number of threads using a resource.
+        /// Before a thread uses the resource, it specifies the semaphore handle in a call to one of the wait functions.
+        /// When the wait function returns, it decreases the semaphore's count by one.
+        /// When the thread has finished using the resource, it calls <see cref="ReleaseSemaphore"/> to increase the semaphore's count by one.
+        /// Another use of <see cref="ReleaseSemaphore"/> is during an application's initialization.
+        /// The application can create a semaphore with an initial count of zero.
+        /// This sets the semaphore's state to nonsignaled and blocks all threads from accessing the protected resource.
+        /// When the application finishes its initialization, it uses <see cref="ReleaseSemaphore"/> to increase the count to its maximum value,
+        /// to permit normal access to the protected resource.
+        /// It is not possible to reduce the semaphore object count using <see cref="ReleaseSemaphore"/>,
+        /// because <paramref name="lReleaseCount"/> cannot be a negative number.
+        /// To temporarily restrict or reduce access, create a loop in which you call the <see cref="WaitForSingleObject"/> function
+        /// with a time-out interval of zero until the semaphore count has been reduced sufficiently.
+        /// (Note that other threads can reduce the count while this loop is being executed.)
+        /// To restore access, call <see cref="ReleaseSemaphore"/> with the release count equal to the number of times
+        /// <see cref="WaitForSingleObject"/> was called in the loop.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "ReleaseSemaphore", ExactSpelling = true, SetLastError = true)]
+        public static extern BOOL ReleaseSemaphore([In]HANDLE hSemaphore, [In]LONG lReleaseCount, [Out]out LONG lpPreviousCount);
     }
 }
