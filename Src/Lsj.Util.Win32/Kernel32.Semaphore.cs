4f0ef62ac667cf63ff2dc5bb6b87a67a92f29dda
Shaojun Li <i@shaojun.li>
2020-04-18 21:32:21 +0800

    Add OpenSemaphore

--- Src/Lsj.Util.Win32/Kernel32.Semaphore.cs
+++ Src/Lsj.Util.Win32/Kernel32.Semaphore.cs
@@ -1,7 +1,10 @@
-﻿using Lsj.Util.Win32.Marshals;
+﻿using Lsj.Util.Win32.BaseTypes;
+using Lsj.Util.Win32.Marshals;
 using Lsj.Util.Win32.Structs;
 using System;
 using System.Runtime.InteropServices;
+using static Lsj.Util.Win32.BaseTypes.BOOL;
+using static Lsj.Util.Win32.Constants;
 using static Lsj.Util.Win32.Enums.SynchronizationObjectAccessRights;
 using static Lsj.Util.Win32.Enums.SystemErrorCodes;
 
@@ -177,5 +180,51 @@ namespace Lsj.Util.Win32
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(StructPointerOrNullObjectMarshaler<SECURITY_ATTRIBUTES>))]
             [In]StructPointerOrNullObject<SECURITY_ATTRIBUTES> lpSemaphoreAttributes, [In]int lInitialCount, [In]int lMaximumCount,
             [MarshalAs(UnmanagedType.LPWStr)][In]string lpName, [In]uint dwFlags, [In]uint dwDesiredAccess);
+
+        /// <summary>
+        /// <para>
+        /// Opens an existing named semaphore object.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/synchapi/nf-synchapi-opensemaphorew
+        /// </para>
+        /// </summary>
+        /// <param name="dwDesiredAccess">
+        /// The access to the semaphore object.
+        /// The function fails if the security descriptor of the specified object does not permit the requested access for the calling process.
+        /// For a list of access rights, see Synchronization Object Security and Access Rights.
+        /// </param>
+        /// <param name="bInheritHandle">
+        /// If this value is <see cref="TRUE"/>, processes created by this process will inherit the handle.
+        /// Otherwise, the processes do not inherit this handle.
+        /// </param>
+        /// <param name="lpName">
+        /// The name of the semaphore to be opened. Name comparisons are case sensitive.
+        /// This function can open objects in a private namespace. For more information, see Object Namespaces.
+        /// Terminal Services:  The name can have a "Global" or "Local" prefix to explicitly open an object in the global or session namespace.
+        /// The remainder of the name can contain any character except the backslash character ().
+        /// For more information, see Kernel Object Namespaces.
+        /// Note Fast user switching is implemented using Terminal Services sessions.
+        /// The first user to log on uses session 0, the next user to log on uses session 1, and so on.
+        /// Kernel object names must follow the guidelines outlined for Terminal Services so that applications can support multiple users.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is a handle to the semaphore object.
+        /// If the function fails, the return value is <see cref="NULL"/>.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// The <see cref="OpenSemaphore"/> function enables multiple processes to open handles of the same semaphore object.
+        /// The function succeeds only if some process has already created the semaphore by using the <see cref="CreateSemaphore"/> function.
+        /// The calling process can use the returned handle in any function that requires a handle to a semaphore object,
+        /// such as the wait functions, subject to the limitations of the access specified in the <paramref name="dwDesiredAccess"/> parameter.
+        /// The handle can be duplicated by using the <see cref="DuplicateHandle"/> function.
+        /// Use the <see cref="CloseHandle"/> function to close the handle.
+        /// The system closes the handle automatically when the process terminates.
+        /// The semaphore object is destroyed when its last handle has been closed.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "OpenSemaphoreW", ExactSpelling = true, SetLastError = true)]
+        public static extern HANDLE OpenSemaphore([In]ACCESS_MASK dwDesiredAccess, [In]BOOL bInheritHandle,
+            [MarshalAs(UnmanagedType.LPWStr)][In]string lpName);
     }
 }
