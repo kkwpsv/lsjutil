2fd6f1219b6339969f5dad508c12e6f47b853b97
Shaojun Li <i@shaojun.li>
2020-04-29 19:42:39 +0800

    Mark UnionStruct as Obsolete, which may lead to runtime error. Will be removed.

--- Src/Lsj.Util.Win32/Marshals/UnionStruct.cs
+++ Src/Lsj.Util.Win32/Marshals/UnionStruct.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Lsj.Util.Win32.Marshals
 {
@@ -8,6 +9,7 @@ namespace Lsj.Util.Win32.Marshals
     /// <typeparam name="T1"></typeparam>
     /// <typeparam name="T2"></typeparam>
     [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
+    [Obsolete]
     public struct UnionStruct<T1, T2> where T1 : struct where T2 : struct
     {
         /// <summary>
@@ -30,6 +32,7 @@ namespace Lsj.Util.Win32.Marshals
     /// <typeparam name="T2"></typeparam>
     /// <typeparam name="T3"></typeparam>
     [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
+    [Obsolete]
     public struct UnionStruct<T1, T2, T3> where T1 : struct where T2 : struct where T3 : struct
     {
         /// <summary>
@@ -52,41 +55,6 @@ namespace Lsj.Util.Win32.Marshals
     }
 
     /// <summary>
-    /// UnionStruct
-    /// </summary>
-    /// <typeparam name="T1"></typeparam>
-    /// <typeparam name="T2"></typeparam>
-    /// <typeparam name="T3"></typeparam>
-    /// <typeparam name="T4"></typeparam>
-    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
-    public struct UnionStruct<T1, T2, T3, T4> where T1 : struct where T2 : struct where T3 : struct where T4 : struct
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T1 Struct1;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T2 Struct2;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T3 Struct3;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T4 Struct4;
-    }
-
-    /// <summary>
     /// 
     /// </summary>
     /// <typeparam name="T1"></typeparam>
@@ -95,6 +63,7 @@ namespace Lsj.Util.Win32.Marshals
     /// <typeparam name="T4"></typeparam>
     /// <typeparam name="T5"></typeparam>
     [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
+    [Obsolete]
     public struct UnionStruct<T1, T2, T3, T4, T5> where T1 : struct where T2 : struct where T3 : struct where T4 : struct where T5 : struct
     {
         /// <summary>
@@ -137,180 +106,11 @@ namespace Lsj.Util.Win32.Marshals
     /// <typeparam name="T4"></typeparam>
     /// <typeparam name="T5"></typeparam>
     /// <typeparam name="T6"></typeparam>
-    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
-    public struct UnionStruct<T1, T2, T3, T4, T5, T6> where T1 : struct where T2 : struct where T3 : struct where T4 : struct where T5 : struct where T6 : struct
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T1 Struct1;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T2 Struct2;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T3 Struct3;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T4 Struct4;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T5 Struct5;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T6 Struct6;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="T1"></typeparam>
-    /// <typeparam name="T2"></typeparam>
-    /// <typeparam name="T3"></typeparam>
-    /// <typeparam name="T4"></typeparam>
-    /// <typeparam name="T5"></typeparam>
-    /// <typeparam name="T6"></typeparam>
-    /// <typeparam name="T7"></typeparam>
-    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
-    public struct UnionStruct<T1, T2, T3, T4, T5, T6, T7> where T1 : struct where T2 : struct where T3 : struct where T4 : struct
-                                                          where T5 : struct where T6 : struct where T7 : struct
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T1 Struct1;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T2 Struct2;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T3 Struct3;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T4 Struct4;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T5 Struct5;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T6 Struct6;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T7 Struct7;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="T1"></typeparam>
-    /// <typeparam name="T2"></typeparam>
-    /// <typeparam name="T3"></typeparam>
-    /// <typeparam name="T4"></typeparam>
-    /// <typeparam name="T5"></typeparam>
-    /// <typeparam name="T6"></typeparam>
-    /// <typeparam name="T7"></typeparam>
-    /// <typeparam name="T8"></typeparam>
-    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
-    public struct UnionStruct<T1, T2, T3, T4, T5, T6, T7, T8> where T1 : struct where T2 : struct where T3 : struct where T4 : struct
-                                                              where T5 : struct where T6 : struct where T7 : struct where T8 : struct
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T1 Struct1;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T2 Struct2;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T3 Struct3;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T4 Struct4;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T5 Struct5;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T6 Struct6;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T7 Struct7;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [FieldOffset(0)]
-        public T8 Struct8;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="T1"></typeparam>
-    /// <typeparam name="T2"></typeparam>
-    /// <typeparam name="T3"></typeparam>
-    /// <typeparam name="T4"></typeparam>
-    /// <typeparam name="T5"></typeparam>
-    /// <typeparam name="T6"></typeparam>
     /// <typeparam name="T7"></typeparam>
     /// <typeparam name="T8"></typeparam>
     /// <typeparam name="T9"></typeparam>
     [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
+    [Obsolete]
     public struct UnionStruct<T1, T2, T3, T4, T5, T6, T7, T8, T9> where T1 : struct where T2 : struct where T3 : struct where T4 : struct
                                                                   where T5 : struct where T6 : struct where T7 : struct where T8 : struct
                                                                   where T9 : struct
