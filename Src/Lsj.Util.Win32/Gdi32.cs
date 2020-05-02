2d648ad5170f9065b993ab4bd76287fafb66b3de
Shaojun Li <i@shaojun.li>
2020-05-02 18:57:02 +0800

    Add SetGraphicsMode

--- Src/Lsj.Util.Win32/Gdi32.cs
+++ Src/Lsj.Util.Win32/Gdi32.cs
@@ -1194,6 +1194,51 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// The <see cref="SetGraphicsMode"/> function sets the graphics mode for the specified device context.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/wingdi/nf-wingdi-setgraphicsmode
+        /// </para>
+        /// </summary>
+        /// <param name="hdc">
+        /// A handle to the device context.
+        /// </param>
+        /// <param name="iMode">
+        /// The graphics mode. This parameter can be one of the following values.
+        /// <see cref="GM_COMPATIBLE"/>, <see cref="GM_ADVANCED"/>
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the old graphics mode.
+        /// If the function fails, the return value is zero.
+        /// </returns>
+        /// <remarks>
+        /// There are three areas in which graphics output differs according to the graphics mode:
+        /// Text Output:
+        /// In the <see cref="GM_COMPATIBLE"/> mode, TrueType (or vector font) text output behaves much the same way as
+        /// raster font text output with respect to the world-to-device transformations in the DC.
+        /// The TrueType text is always written from left to right and right side up, even if the rest of the graphics will be flipped on the x or y axis.
+        /// Only the height of the TrueType (or vector font) text is scaled.
+        /// The only way to write text that is not horizontal in the <see cref="GM_COMPATIBLE"/> mode is to specify nonzero escapement
+        /// and orientation for the logical font selected in this device context.
+        /// In the <see cref="GM_ADVANCED"/> mode, TrueType (or vector font) text output fully conforms to
+        /// the world-to-device transformation in the device context.
+        /// The raster fonts only have very limited transformation capabilities (stretching by some integer factors).
+        /// Graphics device interface (GDI) tries to produce the best output it can with raster fonts for nontrivial transformations.
+        /// Rectangle Exclusion:
+        /// If the default <see cref="GM_COMPATIBLE"/> graphics mode is set, the system excludes bottom and rightmost edges when it draws rectangles.
+        /// The <see cref="GM_ADVANCED"/> graphics mode is required if applications want to draw rectangles that are lower-right inclusive.
+        /// Arc Drawing:
+        /// If the default <see cref="GM_COMPATIBLE"/> graphics mode is set, GDI draws arcs using the current arc direction in the device space.
+        /// With this convention, arcs do not respect page-to-device transforms that require a flip along the x or y axis.
+        /// If the <see cref="GM_ADVANCED"/> graphics mode is set, GDI always draws arcs in the counterclockwise direction in logical space.
+        /// This is equivalent to the statement that, in the <see cref="GM_ADVANCED"/> graphics mode,
+        /// both arc control points and arcs themselves fully respect the device context's world-to-device transformation.
+        /// </remarks>
+        [DllImport("gdi32.dll", CharSet = CharSet.Unicode, EntryPoint = "SetGraphicsMode", ExactSpelling = true, SetLastError = true)]
+        public static extern GraphicsModes SetGraphicsMode([In]HDC hdc, [In]GraphicsModes iMode);
+
+        /// <summary>
+        /// <para>
         /// The <see cref="SetICMMode"/> function causes Image Color Management to be enabled, disabled, or queried on a given device context (DC).
         /// </para>
         /// <para>
