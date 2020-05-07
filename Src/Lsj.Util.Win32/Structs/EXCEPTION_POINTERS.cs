e49786f2ff88a36c160e8a6df7c30bf913ac55f0
Shaojun Li <i@shaojun.li>
2020-05-07 20:17:39 +0800

    Add UnhandledExceptionFilter

---
+++ Src/Lsj.Util.Win32/Structs/EXCEPTION_POINTERS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.Structs
+{
+    /// <summary>
+    /// <para>
+    /// Contains an exception record with a machine-independent description of an exception and a context record
+    /// with a machine-dependent description of the processor context at the time of the exception.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winnt/ns-winnt-exception_pointers
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct EXCEPTION_POINTERS
+    {
+        /// <summary>
+        /// A pointer to an <see cref="EXCEPTION_RECORD"/> structure that contains a machine-independent description of the exception.
+        /// </summary>
+        public IntPtr ExceptionRecord;
+
+        /// <summary>
+        /// A pointer to a <see cref="CONTEXT"/> structure that contains a processor-specific description
+        /// of the state of the processor at the time of the exception.
+        /// </summary>
+        public IntPtr ContextRecord;
+    }
+}
