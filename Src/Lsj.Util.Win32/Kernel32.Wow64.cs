bd8f12824fe599fa3ddafc7e068042e08ec8479e
Shaojun Li <i@shaojun.li>
2020-05-07 19:23:43 +0800

    Add Wow64SuspendThread

--- Src/Lsj.Util.Win32/Kernel32.Wow64.cs
+++ Src/Lsj.Util.Win32/Kernel32.Wow64.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using static Lsj.Util.Win32.Advapi32;
 using static Lsj.Util.Win32.BaseTypes.BOOL;
 using static Lsj.Util.Win32.Enums.ProcessAccessRights;
+using static Lsj.Util.Win32.Enums.ThreadAccessRights;
 
 namespace Lsj.Util.Win32
 {
@@ -192,5 +193,43 @@ namespace Lsj.Util.Win32
         /// </remarks>
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "Wow64RevertWow64FsRedirection", ExactSpelling = true, SetLastError = true)]
         public static extern BOOL Wow64RevertWow64FsRedirection([In]PVOID OlValue);
+
+        /// <summary>
+        /// <para>
+        /// Suspends the specified WOW64 thread.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winbase/nf-winbase-wow64suspendthread
+        /// </para>
+        /// </summary>
+        /// <param name="hThread">
+        /// A handle to the thread that is to be suspended.
+        /// The handle must have the <see cref="THREAD_SUSPEND_RESUME"/> access right.
+        /// For more information, see Thread Security and Access Rights.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the thread's previous suspend count; otherwise, it is (<see cref="DWORD"/>) -1.
+        /// To get extended error information, use the <see cref="GetLastError"/> function.
+        /// </returns>
+        /// <remarks>
+        /// If the function succeeds, execution of the specified thread is suspended and the thread's suspend count is incremented.
+        /// Suspending a thread causes the thread to stop executing user-mode (application) code.
+        /// This function is primarily designed for use by debuggers.
+        /// It is not intended to be used for thread synchronization.
+        /// Calling <see cref="Wow64SuspendThread"/> on a thread that owns a synchronization object, such as a mutex or critical section,
+        /// can lead to a deadlock if the calling thread tries to obtain a synchronization object owned by a suspended thread.
+        /// To avoid this situation, a thread within an application that is not a debugger should signal the other thread to suspend itself.
+        /// The target thread must be designed to watch for this signal and respond appropriately.
+        /// Each thread has a suspend count (with a maximum value of <see cref="MAXIMUM_SUSPEND_COUNT"/>).
+        /// If the suspend count is greater than zero, the thread is suspended; otherwise, the thread is not suspended and is eligible for execution.
+        /// Calling <see cref="Wow64SuspendThread"/> causes the target thread's suspend count to be incremented.
+        /// Attempting to increment past the maximum suspend count causes an error without incrementing the count.
+        /// The ResumeThread function decrements the suspend count of a suspended thread.
+        /// This function is intended for 64-bit applications.
+        /// It is not supported on 32-bit Windows; such calls fail and set the last error code to <see cref="ERROR_INVALID_FUNCTION"/>.
+        /// A 32-bit application can call this function on a WOW64 thread; the result is the same as calling the <see cref="SuspendThread"/> function.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "Wow64SuspendThread", ExactSpelling = true, SetLastError = true)]
+        public static extern DWORD Wow64SuspendThread([In]HANDLE hThread);
     }
 }
