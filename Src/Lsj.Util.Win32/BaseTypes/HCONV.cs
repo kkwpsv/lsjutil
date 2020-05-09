62bfedd375ab6227468952c64cae0c41ebe3ab7c
Shaojun Li <i@shaojun.li>
2020-05-09 18:26:41 +0800

    Add DdeImpersonateClient

---
+++ Src/Lsj.Util.Win32/BaseTypes/HCONV.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.BaseTypes
+{
+    /// <summary>
+    /// <para>
+    /// A handle to a dynamic data exchange (DDE) conversation.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/winprog/windows-data-types
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct HCONV
+    {
+        private HANDLE _value;
+
+        /// <inheritdoc/>
+        public override string ToString() => _value.ToString();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator HANDLE(HCONV val) => val._value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator HCONV(HANDLE val) => new HCONV { _value = val };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator IntPtr(HCONV val) => val._value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator HCONV(IntPtr val) => new HCONV { _value = val };
+    }
+}
