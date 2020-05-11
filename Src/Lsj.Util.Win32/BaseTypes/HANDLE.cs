21470378735fc8355ef09a26db377d3c8bdf63f8
Shaojun Li <i@shaojun.li>
2020-05-11 16:18:47 +0800

    Add operators and override some methods.

--- Src/Lsj.Util.Win32/BaseTypes/HANDLE.cs
+++ Src/Lsj.Util.Win32/BaseTypes/HANDLE.cs
@@ -19,6 +19,28 @@ namespace Lsj.Util.Win32.BaseTypes
         /// <inheritdoc/>
         public override string ToString() => _value.ToString("X");
 
+        /// <inheritdoc/>
+        public override bool Equals(object obj) => obj is HANDLE handle && handle == this;
+
+        /// <inheritdoc/>
+        public override int GetHashCode() => _value.GetHashCode();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator ==(HANDLE a, HANDLE b) => a._value == b._value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator !=(HANDLE a, HANDLE b) => a._value != b._value;
+
         /// <summary>
         /// 
         /// </summary>
@@ -30,5 +52,11 @@ namespace Lsj.Util.Win32.BaseTypes
         /// </summary>
         /// <param name="val"></param>
         public static implicit operator HANDLE(IntPtr val) => new HANDLE { _value = val };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static explicit operator HANDLE(int val) => new HANDLE { _value = (IntPtr)val };
     }
 }
