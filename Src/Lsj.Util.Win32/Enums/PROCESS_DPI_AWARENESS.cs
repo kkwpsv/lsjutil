a78d485a58db26d465ea39625a6c65762ad21363
Shaojun Li <i@shaojun.li>
2020-05-11 16:19:08 +0800

    Fix remarks.

--- Src/Lsj.Util.Win32/Enums/PROCESS_DPI_AWARENESS.cs
+++ Src/Lsj.Util.Win32/Enums/PROCESS_DPI_AWARENESS.cs
@@ -1,4 +1,5 @@
-﻿using static Lsj.Util.Win32.Enums.WindowsMessages;
+﻿using Lsj.Util.Win32.BaseTypes;
+using static Lsj.Util.Win32.Enums.WindowsMessages;
 using static Lsj.Util.Win32.User32;
 
 namespace Lsj.Util.Win32.Enums
