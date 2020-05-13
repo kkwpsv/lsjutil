21307aede75b3fd950e64564b477d8806d04f2a5
Shaojun Li <i@shaojun.li>
2020-05-13 16:18:00 +0800

    Add FreeUserPhysicalPages.

--- Src/Lsj.Util.Win32/Kernel32.Memory.cs
+++ Src/Lsj.Util.Win32/Kernel32.Memory.cs
@@ -24,6 +24,46 @@ namespace Lsj.Util.Win32
     {
         /// <summary>
         /// <para>
+        /// Frees physical memory pages that are allocated previously
+        /// by using <see cref="AllocateUserPhysicalPages"/> or <see cref="AllocateUserPhysicalPagesNuma"/>.
+        /// If any of these pages are currently mapped in the Address Windowing Extensions (AWE) region, they are automatically unmapped by this call.
+        /// This does not affect the virtual address space that is occupied by a specified Address Windowing Extensions (AWE) region.
+        /// 64-bit Windows on Itanium-based systems:
+        /// Due to the difference in page sizes, <see cref="FreeUserPhysicalPages"/> is not supported for 32-bit applications.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/memoryapi/nf-memoryapi-freeuserphysicalpages
+        /// </para>
+        /// </summary>
+        /// <param name="hProcess">
+        /// The handle to a process.
+        /// The function frees memory within the virtual address space of this process.
+        /// </param>
+        /// <param name="NumberOfPages">
+        /// The size of the physical memory to free, in pages.
+        /// On return, if the function fails, this parameter indicates the number of pages that are freed.
+        /// </param>
+        /// <param name="PageArray">
+        /// A pointer to an array of page frame numbers of the allocated memory to be freed.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is <see cref="TRUE"/>.
+        /// If the function fails, the return value is <see cref="FALSE"/>.
+        /// In this case, the <paramref name="NumberOfPages"/> parameter reflect how many pages have actually been released.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// In a multiprocessor environment, this function maintains coherence of the hardware translation buffer.
+        /// When this function returns, all threads on all processors are guaranteed to see the correct mapping.
+        /// To compile an application that uses this function, define the _WIN32_WINNT macro as 0x0500 or later.
+        /// For more information, see Using the Windows Headers.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "FreeUserPhysicalPages", ExactSpelling = true, SetLastError = true)]
+        public static extern BOOL FreeUserPhysicalPages([In]HANDLE hProcess, [In][Out]ref ULONG_PTR NumberOfPages,
+            [MarshalAs(UnmanagedType.LPArray)][In][Out]ULONG_PTR[] PageArray);
+
+        /// <summary>
+        /// <para>
         /// Retrieves the minimum size of a large page.
         /// </para>
         /// <para>
