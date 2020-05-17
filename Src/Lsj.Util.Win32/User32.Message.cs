26ec90005f81378626bb1b2324d4ce89f2a89f50
Shaojun Li <i@shaojun.li>
2020-05-17 16:29:05 +0800

    Add InSendMessageEx

--- Src/Lsj.Util.Win32/User32.Message.cs
+++ Src/Lsj.Util.Win32/User32.Message.cs
@@ -5,6 +5,7 @@ using System;
 using System.Runtime.InteropServices;
 using static Lsj.Util.Win32.BaseTypes.BOOL;
 using static Lsj.Util.Win32.Constants;
+using static Lsj.Util.Win32.Enums.InSendMessageExResults;
 using static Lsj.Util.Win32.Enums.PeekMessageFlags;
 using static Lsj.Util.Win32.Enums.QueueStatus;
 using static Lsj.Util.Win32.Enums.SendMessageTimeoutFlags;
@@ -140,6 +141,30 @@ namespace Lsj.Util.Win32
         /// </returns>
         [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "InSendMessage", ExactSpelling = true, SetLastError = true)]
         public static extern BOOL InSendMessage();
+
+        /// <summary>
+        /// <para>
+        /// Determines whether the current window procedure is processing a message
+        /// that was sent from another thread (in the same process or a different process).
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-insendmessageex
+        /// </para>
+        /// </summary>
+        /// <param name="lpReserved">
+        /// Reserved; must be <see cref="NULL"/>.
+        /// </param>
+        /// <returns>
+        /// If the message was not sent, the return value is <see cref="ISMEX_NOSEND"/> (0x00000000).
+        /// Otherwise, the return value is one or more of the following values.
+        /// <see cref="ISMEX_CALLBACK"/>, <see cref="ISMEX_NOTIFY"/>, <see cref="ISMEX_REPLIED"/>, <see cref="ISMEX_SEND"/>
+        /// </returns>
+        /// <remarks>
+        /// To determine if the sender is blocked, use the following test:
+        /// <code>fBlocked = ( InSendMessageEx(NULL) &amp; (ISMEX_REPLIED|ISMEX_SEND) ) == ISMEX_SEND;</code>
+        /// </remarks>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "InSendMessageEx", ExactSpelling = true, SetLastError = true)]
+        public static extern InSendMessageExResults InSendMessageEx([In]LPVOID lpReserved);
 
         /// <summary>
         /// <para>
