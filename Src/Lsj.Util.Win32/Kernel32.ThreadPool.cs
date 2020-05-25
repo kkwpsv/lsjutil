8371a05f5543606ce849da8e33ccf4013f6d13c6
Shaojun Li <i@shaojun.li>
2020-05-25 09:15:43 +0800

    Add SubmitThreadpoolWork.

--- Src/Lsj.Util.Win32/Kernel32.ThreadPool.cs
+++ Src/Lsj.Util.Win32/Kernel32.ThreadPool.cs
@@ -361,6 +361,28 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Posts a work object to the thread pool.
+        /// A worker thread calls the work object's callback function.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/threadpoolapiset/nf-threadpoolapiset-submitthreadpoolwork
+        /// </para>
+        /// </summary>
+        /// <param name="pwk">
+        /// A TP_WORK structure that defines the work object.
+        /// The <see cref="CreateThreadpoolWork"/> function returns this structure.
+        /// </param>
+        /// <remarks>
+        /// You can post a work object one or more times (up to <see cref="MAXULONG"/>) without waiting for prior callbacks to complete.
+        /// The callbacks will execute in parallel.
+        /// To improve efficiency, the thread pool may throttle the threads.
+        /// To compile an application that uses this function, define _WIN32_WINNT as 0x0600 or higher.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "SubmitThreadpoolWork", ExactSpelling = true, SetLastError = true)]
+        public static extern void SubmitThreadpoolWork([In] PTP_WORK pwk);
+
+        /// <summary>
+        /// <para>
         /// Requests that a thread pool worker thread call the specified callback function.
         /// </para>
         /// <para>
