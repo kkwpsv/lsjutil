1e1aa4096d17eced727ee688b78c57d7b60d90e3
Shaojun Li <i@shaojun.li>
2020-05-25 09:21:40 +0800

    Add CreateThreadpoolWork

--- Src/Lsj.Util.Win32/Kernel32.ThreadPool.cs
+++ Src/Lsj.Util.Win32/Kernel32.ThreadPool.cs
@@ -48,6 +48,37 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Applications implement this callback if they call the <see cref="SubmitThreadpoolWork"/> function
+        /// to start a worker thread for the work object.
+        /// The <see cref="PTP_WORK_CALLBACK"/> type defines a pointer to this callback function.
+        /// WorkCallback is a placeholder for the application-defined function name.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/previous-versions/windows/desktop/legacy/ms687396(v=vs.85)
+        /// </para>
+        /// </summary>
+        /// <param name="Instance">
+        /// A <see cref="TP_CALLBACK_INSTANCE"/> structure that defines the callback instance.
+        /// Applications do not modify the members of this structure.
+        /// This structure can be passed to one of the following functions:
+        /// <see cref="SetEventWhenCallbackReturns"/>
+        /// <see cref="ReleaseSemaphoreWhenCallbackReturns"/>
+        /// <see cref="LeaveCriticalSectionWhenCallbackReturns"/>
+        /// <see cref="ReleaseMutexWhenCallbackReturns"/>
+        /// <see cref="FreeLibraryWhenCallbackReturns"/>
+        /// <see cref="CallbackMayRunLong"/>
+        /// <see cref="DisassociateCurrentThreadFromCallback"/>
+        /// </param>
+        /// <param name="Context">
+        /// The application-defined data.
+        /// </param>
+        /// <param name="Work">
+        /// A TP_WORK structure that defines the work object that generated the callback.
+        /// </param>
+        public delegate void PTP_WORK_CALLBACK([In] PTP_CALLBACK_INSTANCE Instance, [In] PVOID Context, [In] PTP_WORK Work);
+
+        /// <summary>
+        /// <para>
         /// Applications implement this callback if they call the <see cref="SetThreadpoolTimer"/> function to start a worker thread for the timer object.
         /// The <see cref="PTP_TIMER_CALLBACK"/> type defines a pointer to this callback function.
         /// TimerCallback is a placeholder for the application-defined function name.
@@ -137,6 +168,40 @@ namespace Lsj.Util.Win32
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "CreateThreadpoolTimer", ExactSpelling = true, SetLastError = true)]
         public static extern PTP_TIMER CreateThreadpoolTimer([MarshalAs(UnmanagedType.FunctionPtr)][In] PTP_TIMER_CALLBACK pfnti,
           [In] PVOID pv, [In][Out] ref TP_CALLBACK_ENVIRON pcbe);
+
+        /// <summary>
+        /// <para>
+        /// Creates a new work object.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/threadpoolapiset/nf-threadpoolapiset-createthreadpoolwork
+        /// </para>
+        /// </summary>
+        /// <param name="pfnwk">
+        /// The callback function.
+        /// A worker thread calls this callback each time you call <see cref="SubmitThreadpoolWork"/> to post the work object.
+        /// For details, see WorkCallback.
+        /// </param>
+        /// <param name="pv">
+        /// Optional application-defined data to pass to the callback function.
+        /// </param>
+        /// <param name="pcbe">
+        /// A <see cref="TP_CALLBACK_ENVIRON"/> structure that defines the environment in which to execute the callback.
+        /// The <see cref="InitializeThreadpoolEnvironment"/> function returns this structure.
+        /// If this parameter is <see cref="NullRef{TP_CALLBACK_ENVIRON}"/>, the callback executes in the default callback environment.
+        /// For more information, see <see cref="InitializeThreadpoolEnvironment"/>.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, it returns a TP_WORK structure that defines the work object.
+        /// Applications do not modify the members of this structure.
+        /// If the function fails, it returns <see cref="NULL"/>.
+        /// To retrieve extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// To compile an application that uses this function, define _WIN32_WINNT as 0x0600 or higher.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "CreateThreadpoolWork", ExactSpelling = true, SetLastError = true)]
+        public static extern PTP_WORK CreateThreadpoolWork([In] PTP_WORK_CALLBACK pfnwk, [In] PVOID pv, [In][Out] ref TP_CALLBACK_ENVIRON pcbe);
 
         /// <summary>
         /// <para>
