beb4f12313ab5bb5206e2d51c4bb5e433df2afb8
Shaojun Li <i@shaojun.li>
2020-05-25 10:58:32 +0800

    Add CloseThreadpoolCleanupGroupMembers.

--- Src/Lsj.Util.Win32/Kernel32.ThreadPool.cs
+++ Src/Lsj.Util.Win32/Kernel32.ThreadPool.cs
@@ -267,6 +267,64 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Releases the members of the specified cleanup group, waits for all callback functions to complete,
+        /// and optionally cancels any outstanding callback functions.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/threadpoolapiset/nf-threadpoolapiset-closethreadpoolcleanupgroupmembers
+        /// </para>
+        /// </summary>
+        /// <param name="ptpcg">
+        /// A TP_CLEANUP_GROUP structure that defines the cleanup group.
+        /// The <see cref="CreateThreadpoolCleanupGroup"/> function returns this structure.
+        /// </param>
+        /// <param name="fCancelPendingCallbacks">
+        /// If this parameter is <see cref="TRUE"/>, the function cancels outstanding callbacks that have not yet started.
+        /// If this parameter is <see cref="FALSE"/>, the function waits for outstanding callback functions to complete.
+        /// </param>
+        /// <param name="pvCleanupContext">
+        /// The application-defined data to pass to the application's cleanup group callback function.
+        /// You can specify the callback function when you call <see cref="SetThreadpoolCallbackCleanupGroup"/>.
+        /// </param>
+        /// <remarks>
+        /// The <see cref="CloseThreadpoolCleanupGroupMembers"/> function simplifies cleanup of thread pool callback objects by releasing,
+        /// in a single operation, all work objects, wait objects, and timer objects that are members of the cleanup group.
+        /// An object becomes a member of a cleanup group when the object is created with the threadpool callback environment
+        /// that was specified when the cleanup group was created.
+        /// For more information, see <see cref="CreateThreadpoolCleanupGroup"/>.
+        /// The <see cref="CloseThreadpoolCleanupGroupMembers"/> function blocks until all currently executing callback functions finish.
+        /// If <paramref name="fCancelPendingCallbacks"/> is <see cref="TRUE"/>, outstanding callbacks are canceled;
+        /// otherwise, the function blocks until all outstanding callbacks also finish.
+        /// After the <see cref="CloseThreadpoolCleanupGroupMembers"/> function returns, an application should not use any object
+        /// that was a member of the cleanup group at the time <see cref="CloseThreadpoolCleanupGroupMembers"/> was called.
+        /// Also, an application should not release any of the objects individually by calling a function such as <see cref="CloseThreadpoolWork"/>,
+        /// because the objects have already been released.
+        /// The <see cref="CloseThreadpoolCleanupGroupMembers"/> function does not close the cleanup group itself.
+        /// Instead, the cleanup group persists until the <see cref="CloseThreadpoolCleanupGroup"/> function is called.
+        /// Also, closing a cleanup group does not affect the associated threadpool callback environment.
+        /// The callback environment persists until it is destroyed by calling DestroyThreadpoolEnvironment.
+        /// As long as a cleanup group persists, new objects created with the cleanup group's associated threadpool
+        /// callback environment are added to the cleanup group.
+        /// This allows an application to reuse the cleanup group.
+        /// However, it can lead to errors if the application does not synchronize code
+        /// that calls <see cref="CloseThreadpoolCleanupGroupMembers"/> with code that creates new objects.
+        /// For example, suppose a thread creates two threadpool work objects, Work1 and Work2.
+        /// Another thread calls <see cref="CloseThreadpoolCleanupGroupMembers"/>. Depending on when the threads run, any of the following might occur:
+        /// Work1 and Work2 are added to the cleanup group after its existing members were released. Code that submits Work1 and Work2 will succeed.
+        /// Work1 is added to the cleanup group before its existing members are released, causing Work1 to be released along with other members.
+        /// Then Work2 is added. Code that submits Work1 will generate an exception; code that submits Work2 will succeed.
+        /// Work1 and Work2 are added to the cleanup group before its existing members are released, causing both Work1 and Work2 to be released.
+        /// Code that submits Work1 or Work2 will generate an exception.
+        /// To simply wait for or cancel pending work items without releasing them, use one of the threadpool callback functions:
+        /// <see cref="WaitForThreadpoolIoCallbacks"/>, <see cref="WaitForThreadpoolTimerCallbacks"/>, <see cref="WaitForThreadpoolWaitCallbacks"/>, or <see cref="WaitForThreadpoolWorkCallbacks"/>.
+        /// To compile an application that uses this function, define _WIN32_WINNT as 0x0600 or higher.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "CloseThreadpool", ExactSpelling = true, SetLastError = true)]
+        public static extern void CloseThreadpoolCleanupGroupMembers([In] PTP_CLEANUP_GROUP ptpcg, [In] BOOL fCancelPendingCallbacks,
+            [In] PVOID pvCleanupContext);
+
+        /// <summary>
+        /// <para>
         /// Releases the specified I/O completion object.
         /// </para>
         /// <para>
@@ -992,7 +1050,8 @@ namespace Lsj.Util.Win32
         /// The function is called when you call <see cref="CloseThreadpoolCleanupGroupMembers"/>.
         /// </param>
         /// <remarks>
-        /// 
+        /// This function is implemented as an inline function.
+        /// To compile an application that uses this function, define _WIN32_WINNT as 0x0600 or higher.
         /// </remarks>
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "SetThreadpoolCallbackCleanupGroup", ExactSpelling = true, SetLastError = true)]
         public static extern void SetThreadpoolCallbackCleanupGroup([In][Out] ref TP_CALLBACK_ENVIRON pcbe, [In] PTP_CLEANUP_GROUP ptpcg,
