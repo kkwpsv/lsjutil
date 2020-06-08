54c17a38f75645da115e6c749f06fa9e3b6bafce
Shaojun Li <i@shaojun.li>
2020-06-09 04:15:21 +0800

    Add VirtualAlloc2.

---
+++ Src/Lsj.Util.Win32/Enums/MEM_EXTENDED_PARAMETER_TYPE.cs
@@ -0,0 +1,52 @@
+﻿using static Lsj.Util.Win32.Kernel32;
+
+namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// <para>
+    /// Defines values for extended parameters used for file mapping into an address space.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winnt/ne-winnt-mem_extended_parameter_type
+    /// </para>
+    /// </summary>
+    public enum MEM_EXTENDED_PARAMETER_TYPE : byte
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        MemExtendedParameterInvalidType = 0,
+
+        /// <summary>
+        /// This extended parameter type is used to specify alignment and virtual address range restrictions
+        /// for new memory allocations created by <see cref="VirtualAlloc2"/> and <see cref="MapViewOfFile3"/>.
+        /// </summary>
+        MemExtendedParameterAddressRequirements,
+
+        /// <summary>
+        /// This extended parameter type is used to specify the preferred NUMA node
+        /// for new memory allocations created by <see cref="VirtualAlloc2"/> and <see cref="MapViewOfFile3"/>.
+        /// </summary>
+        MemExtendedParameterNumaNode,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        MemExtendedParameterPartitionHandle,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        MemExtendedParameterUserPhysicalHandle,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        MemExtendedParameterAttributeFlags,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        MemExtendedParameterMax
+    }
+}
--- Src/Lsj.Util.Win32/Kernel32.Memory.cs
+++ Src/Lsj.Util.Win32/Kernel32.Memory.cs
@@ -9,6 +9,7 @@ using static Lsj.Util.Win32.Enums.GlobalMemoryFlags;
 using static Lsj.Util.Win32.Enums.HEAP_INFORMATION_CLASS;
 using static Lsj.Util.Win32.Enums.HeapFlags;
 using static Lsj.Util.Win32.Enums.LocalMemoryFlags;
+using static Lsj.Util.Win32.Enums.MEM_EXTENDED_PARAMETER_TYPE;
 using static Lsj.Util.Win32.Enums.MemoryAllocationTypes;
 using static Lsj.Util.Win32.Enums.MemoryProtectionConstants;
 using static Lsj.Util.Win32.Enums.MemoryTypes;
@@ -1854,6 +1855,175 @@ namespace Lsj.Util.Win32
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "VirtualAlloc", ExactSpelling = true, SetLastError = true)]
         public static extern LPVOID VirtualAlloc([In] LPVOID lpAddress, [In] SIZE_T dwSize, [In] MemoryAllocationTypes flAllocationType,
             [In] MemoryProtectionConstants flProtect);
+
+        /// <summary>
+        /// <para>
+        /// Reserves, commits, or changes the state of a region of memory within the virtual address space of a specified process.
+        /// The function initializes the memory it allocates to zero.
+        /// Using this function, you can: for new allocations, specify a range of virtual address space and a power-of-2 alignment restriction;
+        /// specify an arbitrary number of extended parameters; specify a preferred NUMA node for the physical memory as an extended parameter;
+        /// and specify a placeholder operation (specifically, replacement).
+        /// To specify the NUMA node, see the <paramref name="ExtendedParameters"/> parameter.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/memoryapi/nf-memoryapi-virtualalloc2
+        /// </para>
+        /// </summary>
+        /// <param name="Process">
+        /// The handle to a process.
+        /// The function allocates memory within the virtual address space of this process.
+        /// The handle must have the <see cref="PROCESS_VM_OPERATION"/> access right.
+        /// For more information, see Process Security and Access Rights.
+        /// </param>
+        /// <param name="BaseAddress">
+        /// The pointer that specifies a desired starting address for the region of pages that you want to allocate.
+        /// If an explicit base address is specified, then it must be a multiple of the system allocation granularity.
+        /// To determine the size of a page and the allocation granularity on the host computer, use the <see cref="GetSystemInfo"/> function.
+        /// If <paramref name="BaseAddress"/> is <see cref="NULL"/>, the function determines where to allocate the region.
+        /// If this address is within an enclave that you have not initialized by calling <see cref="InitializeEnclave"/>,
+        /// <see cref="VirtualAlloc2"/> allocates a page of zeros for the enclave at that address.
+        /// The page must be previously uncommitted, and will not be measured with the EEXTEND instruction
+        /// of the Intel Software Guard Extensions programming model.
+        /// If the address in within an enclave that you initialized,
+        /// then the allocation operation fails with the <see cref="ERROR_INVALID_ADDRESS"/> error.
+        /// </param>
+        /// <param name="Size">
+        /// The size of the region of memory to allocate, in bytes.
+        /// The size must always be a multiple of the page size.
+        /// If <paramref name="BaseAddress"/> is not <see cref="NULL"/>, the function allocates all pages that contain one or more bytes
+        /// in the range from <paramref name="BaseAddress"/> to <paramref name="BaseAddress"/>+<paramref name="Size"/>.
+        /// This means, for example, that a 2-byte range that straddles a page boundary causes the function to allocate both pages.
+        /// </param>
+        /// <param name="AllocationType">
+        /// The type of memory allocation. This parameter must contain one of the following values.
+        /// <see cref="MEM_COMMIT"/>:
+        /// Allocates memory charges (from the overall size of memory and the paging files on disk) for the specified reserved memory pages.
+        /// The function also guarantees that when the caller later initially accesses the memory, the contents will be zero.
+        /// Actual physical pages are not allocated unless/until the virtual addresses are actually accessed.
+        /// To reserve and commit pages in one step, call <see cref="VirtualAlloc2"/> with <code>MEM_COMMIT | MEM_RESERVE</code>.
+        /// Attempting to commit a specific address range by specifying <see cref="MEM_COMMIT"/> without <see cref="MEM_RESERVE"/>
+        /// and a non-NULL <paramref name="BaseAddress"/> fails unless the entire range has already been reserved.
+        /// The resulting error code is <see cref="ERROR_INVALID_ADDRESS"/>.
+        /// An attempt to commit a page that is already committed does not cause the function to fail.
+        /// This means that you can commit pages without first determining the current commitment state of each page.
+        /// If <paramref name="BaseAddress"/> specifies an address within an enclave, <paramref name="AllocationType"/> must be <see cref="MEM_COMMIT"/>.
+        /// <see cref="MEM_RESERVE"/>:
+        /// Reserves a range of the process's virtual address space without allocating any actual physical storage in memory
+        /// or in the paging file on disk.
+        /// You commit reserved pages by calling <see cref="VirtualAlloc2"/> again with <see cref="MEM_COMMIT"/>.
+        /// To reserve and commit pages in one step, call <see cref="VirtualAlloc2"/> with <code>MEM_COMMIT | MEM_RESERVE</code>.
+        /// Other memory allocation functions, such as malloc and <see cref="LocalAlloc"/>, cannot use reserved memory until it has been released.
+        /// <see cref="MEM_REPLACE_PLACEHOLDER"/>:
+        /// Replaces a placeholder with a normal private allocation.
+        /// Only data/pf-backed section views are supported (no images, physical memory, etc.).
+        /// When you replace a placeholder, <paramref name="BaseAddress"/> and <paramref name="Size"/> must exactly match those of the placeholder.
+        /// After you replace a placeholder with a private allocation, to free that allocation back to a placeholder,
+        /// see the dwFreeType parameter of <see cref="VirtualFree"/> and <see cref="VirtualFreeEx"/>.
+        /// A placeholder is a type of reserved memory region.
+        /// <see cref="MEM_RESERVE_PLACEHOLDER"/>:
+        /// To create a placeholder, call <see cref="VirtualAlloc2"/> with <code>MEM_RESERVE | MEM_RESERVE_PLACEHOLDER</code>
+        /// and <paramref name="PageProtection"/> set to <see cref="PAGE_NOACCESS"/>.
+        /// To free/split/coalesce a placeholder, see the dwFreeType parameter of <see cref="VirtualFree"/> and <see cref="VirtualFreeEx"/>.
+        /// A placeholder is a type of reserved memory region.
+        /// <see cref="MEM_RESET"/>:
+        /// Indicates that data in the memory range specified by <paramref name="BaseAddress"/> and <paramref name="Size"/> is no longer of interest.
+        /// The pages should not be read from or written to the paging file.
+        /// However, the memory block will be used again later, so it should not be decommitted.
+        /// This value cannot be used with any other value.
+        /// Using this value does not guarantee that the range operated on with <see cref="MEM_RESET"/> will contain zeros.
+        /// If you want the range to contain zeros, decommit the memory and then recommit it.
+        /// When you use <see cref="MEM_RESET"/>, the <see cref="VirtualAlloc2"/> function ignores the value of <paramref name="PageProtection"/>.
+        /// However, you must still set <paramref name="PageProtection"/> to a valid protection value, such as <see cref="PAGE_NOACCESS"/>.
+        /// <see cref="VirtualAlloc2"/> returns an error if you use <see cref="MEM_RESET"/> and the range of memory is mapped to a file.
+        /// A shared view is only acceptable if it is mapped to a paging file.
+        /// <see cref="MEM_RESET_UNDO"/>:
+        /// <see cref="MEM_RESET_UNDO"/> should only be called on an address range to which <see cref="MEM_RESET"/> was successfully applied earlier.
+        /// It indicates that the data in the specified memory range specified by <paramref name="BaseAddress"/> and <paramref name="Size"/>
+        /// is of interest to the caller and attempts to reverse the effects of <see cref="MEM_RESET"/>.
+        /// If the function succeeds, that means all data in the specified address range is intact.
+        /// If the function fails, at least some of the data in the address range has been replaced with zeroes.
+        /// This value cannot be used with any other value.
+        /// If <see cref="MEM_RESET_UNDO"/> is called on an address range which was not <see cref="MEM_RESET"/> earlier, the behavior is undefined.
+        /// When you specify <see cref="MEM_RESET"/>, the <see cref="VirtualAlloc2"/> function ignores the value of <paramref name="PageProtection"/>.
+        /// However, you must still set <paramref name="PageProtection"/> to a valid protection value, such as <see cref="PAGE_NOACCESS"/>.
+        /// Windows Server 2008 R2, Windows 7, Windows Server 2008, Windows Vista, Windows Server 2003 and Windows XP:
+        /// The <see cref="MEM_RESET_UNDO"/> flag is not supported until Windows 8 and Windows Server 2012.
+        /// This parameter can also specify the following values as indicated.
+        /// <see cref="MEM_LARGE_PAGES"/>:
+        /// Allocates memory using large page support.
+        /// The size and alignment must be a multiple of the large-page minimum.
+        /// To obtain this value, use the <see cref="GetLargePageMinimum"/> function.
+        /// If you specify this value, you must also specify <see cref="MEM_RESERVE"/> and <see cref="MEM_COMMIT"/>.
+        /// <see cref="MEM_PHYSICAL"/>:
+        /// Reserves an address range that can be used to map Address Windowing Extensions (AWE) pages.
+        /// This value must be used with <see cref="MEM_RESERVE"/> and no other values.
+        /// <see cref="MEM_TOP_DOWN"/>:
+        /// Allocates memory at the highest possible address. 
+        /// This can be slower than regular allocations, especially when there are many allocations. 
+        /// </param>
+        /// <param name="PageProtection">
+        /// The memory protection for the region of pages to be allocated.
+        /// If the pages are being committed, you can specify any one of the memory protection constants.
+        /// If <paramref name="BaseAddress"/> specifies an address within an enclave,
+        /// <paramref name="PageProtection"/> cannot be any of the following values:
+        /// <see cref="PAGE_NOACCESS"/>, <see cref="PAGE_GUARD"/>, <see cref="PAGE_NOCACHE"/>, <see cref="PAGE_WRITECOMBINE"/>
+        /// </param>
+        /// <param name="ExtendedParameters">
+        /// An optional pointer to one or more extended parameters of type <see cref="MEM_EXTENDED_PARAMETER"/>.
+        /// Each of those extended parameter values can itself have a <see cref="MEM_EXTENDED_PARAMETER.Type"/> field
+        /// of either <see cref="MemExtendedParameterAddressRequirements"/> or <see cref="MemExtendedParameterNumaNode"/>.
+        /// If no <see cref="MemExtendedParameterNumaNode"/> extended parameter is provided,
+        /// then the behavior is the same as for the <see cref="VirtualAlloc"/>/<see cref="MapViewOfFile"/> functions
+        /// (that is, the preferred NUMA node for the physical pages is determined based on the ideal processor
+        /// of the thread that first accesses the memory).
+        /// </param>
+        /// <param name="ParameterCount">
+        /// The number of extended parameters pointed to by <paramref name="ExtendedParameters"/>.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the base address of the allocated region of pages.
+        /// If the function fails, the return value is <see cref="NULL"/>.
+        /// To get extended error information, call <see cref="GetLastError"/>.
+        /// </returns>
+        /// <remarks>
+        /// This API helps support high-performance games, and server applications,
+        /// which have particular requirements around managing their virtual address space.
+        /// For example, mapping memory on top of a previously reserved region; this is useful for implementing an automatically wrapping ring buffer.
+        /// And allocating memory with specific alignment; for example, to enable your application to commit large/huge page-mapped regions on demand.
+        /// Each page has an associated page state.
+        /// The <see cref="VirtualAlloc2"/> function can perform the following operations:
+        /// Commit a region of reserved pages
+        /// Reserve a region of free pages
+        /// Simultaneously reserve and commit a region of free pages
+        /// <see cref="VirtualAlloc2"/> cannot reserve a reserved page. It can commit a page that is already committed.
+        /// This means you can commit a range of pages, regardless of whether they have already been committed, and the function will not fail.
+        /// You can use <see cref="VirtualAlloc2"/> to reserve a block of pages and
+        /// then make additional calls to <see cref="VirtualAlloc2"/> to commit individual pages from the reserved block.
+        /// This enables a process to reserve a range of its virtual address space without consuming physical storage until it is needed.
+        /// If the <paramref name="BaseAddress"/> parameter is not <see cref="NULL"/>,
+        /// the function uses the <paramref name="BaseAddress"/> and <paramref name="Size"/> parameters to compute the region of pages to be allocated.
+        /// The current state of the entire range of pages must be compatible with the type
+        /// of allocation specified by the <paramref name="AllocationType"/> parameter.
+        /// Otherwise, the function fails and none of the pages is allocated.
+        /// This compatibility requirement does not preclude committing an already committed page; see the preceding list.
+        /// To execute dynamically generated code, use <see cref="VirtualAlloc2"/> to allocate memory,
+        /// and the <see cref="VirtualProtectEx"/> function to grant <see cref="PAGE_EXECUTE"/> access.
+        /// The <see cref="VirtualAlloc2"/> function can be used to reserve an Address Windowing Extensions (AWE) region of memory
+        /// within the virtual address space of a specified process.
+        /// This region of memory can then be used to map physical pages into and out of virtual memory as required by the application.
+        /// The <see cref="MEM_PHYSICAL"/> and <see cref="MEM_RESERVE"/> values must be set in the <paramref name="AllocationType"/> parameter.
+        /// The <see cref="MEM_COMMIT"/> value must not be set. The page protection must be set to <see cref="PAGE_READWRITE"/>.
+        /// The <see cref="VirtualFreeEx"/> function can decommit a committed page, releasing the page's storage,
+        /// or it can simultaneously decommit and release a committed page.
+        /// It can also release a reserved page, making it a free page.
+        /// When creating a region that will be executable, the calling program bears responsibility for ensuring cache coherency
+        /// via an appropriate call to <see cref="FlushInstructionCache"/> once the code has been set in place.
+        /// Otherwise attempts to execute code out of the newly executable region may produce unpredictable results.
+        /// </remarks>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, EntryPoint = "VirtualAlloc2", ExactSpelling = true, SetLastError = true)]
+        public static extern PVOID VirtualAlloc2([In] HANDLE Process, [In] PVOID BaseAddress, [In] SIZE_T Size,
+            [In] MemoryAllocationTypes AllocationType, [In] MemoryProtectionConstants PageProtection,
+            [MarshalAs(UnmanagedType.LPArray)][In] MEM_EXTENDED_PARAMETER[] ExtendedParameters, [In] ULONG ParameterCount);
 
         /// <summary>
         /// <para>
