45206ce39285fab9c2eba445393289ea80770527
Shaojun Li <i@shaojun.li>
2020-06-12 20:11:31 +0800

    Add FlashWindowEx.

--- Src/Lsj.Util.Win32/User32.Window.cs
+++ Src/Lsj.Util.Win32/User32.Window.cs
@@ -1572,6 +1572,31 @@ namespace Lsj.Util.Win32
 
         /// <summary>
         /// <para>
+        /// Flashes the specified window.
+        /// It does not change the active state of the window.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-flashwindowex
+        /// </para>
+        /// </summary>
+        /// <param name="pfwi">
+        /// A pointer to a <see cref="FLASHWINFO"/> structure.
+        /// </param>
+        /// <returns>
+        /// The return value specifies the window's state before the call to the <see cref="FlashWindowEx"/> function.
+        /// If the window caption was drawn as active before the call, the return value is <see cref="TRUE"/>.
+        /// Otherwise, the return value is <see cref="FALSE"/>.
+        /// </returns>
+        /// <remarks>
+        /// Typically, you flash a window to inform the user that the window requires attention but does not currently have the keyboard focus.
+        /// When a window flashes, it appears to change from inactive to active status.
+        /// An inactive caption bar changes to an active caption bar; an active caption bar changes to an inactive caption bar.
+        /// </remarks>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "FlashWindowEx", ExactSpelling = true, SetLastError = true)]
+        private static extern BOOL FlashWindowEx([In] in FLASHWINFO pfwi);
+
+        /// <summary>
+        /// <para>
         /// Retrieves the handle to the ancestor of the specified window.
         /// </para>
         /// <para>
