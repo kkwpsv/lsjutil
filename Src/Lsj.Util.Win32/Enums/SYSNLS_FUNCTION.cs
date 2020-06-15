d7b5cff485cd59231f0bdac2e6d95f79b39e60c9
Shaojun Li <i@shaojun.li>
2020-06-15 17:54:15 +0800

    Add SYSNLS_FUNCTION.

---
+++ Src/Lsj.Util.Win32/Enums/SYSNLS_FUNCTION.cs
@@ -0,0 +1,21 @@
+﻿using static Lsj.Util.Win32.Kernel32;
+
+namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// <para>
+    /// Specifies NLS function capabilities.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/winnls/ne-winnls-sysnls_function
+    /// </para>
+    /// </summary>
+    public enum SYSNLS_FUNCTION : uint
+    {
+        /// <summary>
+        /// Value indicating comparison of two strings in the manner of the <see cref="CompareString"/> function
+        /// or <see cref="LCMapString"/> with the <see cref="LCMAP_SORTKEY"/> flag specified.
+        /// </summary>
+        COMPARE_STRING = 1,
+    }
+}
