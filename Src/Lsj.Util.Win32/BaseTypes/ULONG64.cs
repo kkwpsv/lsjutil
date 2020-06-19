3d01642691f46c26115c0ece41de9b97554254fe
Shaojun Li <i@shaojun.li>
2020-06-20 02:30:53 +0800

    Add MapViewOfFile3.

---
+++ Src/Lsj.Util.Win32/BaseTypes/ULONG64.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.BaseTypes
+{
+    /// <summary>
+    /// <para>
+    /// A <see cref="ULONG64"/> is a 64-bit unsigned integer (range: 0 through 18446744073709551615 decimal).
+    /// Because a <see cref="ULONG64"/> is unsigned, its first bit (Most Significant Bit (MSB)) is not reserved for signing.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/openspecs/windows_protocols/ms-dtyp/2dc4c492-95db-4fa6-ae2b-8546b13c9141
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 8)]
+    public struct ULONG64
+    {
+        [FieldOffset(0)]
+        private ulong _value;
+
+        /// <inheritdoc/>
+        public override string ToString() => _value.ToString();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator ulong(ULONG64 val) => val._value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator ULONG64(ulong val) => new ULONG64 { _value = val };
+    }
+}
