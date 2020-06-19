60af9f9bfbc324b906299145dc709061ea4ece30
Shaojun Li <i@shaojun.li>
2020-06-20 02:50:10 +0800

    Add MKRREDUCE.

---
+++ Src/Lsj.Util.Win32/Enums/MKRREDUCE.cs
@@ -0,0 +1,36 @@
+﻿namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// <para>
+    /// Specifies how far a moniker should be reduced.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/objidl/ne-objidl-mkrreduce
+    /// </para>
+    /// </summary>
+    public enum MKRREDUCE : uint
+    {
+        /// <summary>
+        /// Performs only one step of reducing the moniker.
+        /// In general, the caller must have specific knowledge about the particular kind of moniker to take advantage of this option.
+        /// </summary>
+        MKRREDUCE_ONE = (3 << 16),
+
+        /// <summary>
+        ///  Reduces the moniker to a form that the user identifies as a persistent object.
+        ///  If no such point exists, then this option should be treated as <see cref="MKRREDUCE_ALL"/>.
+        /// </summary>
+        MKRREDUCE_TOUSER = (2 << 16),
+
+        /// <summary>
+        /// Reduces the moniker to where any further reduction would reduce it to a form that the user does not identify as a persistent object.
+        /// Often, this is the same stage as <see cref="MKRREDUCE_TOUSER"/>.
+        /// </summary>
+        MKRREDUCE_THROUGHUSER = (1 << 16),
+
+        /// <summary>
+        /// Reduces the moniker until it is in its simplest form, that is, reduce it to itself.
+        /// </summary>
+        MKRREDUCE_ALL = 0
+    }
+}
