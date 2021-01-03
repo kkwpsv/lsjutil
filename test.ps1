792ce2b239e39c63dc6ea35fef1bb7e714646b98
Shaojun Li <i@shaojun.li>
2021-01-03 18:40:07 +0800

    Format coverage.

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
 Set-Location Test/Lsj.Util.Tests
 dotnet test --collect:"XPlat Code Coverage"
-$coverage = (Get-Content .\TestResults\*\coverage.cobertura.xml | Select-String -Pattern "coverage line-rate=`"(.+?)`"").Matches.Groups[1]
-Write-Host "Coverage: $coverage"
+$coverage = (Get-Content .\TestResults\*\coverage.cobertura.xml | Select-String -Pattern "coverage line-rate=`"(.+?)`"").Matches.Groups[1].Value
+Write-Host "Coverage: $("{0:n2}" -f ([decimal]$coverage*100))%"
