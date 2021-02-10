f43e05464477e6f351609e8fa243f15d4a328950
Shaojun Li <i@shaojun.li>
2021-02-10 13:26:45 +0800

    Add many dwm apis.

---
+++ Src/Lsj.Util.Win32/Enums/DWMFLIP3DWINDOWPOLICY.cs
@@ -0,0 +1,41 @@
+﻿using static Lsj.Util.Win32.Dwmapi;
+using static Lsj.Util.Win32.Enums.DWMWINDOWATTRIBUTE;
+
+namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// <para>
+    /// Flags used by the <see cref="DwmSetWindowAttribute"/> function to specify the Flip3D window policy.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/dwmapi/ne-dwmapi-dwmflip3dwindowpolicy
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// To use a <see cref="DWMFLIP3DWINDOWPOLICY"/> value,
+    /// set the dwAttribute parameter of the <see cref="DwmSetWindowAttribute"/> function to <see cref="DWMWA_FLIP3D_POLICY"/>.
+    /// Set the pvAttribute parameter to the <see cref="DWMFLIP3DWINDOWPOLICY"/> value.
+    /// </remarks>
+    public enum DWMFLIP3DWINDOWPOLICY
+    {
+        /// <summary>
+        /// Use the window's style and visibility settings to determine whether to hide or include the window in Flip3D rendering.
+        /// </summary>
+        DWMFLIP3D_DEFAULT,
+
+        /// <summary>
+        /// Exclude the window from Flip3D and display it below the Flip3D rendering.
+        /// </summary>
+        DWMFLIP3D_EXCLUDEBELOW,
+
+        /// <summary>
+        /// Exclude the window from Flip3D and display it above the Flip3D rendering.
+        /// </summary>
+        DWMFLIP3D_EXCLUDEABOVE,
+
+        /// <summary>
+        /// 	The maximum recognized DWMFLIP3DWINDOWPOLICY value, used for validation purposes.
+        /// </summary>
+        DWMFLIP3D_LAST
+    }
+}
---
+++ Src/Lsj.Util.Win32/Enums/DWMNCRENDERINGPOLICY.cs
@@ -0,0 +1,41 @@
+﻿using static Lsj.Util.Win32.Dwmapi;
+using static Lsj.Util.Win32.Enums.DWMWINDOWATTRIBUTE;
+
+namespace Lsj.Util.Win32.Enums
+{
+    /// <summary>
+    /// <para>
+    /// Flags used by the <see cref="DwmSetWindowAttribute"/> function to specify the non-client area rendering policy.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/dwmapi/ne-dwmapi-dwmncrenderingpolicy
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// To use a <see cref="DWMNCRENDERINGPOLICY"/> value, set the dwAttribute parameter
+    /// of the <see cref="DwmSetWindowAttribute"/> function to <see cref="DWMWA_NCRENDERING_POLICY"/>.
+    /// Set the pvAttribute parameter to the <see cref="DWMNCRENDERINGPOLICY"/> value.
+    /// </remarks>
+    public enum DWMNCRENDERINGPOLICY
+    {
+        /// <summary>
+        /// The non-client rendering area is rendered based on the window style.
+        /// </summary>
+        DWMNCRP_USEWINDOWSTYLE,
+
+        /// <summary>
+        /// The non-client area rendering is disabled; the window style is ignored.
+        /// </summary>
+        DWMNCRP_DISABLED,
+
+        /// <summary>
+        /// The non-client area rendering is enabled; the window style is ignored.
+        /// </summary>
+        DWMNCRP_ENABLED,
+
+        /// <summary>
+        /// The maximum recognized <see cref="DWMNCRENDERINGPOLICY"/> value, used for validation purposes.
+        /// </summary>
+        DWMNCRP_LAST
+    }
+}
