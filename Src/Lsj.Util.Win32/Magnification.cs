6146c8bd798cf3601335272a6a5bde5b96ffceee
Shaojun Li <i@shaojun.li>
2021-04-04 10:50:00 +0800

    Add magnification apis.

---
+++ Src/Lsj.Util.Win32/Magnification.cs
@@ -0,0 +1,140 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using Lsj.Util.Win32.Structs;
+using System;
+using System.Runtime.InteropServices;
+using static Lsj.Util.Win32.BaseTypes.BOOL;
+
+namespace Lsj.Util.Win32
+{
+    /// <summary>
+    /// Magnification.dll
+    /// </summary>
+    public static class Magnification
+    {
+        /// <summary>
+        /// <para>
+        /// Prototype for a callback function that implements a custom transform for image scaling.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/magnification/nc-magnification-magimagescalingcallback
+        /// </para>
+        /// </summary>
+        /// <param name="hwnd">
+        /// The magnification window.
+        /// </param>
+        /// <param name="srcdata">
+        /// The input data.
+        /// </param>
+        /// <param name="srcheader">
+        /// The description of the input format.
+        /// </param>
+        /// <param name="destdata">
+        /// The output data.
+        /// </param>
+        /// <param name="destheader">
+        /// The description of the output format.
+        /// </param>
+        /// <param name="unclipped">
+        /// The coordinates of the scaled version of the source bitmap.
+        /// </param>
+        /// <param name="clipped">
+        /// The coordinates of the window to which the scaled bitmap is clipped.
+        /// </param>
+        /// <param name="dirty">
+        /// The region that needs to be refreshed.
+        /// </param>
+        /// <returns>
+        /// Returns <see cref="TRUE"/> if successful, or <see cref="FALSE"/> otherwise.
+        /// </returns>
+        [Obsolete(" The MagImageScalingCallback function is deprecated in Windows 7 and later, and should not be used in new applications." +
+            " There is no alternate functionality.")]
+        public delegate BOOL MagImageScalingCallback([In] HWND hwnd, [In] IntPtr srcdata, [In] MAGIMAGEHEADER srcheader, [In] IntPtr destdata,
+            [In] MAGIMAGEHEADER destheader, [In] RECT unclipped, [In] RECT clipped, [In] HRGN dirty);
+
+        /// <summary>
+        /// <para>
+        /// Gets the rectangle of the area that is being magnified.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/magnification/nf-magnification-maggetwindowsource
+        /// </para>
+        /// </summary>
+        /// <param name="hwnd">
+        /// The magnification window.
+        /// </param>
+        /// <param name="pRect">
+        /// The rectangle that is being magnified, in desktop coordinates.
+        /// </param>
+        /// <returns>
+        /// Returns <see cref="TRUE"/> if successful, or <see cref="FALSE"/> otherwise.
+        /// </returns>
+        [DllImport("Magnification.dll", CharSet = CharSet.Unicode, EntryPoint = "MagGetWindowSource", ExactSpelling = true, SetLastError = true)]
+        public static extern BOOL MagGetWindowSource([In] HWND hwnd, [In] in RECT pRect);
+
+        /// <summary>
+        /// <para>
+        /// Creates and initializes the magnifier run-time objects.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/magnification/nf-magnification-maginitialize
+        /// </para>
+        /// </summary>
+        /// <returns>
+        /// Returns <see cref="TRUE"/> if initialization was successful; otherwise <see cref="FALSE"/>.
+        /// </returns>
+        [DllImport("Magnification.dll", CharSet = CharSet.Unicode, EntryPoint = "MagInitialize", ExactSpelling = true, SetLastError = true)]
+        public static extern BOOL MagInitialize();
+
+        /// <summary>
+        /// <para>
+        /// Sets the callback function for external image filtering and scaling.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/magnification/nf-magnification-magsetimagescalingcallback
+        /// </para>
+        /// </summary>
+        /// <param name="hwnd">
+        /// The handle of the magnification window.
+        /// </param>
+        /// <param name="callback">
+        /// The callback function, or <see langword="null"/> to remove a callback that was previously set.
+        /// </param>
+        /// <returns>
+        /// Returns <see cref="TRUE"/> if successful, or <see cref="FALSE"/> otherwise.
+        /// </returns>
+        /// <remarks>
+        /// This function requires Windows Display Driver Model (WDDM)-capable video cards.
+        /// This function works only when Desktop Window Manager (DWM) is off.
+        /// This callback mechanism enables custom image filtering and scaling mechanisms.
+        /// Filtering might include bilinear, trilinear, bicubic, and flat.
+        /// The mechanism also enables edge detection and enhancement.
+        /// The only transform that can be performed within the callback is scaling.
+        /// Rotations and skews that may compose the arbitrary transform passed to the <see cref="MagSetWindowTransform"/> function are performed after the callback function returns.
+        /// The specified function is called by the magnification engine for all rasterized Windows Graphics Device Interface (GDI) bitmaps before they are composited.
+        /// After the callback function returns, the bitmap in video memory can have one of the following size states:
+        /// Unscaled. The returned bitmap is the same size as the bitmap passed by the caller.
+        /// The magnification engine does the scaling by the transform specified in the <see cref="MagSetWindowTransform"/> function.
+        /// Scaled. The returned bitmap is scaled by the transform specified in <see cref="MagSetWindowTransform"/>.
+        /// If no callback is registered, the magnification engine scales bitmaps by the transform specified in <see cref="MagSetWindowTransform"/>.
+        /// Windows Presentation Foundation (WPF) bitmaps can be scaled automatically using flat, bilinear, bicubic filtering and consequently do not use this callback mechanism.
+        /// </remarks>
+        [Obsolete("The MagSetImageScalingCallback function is deprecated in Windows 7 and later, and should not be used in new applications." +
+            " There is no alternate functionality.")]
+        [DllImport("Magnification.dll", CharSet = CharSet.Unicode, EntryPoint = "MagSetImageScalingCallback", ExactSpelling = true, SetLastError = true)]
+        public static extern BOOL MagSetImageScalingCallback([In] HWND hwnd, [In] MagImageScalingCallback callback);
+
+        /// <summary>
+        /// <para>
+        /// Destroys the magnifier run-time objects.
+        /// </para>
+        /// <para>
+        /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/magnification/nf-magnification-maguninitialize
+        /// </para>
+        /// </summary>
+        /// <returns>
+        /// Returns <see cref="TRUE"/> if successful, or <see cref="FALSE"/> otherwise.
+        /// </returns>
+        [DllImport("Magnification.dll", CharSet = CharSet.Unicode, EntryPoint = "MagUninitialize", ExactSpelling = true, SetLastError = true)]
+        public static extern BOOL MagUninitialize();
+    }
+}
---
+++ Src/Lsj.Util.Win32/Structs/MAGIMAGEHEADER.cs
@@ -0,0 +1,51 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.Structs
+{
+    /// <summary>
+    /// <para>
+    /// Describes an image format.
+    /// </para>
+    /// <para>
+    /// From: https://docs.microsoft.com/zh-cn/windows/win32/api/magnification/ns-magnification-magimageheader
+    /// </para>
+    /// </summary>
+    [Obsolete("The MAGIMAGEHEADER structure is deprecated in Windows 7 and later, and should not be used in new applications. " +
+        "There is no alternate functionality.")]
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct MAGIMAGEHEADER
+    {
+        /// <summary>
+        /// The width of the image.
+        /// </summary>
+        public UINT width;
+
+        /// <summary>
+        /// The height of the image.
+        /// </summary>
+        public UINT height;
+
+        /// <summary>
+        /// A WICPixelFormatGUID (declared in wincodec.h) that specifies the pixel format of the image.
+        /// For a list of available pixel formats, see the Native Pixel Formats topic.
+        /// </summary>
+        public GUID format;
+
+        /// <summary>
+        /// The stride, or number of bytes in a row of the image.
+        /// </summary>
+        public UINT stride;
+
+        /// <summary>
+        /// The offset of the start of the image data from the beginning of the file.
+        /// </summary>
+        public UINT offset;
+
+        /// <summary>
+        /// The size of the data.
+        /// </summary>
+        public SIZE_T cbSize;
+    }
+}
