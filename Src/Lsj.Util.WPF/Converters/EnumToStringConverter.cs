5a07ff6bb3de0285fcee20b2d98ec64ce79a54fe
Shaojun Li <i@shaojun.li>
2021-04-05 14:38:44 +0800

    Add EnumToStringConverter.

---
+++ Src/Lsj.Util.WPF/Converters/EnumToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Lsj.Util.WPF.Converters
+{
+    /// <summary>
+    /// <see cref="Enum"/> To <see cref="string"/> Converter
+    /// </summary>
+    [ValueConversion(typeof(Enum), typeof(string))]
+    public class EnumToStringConverter : IValueConverter
+    {
+        /// <summary>
+        /// Enum Type
+        /// </summary>
+        public Type EnumType { get; set; }
+
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is Enum ? value.ToString() : DependencyProperty.UnsetValue;
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var target = EnumType ?? targetType;
+            return target != null && target.IsEnum && value is string str ? Enum.Parse(target, str) : throw new InvalidOperationException();
+        }
+    }
+}
