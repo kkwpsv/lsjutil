e07516f8af70256e87c18b4f4b5edf281565bbd0
Shaojun Li <i@shaojun.li>
2021-06-13 16:30:41 +0800

    Add LPCWSTR.

---
+++ Src/Lsj.Util.Win32/Marshals/LPCWSTR.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.Marshals
+{
+    /// <summary>
+    /// LPCWSTR implement by pinned string or dirtect pointer.
+    /// </summary>
+    public class LPCWSTR : CriticalHandle
+    {
+        private GCHandle _gcHandle;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="str"></param>
+        public LPCWSTR(string str) : base(IntPtr.Zero)
+        {
+            _gcHandle = GCHandle.Alloc(str, GCHandleType.Pinned);
+            SetHandle(_gcHandle.AddrOfPinnedObject());
+        }
+
+        public LPCWSTR(IntPtr handle) : base(IntPtr.Zero)
+        {
+            SetHandle(handle);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <inheritdoc/>
+        public override bool IsInvalid => IsClosed;
+
+        /// <inheritdoc/>
+        protected override bool ReleaseHandle()
+        {
+            if (_gcHandle.IsAllocated)
+            {
+                _gcHandle.Free();
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator LPCWSTR(string val) => new LPCWSTR(val);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        public static implicit operator LPCWSTR(IntPtr val) => new LPCWSTR(val);
+    }
+}
