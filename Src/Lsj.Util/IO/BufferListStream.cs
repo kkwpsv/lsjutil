c712bfedb96eb30e84612dc93e6d2a6557948ec2
Shaojun Li <i@shaojun.li>
2021-11-21 15:34:15 +0800

    Add BufferListStream.

---
+++ Src/Lsj.Util/IO/BufferListStream.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+
+namespace Lsj.Util.IO
+{
+    /// <summary>
+    /// A Stream that write and read once buffer list.
+    /// </summary>
+    public class BufferListStream : Stream
+    {
+        private int _currentBufferPos = 0;
+
+        public ConcurrentQueue<byte[]> _buffers = new ConcurrentQueue<byte[]>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public BufferListStream()
+        {
+
+        }
+
+        /// <inheritdoc/>
+        public override bool CanRead => true;
+
+        /// <inheritdoc/>
+        public override bool CanSeek => false;
+
+        /// <inheritdoc/>
+        public override bool CanWrite => true;
+
+        /// <inheritdoc/>
+        public override long Length => throw new NotSupportedException();
+
+        /// <inheritdoc/>
+        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+        /// <inheritdoc/>
+        public override void Flush()
+        {
+        }
+
+        private object _readLockObject = new object();
+        /// <inheritdoc/>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            lock (_readLockObject)
+            {
+                var write = 0;
+                while (count > 0 && _buffers.TryPeek(out var bufferToRead))
+                {
+                    var avaliable = bufferToRead.Length - _currentBufferPos;
+                    if (avaliable > count)
+                    {
+                        UnsafeHelper.Copy(bufferToRead, _currentBufferPos, buffer, offset, count);
+                        write += count;
+                        offset += count;
+                        count -= count;
+                        _currentBufferPos += count;
+                    }
+                    else
+                    {
+                        UnsafeHelper.Copy(bufferToRead, _currentBufferPos, buffer, offset, avaliable);
+                        write += avaliable;
+                        offset += avaliable;
+                        count -= avaliable;
+                        _currentBufferPos = 0;
+                        _buffers.TryDequeue(out _);
+                    }
+                }
+                return write;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+
+        /// <inheritdoc/>
+        public override void SetLength(long value) => throw new NotSupportedException();
+
+        /// <inheritdoc/>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+#if NET40
+            var copiedBuffer = new byte[count];
+            UnsafeHelper.Copy(buffer, copiedBuffer, count);
+            AddBuffer(copiedBuffer);
+#else
+            AddBuffer(buffer.AsSpan(offset, count).ToArray());
+#endif
+        }
+
+        /// <summary>
+        /// Add new buffer to buffer list.
+        /// </summary>
+        /// <param name="buffer"></param>
+        public void AddBuffer(byte[] buffer)
+        {
+            _buffers.Enqueue(buffer);
+        }
+    }
+}
---
+++ Test/Lsj.Util.Tests/IO/BufferListStreamTests.cs
@@ -0,0 +1,43 @@
+﻿using Lsj.Util.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lsj.Util.Tests.IO
+{
+    [TestClass]
+    public class BufferListStreamTests
+    {
+        [TestMethod]
+        public void TestReadWrite()
+        {
+            var stream = new BufferListStream();
+            var buffer1 = new byte[] { 0, 1, 2, 3, 5 };
+            stream.AddBuffer(buffer1);
+            buffer1[4] = 4;
+            var buffer2 = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            stream.Write(buffer2, 5, 5);
+            buffer2[5] = 6;
+
+            int val = 0;
+            var readBuffer = new byte[5];
+            Assert.AreEqual(5, stream.Read(readBuffer));
+            for (int i = 0; i < 5; i++, val++)
+            {
+                Assert.AreEqual(val, readBuffer[i]);
+            }
+            readBuffer = new byte[0];
+            Assert.AreEqual(0, stream.Read(readBuffer));
+            readBuffer = new byte[10];
+            Assert.AreEqual(5, stream.Read(readBuffer));
+            for (int i = 0; i < 5; i++, val++)
+            {
+                Assert.AreEqual(val, readBuffer[i]);
+            }
+            Assert.AreEqual(0, stream.Read(readBuffer));
+        }
+    }
+}
