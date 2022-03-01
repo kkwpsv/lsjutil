4c4b8e48e3444aa59cce6568ede6f94b90656e24
Shaojun Li <i@shaojun.li>
2022-03-01 11:27:18 +0800

    Add tests.

---
+++ Test/Lsj.Util.Tests/Win32/NativeUI/Win32WindowTests.cs
@@ -0,0 +1,29 @@
+﻿using Lsj.Util.Win32.NativeUI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Lsj.Util.Win32.Enums.WindowStyles;
+using static Lsj.Util.Win32.Enums.WindowStylesEx;
+
+namespace Lsj.Util.Tests.Win32.NativeUI
+{
+    [TestClass]
+    public class Win32WindowTests
+    {
+        [TestMethod]
+        public void TestCreateWin32Window()
+        {
+            var win = new Win32Window();
+            Assert.IsTrue(win.Handle != IntPtr.Zero);
+        }
+
+        [TestMethod]
+        public void TestWin32WindowStyle()
+        {
+            var win = new Win32Window();
+            win.WindowStyles |= WS_POPUP;
+            Assert.IsTrue(win.WindowStyles.HasFlag(WS_POPUP));
+            win.WindowStylesEx |= WS_EX_NOACTIVATE;
+            Assert.IsTrue(win.WindowStylesEx.HasFlag(WS_EX_NOACTIVATE));
+        }
+    }
+}
