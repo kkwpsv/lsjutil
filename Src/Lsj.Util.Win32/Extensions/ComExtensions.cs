a07f3182018ff3ff6015e394e5bfc5e24f0e5d38
Shaojun Li <i@shaojun.li>
2022-05-29 18:48:07 +0800

    Add InterfaceFromPointer.

---
+++ Src/Lsj.Util.Win32/Extensions/ComExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lsj.Util.Win32.Extensions
+{
+    /// <summary>
+    /// COM Extensions
+    /// </summary>
+    public static class ComExtensions
+    {
+        /// <summary>
+        /// <para>
+        /// Get com interface struct from pointer
+        /// </para>
+        /// <para>
+        /// ONLY use return value directly.
+        /// DON'T copy return value (like assign it to local variables), which will crash.
+        /// </para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pointer"></param>
+        /// <returns></returns>
+        public static unsafe ref T InterfaceFromPointer<T>(IntPtr pointer) where T : struct
+        {
+            return ref UnsafePInvokeExtensions.AsStructRef<T>(pointer);
+        }
+    }
+}
