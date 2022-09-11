7e77e9842755f22a11d1535e7e043803cba8bb0e
Shaojun Li <i@shaojun.li>
2022-09-12 00:47:24 +0800

    Add GDI methods.

---
+++ Src/Lsj.Util.Win32.NativeUI/GDI/DeviceContext.cs
@@ -0,0 +1,76 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using Lsj.Util.Win32.Enums;
+using static Lsj.Util.Win32.Gdi32;
+
+namespace Lsj.Util.Win32.NativeUI.GDI
+{
+    /// <summary>
+    /// Device context
+    /// </summary>
+    public class DeviceContext
+    {
+        /// <summary>
+        /// Handle
+        /// </summary>
+        public HDC Handle { get; private set; }
+
+        /// <summary>
+        /// Device context
+        /// </summary>
+        /// <param name="dc"></param>
+        public DeviceContext(HDC dc)
+        {
+            Handle = dc;
+        }
+
+        /// <summary>
+        /// Driver version
+        /// </summary>
+        public int DriverVersion => GetDeviceCaps(Handle, DeviceCapIndexes.DRIVERVERSION);
+
+        /// <summary>
+        /// Device technologies
+        /// </summary>
+        public DeviceTechnologies Technologies => (DeviceTechnologies)GetDeviceCaps(Handle, DeviceCapIndexes.TECHNOLOGY);
+
+        /// <summary>
+        /// Physical width in millimeters
+        /// </summary>
+        public int PhysicalWidth => GetDeviceCaps(Handle, DeviceCapIndexes.HORZSIZE);
+
+        /// <summary>
+        /// Physical height in millimeters
+        /// </summary>
+        public int PhysicalHeight => GetDeviceCaps(Handle, DeviceCapIndexes.VERTSIZE);
+
+        /// <summary>
+        /// Width in pixels
+        /// </summary>
+        public int Width => GetDeviceCaps(Handle, DeviceCapIndexes.HORZRES);
+
+        /// <summary>
+        /// Height in pixels
+        /// </summary>
+        public int Height => GetDeviceCaps(Handle, DeviceCapIndexes.VERTRES);
+
+        /// <summary>
+        /// Dpi in width
+        /// </summary>
+        public int DpiX => GetDeviceCaps(Handle, DeviceCapIndexes.LOGPIXELSX);
+
+        /// <summary>
+        /// Dpi in height
+        /// </summary>
+        public int DpiY => GetDeviceCaps(Handle, DeviceCapIndexes.LOGPIXELSY);
+
+        /// <summary>
+        /// Color bits per pixel
+        /// </summary>
+        public int ColorBits => GetDeviceCaps(Handle, DeviceCapIndexes.BITSPIXEL);
+
+        /// <summary>
+        /// Vertical refresh rate
+        /// </summary>
+        public int RefreshRate => GetDeviceCaps(Handle, DeviceCapIndexes.VREFRESH);
+    }
+}
---
+++ Src/Lsj.Util.Win32.NativeUI/GDI/PaintScope.cs
@@ -0,0 +1,55 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using Lsj.Util.Win32.Structs;
+using System;
+using System.ComponentModel;
+
+namespace Lsj.Util.Win32.NativeUI.GDI
+{
+    /// <summary>
+    /// Paint Scope
+    /// </summary>
+    public class PaintScope : IDisposable
+    {
+        private HWND _hwnd;
+        private PAINTSTRUCT _paintStruct;
+
+        /// <summary>
+        /// Device Context
+        /// </summary>
+        public DeviceContext DeviceContext { get; private set; }
+
+        private PaintScope(HWND hwnd, PAINTSTRUCT paintStruct, HDC hdc)
+        {
+            _hwnd = hwnd;
+            _paintStruct = paintStruct;
+            DeviceContext = new DeviceContext(hdc);
+        }
+
+        /// <summary>
+        /// EndPaint
+        /// </summary>
+        public void Dispose()
+        {
+            User32.EndPaint(_hwnd, _paintStruct);
+        }
+
+        /// <summary>
+        /// Begin
+        /// </summary>
+        /// <param name="hwnd"></param>
+        /// <returns></returns>
+        /// <exception cref="Win32Exception"></exception>
+        public static PaintScope Begin(HWND hwnd)
+        {
+            var hdc = User32.BeginPaint(hwnd, out var paintStruct);
+            if (hdc != IntPtr.Zero)
+            {
+                return new PaintScope(hwnd, paintStruct, hdc);
+            }
+            else
+            {
+                throw new Win32Exception();
+            }
+        }
+    }
+}
