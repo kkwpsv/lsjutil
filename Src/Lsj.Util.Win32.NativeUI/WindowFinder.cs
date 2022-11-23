fc434a9bd2203aa9527775226d92db04e71d6f6c
Shaojun Li <i@shaojun.li>
2022-11-23 10:48:34 +0800

    Add WindowFinder

---
+++ Src/Lsj.Util.Win32.NativeUI/WindowFinder.cs
@@ -0,0 +1,29 @@
+﻿using Lsj.Util.Win32.Structs;
+using System;
+
+namespace Lsj.Util.Win32.NativeUI
+{
+    /// <summary>
+    /// Window Finder
+    /// </summary>
+    public static class WindowFinder
+    {
+        /// <summary>
+        /// Find window from point.
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public static Win32Window? FromPoint(POINT point)
+        {
+            var handle = User32.WindowFromPoint(point);
+            if (handle != IntPtr.Zero)
+            {
+                return new Win32Window(handle);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
