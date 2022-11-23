7866421e7e2c32534163b23624e88dfef7c7dd4d
Shaojun Li <i@shaojun.li>
2022-11-23 16:44:17 +0800

    Add DrawingRectangle

--- Src/Lsj.Util.Win32.NativeUI/GDI/DeviceContext.Drawing.cs
+++ Src/Lsj.Util.Win32.NativeUI/GDI/DeviceContext.Drawing.cs
@@ -4,8 +4,34 @@ namespace Lsj.Util.Win32.NativeUI.GDI
 {
     partial class DeviceContext
     {
+        /// <summary>
+        /// Select brush
+        /// </summary>
+        /// <param name="pen"></param>
+        /// <returns></returns>
         public Brush SelectBrush(Brush pen) => new Brush(SelectObject(Handle, pen.Handle), GdiObjectReleaseMode.None);
 
+        /// <summary>
+        /// Select pen
+        /// </summary>
+        /// <param name="pen"></param>
+        /// <returns></returns>
         public Pen SelectPen(Pen pen) => new Pen(SelectObject(Handle, pen.Handle), GdiObjectReleaseMode.None);
+
+        /// <summary>
+        /// Drawing rectangle with current pen and current brush.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="top"></param>
+        /// <param name="right"></param>
+        /// <param name="bottom"></param>
+        /// <exception cref="GDIOperationFailedException"></exception>
+        public void DrawingRectangle(int left, int top, int right, int bottom)
+        {
+            if (!Rectangle(Handle, left, top, right, bottom))
+            {
+                throw new GDIOperationFailedException(nameof(Rectangle));
+            }
+        }
     }
 }
