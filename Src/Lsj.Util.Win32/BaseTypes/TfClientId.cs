907eb0c58c869df1746c9938fea2c3fcaffa46ee
Shaojun Li <i@shaojun.li>
2023-03-07 09:57:56 +0800

    Add ITfThreadMgr.

---
+++ Src/Lsj.Util.Win32/BaseTypes/TfClientId.cs
@@ -0,0 +1,32 @@
+﻿using Lsj.Util.Win32.ComInterfaces;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.BaseTypes
+{
+    /// <summary>
+    /// <para>
+    /// The <see cref="TfClientId"/> data type is used to identify the client.
+    /// </para>
+    /// <para>
+    /// From: <see href="https://learn.microsoft.com/en-us/windows/win32/tsf/tfclientid"/>
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// Within TSF, applications and text services are generally referred to as clients.
+    /// Each client receives a unique identifier that it uses to identify itself when calling various TSF manager methods.
+    /// This identifier is of the <see cref="TfClientId"/> type.
+    /// The <see cref="TfClientId"/> data type is supplied by the TSF manager.
+    /// An application obtains a <see cref="TfClientId"/> value when it calls <see cref="ITfThreadMgr.Activate"/>.
+    /// The <see cref="TfClientId"/> value for a text service is passed to the <see cref="ITfTextInputProcessor.Activate"/> method.
+    /// Any object that does not fit the above categories can obtain a client identifier by calling <see cref="ITfClientId.GetClientId"/>.
+    /// </remarks>
+    [StructLayout(LayoutKind.Explicit, Size = 4)]
+    public struct TfClientId
+    {
+        [FieldOffset(0)]
+        private uint _value;
+
+        /// <inheritdoc/>
+        public override string ToString() => _value.ToString();
+    }
+}
---
+++ Src/Lsj.Util.Win32/ComInterfaces/ITfThreadMgr.cs
@@ -0,0 +1,59 @@
+﻿using Lsj.Util.Win32.BaseTypes;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Lsj.Util.Win32.ComInterfaces
+{
+    public unsafe struct ITfThreadMgr
+    {
+        IntPtr* _vTable;
+
+        public HRESULT Activate([Out] out TfClientId ptid)
+        {
+            fixed (void* thisPtr = &this)
+            {
+                return ((delegate* unmanaged[Stdcall]<void*, out TfClientId, HRESULT>)_vTable[3])(thisPtr, out ptid);
+            }
+        }
+
+        public HRESULT Deactivate()
+        {
+            fixed (void* thisPtr = &this)
+            {
+                return ((delegate* unmanaged[Stdcall]<void*, HRESULT>)_vTable[4])(thisPtr);
+            }
+        }
+
+        public HRESULT CreateDocumentMgr([Out] out P<ITfDocumentMgr> ppdim)
+        {
+            fixed (void* thisPtr = &this)
+            {
+                return ((delegate* unmanaged[Stdcall]<void*, out P<ITfDocumentMgr>, HRESULT>)_vTable[5])(thisPtr, out ppdim);
+            }
+        }
+
+        public HRESULT GetFocus([Out] out P<ITfDocumentMgr> ppdimFocus)
+        {
+            fixed (void* thisPtr = &this)
+            {
+                return ((delegate* unmanaged[Stdcall]<void*, out P<ITfDocumentMgr>, HRESULT>)_vTable[7])(thisPtr, out ppdimFocus);
+            }
+        }
+
+        public HRESULT SetFocus([In] in ITfDocumentMgr ppdimFocus)
+        {
+            fixed (void* thisPtr = &this)
+            {
+                return ((delegate* unmanaged[Stdcall]<void*, in ITfDocumentMgr, HRESULT>)_vTable[8])(thisPtr, ppdimFocus);
+            }
+        }
+
+        public HRESULT IsThreadFocus([Out] out BOOL pfThreadFocus)
+        {
+            fixed (void* thisPtr = &this)
+            {
+                return ((delegate* unmanaged[Stdcall]<void*, out BOOL, HRESULT>)_vTable[10])(thisPtr, out pfThreadFocus);
+            }
+        }
+    }
+}
